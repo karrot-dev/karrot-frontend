ec7e4f462f076878d522c54b5530e8f364fafe0c
Michael Dougherty <maackle.d@gmail.com>
2015-09-24 12:26:54 +0200

    adds simple test, adds django-extensions

--- wuppdays/settings.py
+++ wuppdays/settings.py
@@ -31,14 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # core Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'yunity',
+
+    # Django packages
     'django_jinja',
+    'django_extensions',
+
+    # Application
+    'yunity',
 )
 
 MIDDLEWARE_CLASSES = (

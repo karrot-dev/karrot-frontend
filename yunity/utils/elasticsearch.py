83cb096fd9f87061173d462f6c8de04814157aef
Michael Dougherty <maackle.d@gmail.com>
2015-09-26 18:41:58 +0200

    this actually works

--- yunity/utils/elasticsearch.py
+++ yunity/utils/elasticsearch.py
@@ -8,12 +8,18 @@ def es_client(timeout=120):
     return Elasticsearch([{'host': settings.ES_HOST, 'timeout': timeout}])
 
 
-def create_index(es):
-    SHAREABLE_MAPPING = {}
+def drop_index(es):
     try:
         es.indices.delete(index=settings.ES_INDEX)
     except NotFoundError:
         pass
+
+
+def create_index(es):
+
+    SHAREABLE_MAPPING = {
+        "shareable": {}
+    }
 
     es.indices.create(index=settings.ES_INDEX, ignore=400)
 
@@ -23,5 +29,7 @@ def create_index(es):
         body=SHAREABLE_MAPPING
     )
 
+
 def rebuild_index(es):
+    drop_index(es)
     create_index(es)

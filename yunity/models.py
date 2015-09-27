09236672146d13fbd66b0c9964fe0881268b8ea6
Michael Dougherty <maackle.d@gmail.com>
2015-09-27 18:34:12 +0200

    basic models

--- yunity/models.py
+++ yunity/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class CreatedModified(models.Model):
+
+    created = models.DateTimeField(default=datetime.now)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    parent = models.ForeignKey(
+        'yunity.Category',
+        null=True,
+        related_name='children',
+    )
+
+
+class Shareable(CreatedModified):
+
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+

eccbafe05317565069a52c9741430af62841c3d0
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-09-29 10:49:04 +0200

    Implement to_dict via reflection

--- yunity/models/utils.py
+++ yunity/models/utils.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, Field
 
 
 class MaxLengthCharField(CharField):
@@ -11,8 +11,13 @@ class BaseModel(Model):
     class Meta:
         abstract = True
 
+    def _get_explicit_field_names(self):
+        return [field.name for field in self._meta.get_fields()
+                if isinstance(field, Field) and field.name != 'id']
+
     def to_dict(self):
-        return {}
+        fields = self._get_explicit_field_names()
+        return {field: getattr(self, field) for field in fields}
 
     def __repr__(self):
         return 'Model({})'.format(repr(self.to_dict()))

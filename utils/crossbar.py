69be5040313969c328f92c5106583c510da83ded
Matthias Larisch <mail@matthias-larisch.de>
2015-09-29 19:21:52 +0200

    Lookup user for crossbar authorization
    Crossbar authorization function decides based on topic name:
    yunity.public.* is allowed for everyone to subscribe
    yunity.user.<userid>.* is only allowed to subscribe, if the session
    belongs to the user named in the topic.
    Use the SharedSessionData to lookup the user for a session.

--- utils/crossbar.py
+++ utils/crossbar.py
@@ -1,8 +1,13 @@
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
+from utils.session import SharedSessionData
 
 
 class YunityAuthorizer(ApplicationSession):
+
+    def __init__(self):
+        self.s = SharedSessionData(False)
+
     @inlineCallbacks
     def onJoin(self, details):
         print("MyAuthorizer.onJoin({})".format(details))
@@ -13,4 +18,15 @@ class YunityAuthorizer(ApplicationSession):
             print("MyAuthorizer: failed to register authorizer procedure ({})".format(e))
 
     def authorize(self, session, uri, action):
-        return True
+        if action != 'subscribe':
+            return False
+        if uri.startswith('yunity.public.'):
+            return True
+        elif uri.startswith('yunity.user.'):
+            user = self.s.get_user_by_session(session)
+            if user is None:
+                return False
+            if uri.startswith('yunity.user.' + user + '.'):
+                return True
+
+        return False

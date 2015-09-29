6d7a84072f0086ce2afd99e7b68bbfb4fc527cbe
Michael Dougherty <maackle.d@gmail.com>
2015-09-29 18:35:30 +0200

    simple filter by geographic distance

--- yunity/views/search.py
+++ yunity/views/search.py
@@ -10,8 +10,32 @@ from yunity.utils.elasticsearch import es_search
 logger = logging.getLogger(__name__)
 
 
+def filter_es_geo_distance(esq, lat, lon, radius_km):
+    geo_params = {
+        'distance': "%skm" % radius_km,
+        'location': {
+            'lat': lat,
+            'lon': lon,
+        }
+    }
+    return esq.filter('geo_distance', **geo_params)
+
+
 class SearchMappableView(ApiBase, View):
 
     def get(self, request):
-        hits = [m.to_dict() for m in Mappable.es_search().execute().hits]
+        location_centroid_lat = request.GET.get('location_centroid_lat')
+        location_centroid_lon = request.GET.get('location_centroid_lon')
+        location_radius_km = request.GET.get('location_radius_km')
+
+        esq = Mappable.es_search()
+        if location_centroid_lat and location_centroid_lon and location_radius_km:
+            esq = filter_es_geo_distance(
+                esq,
+                location_centroid_lat,
+                location_centroid_lon,
+                location_radius_km
+            )
+
+        hits = [m.to_dict() for m in esq.execute().hits]
         return self.json_response(hits)

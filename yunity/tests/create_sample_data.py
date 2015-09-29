de4667c0915d3c1c9e3afec45e2aa08b552a9f92
Tilmann Becker <tilmann.becker@gmail.com>
2015-09-29 17:54:04 +0200

    changed Tilmann in testcase

--- yunity/tests/create_sample_data.py
+++ yunity/tests/create_sample_data.py
@@ -3,24 +3,24 @@
 Food store
 ========================
 
-Matthias and Tilman talked with Jon who is the manager of the Alnatura organic shop in Munich. The trio would like to
-start saving food from the Alnatura store, so Matthias and Tilman create an entry for the store and gather a team to
+Matthias and Tilmann talked with Jon who is the manager of the Alnatura organic shop in Munich. The trio would like to
+start saving food from the Alnatura store, so Matthias and Tilmann create an entry for the store and gather a team to
 organize the rescue: Neel and Flo will help with picking up food.
 As such, on the 15th of October at 5pm, Flo will be picking up some food from the store. Neel signed up for the pick-ups
 on the 13th and 14th but he hasn't been given the go-ahead yet. Unfortunately, Neel can't make the pick-up on the 12th
 for which he was originally down (he told the group as much in a message) and no one volunteered to take over his slot.
 If anyone has questions about the way that the group organizes pickups from the store, they decide that people should
-contact Tilman via direct message.
+contact Tilmann via direct message.
 
 
 Food basket
 ========================
 
-Tilman wants to get rid of 5.1 kg of "super tasty" bananas and bread. He advertises the basket with a picture.
+Tilmann wants to get rid of 5.1 kg of "super tasty" bananas and bread. He advertises the basket with a picture.
 Our bestower of tastiness is very busy, meaning that the food can only be picked up on October 15th 2015. The pick up
-can happen at Tilman's office in Munich (between 5pm and 6pm) or at his home between 7pm and 8pm.
-Neel and Matthias showed interest in picking up the food and Tilman chose to give the food to Neel. The pick up went
-seamlessly so Tilman left some positive feedback on Neel.
+can happen at Tilmann's office in Munich (between 5pm and 6pm) or at his home between 7pm and 8pm.
+Neel and Matthias showed interest in picking up the food and Tilmann chose to give the food to Neel. The pick up went
+seamlessly so Tilmann left some positive feedback on Neel.
 
 """
 from datetime import datetime
@@ -52,9 +52,9 @@ munich3 = Location.objects.create(latitude=48.161552, longitude=11.642)
 ##################################################
 # user
 ##################################################
-user_tilman = User.objects.create(name='Tilman')
-UserLocation.objects.create(user=user_tilman, location=munich2, type='UserLocation_type.HOME')
-UserLocation.objects.create(user=user_tilman, location=munich3, type='UserLocation_type.WORK')
+user_Tilmann = User.objects.create(name='Tilmann')
+UserLocation.objects.create(user=user_Tilmann, location=munich2, type='UserLocation_type.HOME')
+UserLocation.objects.create(user=user_Tilmann, location=munich3, type='UserLocation_type.WORK')
 
 user_matthias = User.objects.create(name='Matthias')
 user_neel = User.objects.create(name='Neel')
@@ -64,10 +64,10 @@ user_flo = User.objects.create(name='Flo')
 ##################################################
 # contact
 ##################################################
-contact_tilman_pm = Contact.objects.create(type=Contact.TYPE.DIRECT, value=user_tilman.id)
-contact_tilman_email = Contact.objects.create(type=Contact.TYPE.EMAIL, value='tilman@foodsharing.de')
-user_tilman.contact.add(contact_tilman_pm)
-user_tilman.contact.add(contact_tilman_email)
+contact_Tilmann_pm = Contact.objects.create(type=Contact.TYPE.DIRECT, value=user_Tilmann.id)
+contact_Tilmann_email = Contact.objects.create(type=Contact.TYPE.EMAIL, value='Tilmann@foodsharing.de')
+user_Tilmann.contact.add(contact_Tilmann_pm)
+user_Tilmann.contact.add(contact_Tilmann_email)
 
 
 ##################################################
@@ -81,8 +81,8 @@ metadata_basketweight = Metadata.objects.create(key='basketweight', value='5.1kg
 # message
 ##################################################
 message_neel_cantcome = Message.objects.create(type=Message.TYPE.TEXT, content="hey guys, i can't make the pickup today :(", sender=user_neel)
-message_tilman_basketdescription = Message.objects.create(type=Message.TYPE.TEXT, content='please pick up my super tasty stuff', sender=user_tilman)
-message_tilman_basketpicture = Message.objects.create(type=Message.TYPE.PICTURE, content='yunity.org/pics/mybasket.png', sender=user_tilman)
+message_Tilmann_basketdescription = Message.objects.create(type=Message.TYPE.TEXT, content='please pick up my super tasty stuff', sender=user_Tilmann)
+message_Tilmann_basketpicture = Message.objects.create(type=Message.TYPE.PICTURE, content='yunity.org/pics/mybasket.png', sender=user_Tilmann)
 
 
 ##################################################
@@ -93,10 +93,10 @@ foodsharing_store = Mappable.objects.create(provenance='yunity.org', name='alnat
 foodsharing_store.category.add(category_foodsharing_food_organic, category_foodsharing_company, category_foodsharing)
 foodsharing_store.metadata.add(metadata_companycontact)
 foodsharing_store.wall.add(message_neel_cantcome)
-foodsharing_store.contact.add(contact_tilman_pm)
+foodsharing_store.contact.add(contact_Tilmann_pm)
 MappableLocation.objects.create(mappable=foodsharing_store, location=munich1)
 
-MappableResponsibility.objects.create(responsible=user_tilman, mappable=foodsharing_store, status=MappableResponsibility.STATUS.GRANTED, type=MappableResponsibility.TYPE.OWNER)
+MappableResponsibility.objects.create(responsible=user_Tilmann, mappable=foodsharing_store, status=MappableResponsibility.STATUS.GRANTED, type=MappableResponsibility.TYPE.OWNER)
 MappableResponsibility.objects.create(responsible=user_matthias, mappable=foodsharing_store, status=MappableResponsibility.STATUS.GRANTED, type=MappableResponsibility.TYPE.OWNER)
 MappableResponsibility.objects.create(responsible=user_neel, mappable=foodsharing_store, status=MappableResponsibility.STATUS.GRANTED, type='MappableResponsibility_type.TEAM')
 MappableResponsibility.objects.create(responsible=user_flo, mappable=foodsharing_store, status=MappableResponsibility.STATUS.GRANTED, type='MappableResponsibility_type.TEAM')
@@ -113,12 +113,12 @@ MappableResponsibility.objects.create(responsible=None, mappable=foodsharing_sto
 foodsharing_basket = Mappable.objects.create(provenance='foodsharing.de', name='super tasty bananas and bread')
 foodsharing_basket.category.add(category_foodsharing_food_bread, category_foodsharing_food_banana, category_foodsharing_food_basket, category_foodsharing)
 foodsharing_basket.metadata.add(metadata_basketweight)
-foodsharing_basket.wall.add(message_tilman_basketdescription, message_tilman_basketpicture)
-foodsharing_basket.contact.add(contact_tilman_email)
+foodsharing_basket.wall.add(message_Tilmann_basketdescription, message_Tilmann_basketpicture)
+foodsharing_basket.contact.add(contact_Tilmann_email)
 MappableLocation.objects.create(mappable=foodsharing_basket, location=munich2, startTime=add_timezone(datetime.strptime('2015-10-15 17:00', '%Y-%m-%d %H:%M')), endTime=add_timezone(datetime.strptime('2015-10-15 18:00', '%Y-%m-%d %H:%M')))
 MappableLocation.objects.create(mappable=foodsharing_basket, location=munich3, startTime=add_timezone(datetime.strptime('2015-10-15 19:00', '%Y-%m-%d %H:%M')), endTime=add_timezone(datetime.strptime('2015-10-15 20:00', '%Y-%m-%d %H:%M')))
 
-MappableResponsibility.objects.create(responsible=user_tilman, mappable=foodsharing_basket, status=MappableResponsibility.STATUS.GRANTED, type=MappableResponsibility.TYPE.OWNER)
+MappableResponsibility.objects.create(responsible=user_Tilmann, mappable=foodsharing_basket, status=MappableResponsibility.STATUS.GRANTED, type=MappableResponsibility.TYPE.OWNER)
 
 ItemRequest.objects.create(requester=user_neel, requested=foodsharing_basket, feedback=ItemRequest.FEEDBACK.OK)
 ItemRequest.objects.create(requester=user_matthias, requested=foodsharing_basket, feedback=ItemRequest.FEEDBACK.NOT_GRANTED)

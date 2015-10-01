382af73c54a386e18b86385b31bde883b2094ba0
Michael Dougherty <maackle.d@gmail.com>
2015-10-01 11:27:15 +0200

    adds url for built-in django login view

--- yunity/urls.py
+++ yunity/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 import yunity.views.auth
 import yunity.views.chat
@@ -8,7 +9,7 @@ import yunity.views.search
 
 
 urlpatterns = [
-    url(r'^login/$', yunity.views.auth.LoginView.as_view()),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}),
     url(r'^register/$', yunity.views.auth.RegisterView.as_view()),
     url(r'^mappables/new/$', yunity.views.mappable.CreateMappableView.as_view()),
     url(r'^mappables/(?P<mappable_id>[0-9]+)$', yunity.views.mappable.GetMappableView.as_view()),

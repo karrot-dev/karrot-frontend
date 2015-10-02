7253e33281474c7655476a0a3a8399787bf1bae1
Tilmann Becker <tilmann.becker@gmail.com>
2015-10-02 16:48:45 +0200

    added API skeleton - still v0.1

--- yunity/urls.py
+++ yunity/urls.py
@@ -3,10 +3,21 @@ from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
 import yunity.views.auth
+from yunity.views.rest.auth import AuthViewSet
+from yunity.views.rest.category import CategoryViewSet
 from yunity.views.rest.chat import ChatViewSet
+from yunity.views.rest.feedback import FeedbackViewSet
+from yunity.views.rest.mapitem import MapitemViewSet
+from yunity.views.rest.search import SearchViewSet
 
 router = DefaultRouter()
 router.register(r'chat', ChatViewSet)
+router.register(r'user', FeedbackViewSet)
+router.register(r'auth', AuthViewSet)
+router.register(r'mapitem', MapitemViewSet)
+router.register(r'search', SearchViewSet)
+router.register(r'feedback', FeedbackViewSet)
+router.register(r'category', CategoryViewSet)
 
 
 urlpatterns = [
--- yunity/views/rest/category.py
+++ yunity/views/rest/category.py
@@ -1,40 +1,41 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from yunity.models import Feedback
+from yunity.models import Category
 
 
-class FeedbackViewSet(viewsets.ViewSet):
+class CategoryViewSet(viewsets.ViewSet):
     """
-    Viewset to handle feedback about mapitem
     """
-    queryset = Feedback.objects.all()
-
+    queryset = Category.objects.all()
+    
     def create(self, request):
         """
-        Create new feedback about mapitem
-
-        {mapitemid, type, status, text}
         """
         return Response(status=status.HTTP_201_CREATED)
 
+    def list(self):
+        """
+        """
+        return Response(status=status.HTTP_200_OK)
+
     def retrieve(self, request, pk=None):
         """
-        Get Feedback for Mapitem pk
         """
         return Response(status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
         """
-        Modify Feedback status/type or add arbitrator of mapitem pk
         """
         return Response(status=status.HTTP_200_OK)
+
+    def destroy(self, request, pk=None):
+        """
+        """
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     @detail_route(methods=["POST"])
     def arbitrationlog(self, request, pk=None):
         """
-        Add entry to arbitrationlog
-
-        {sender, type, content, replyTo}
         """
         return Response(status=status.HTTP_201_CREATED)

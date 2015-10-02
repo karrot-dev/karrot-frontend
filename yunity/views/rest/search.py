5cf6043bf19e441b7bcb276ca254610543669fe6
Tilmann Becker <tilmann.becker@gmail.com>
2015-10-02 17:33:18 +0200

    search docstring

--- yunity/views/rest/search.py
+++ yunity/views/rest/search.py
@@ -1,41 +1,27 @@
 from rest_framework import viewsets, status
-from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from yunity.models import MapItem
 
 
 class SearchViewSet(viewsets.ViewSet):
     """
+    Fulltext search in mapitems
+
+    get results for fulltext search. if parameters are set, don't return results without these attributes
+
+    :param center: longitude,latitude
+
+    :param radius: maximum distance of results
+
+    :param categories: restrict results to categories (OR combined)
+
+    :param metadata: key=value pairs to query metadata (as a string to support and/or combinations)
+
+    results with location may be sorted by distance to center
+
+    result json: {[title, lat/lon, text, category, description, [metadata]}
     """
     queryset = MapItem.objects.all()
 
-    def create(self, request):
-        """
-        """
-        return Response(status=status.HTTP_201_CREATED)
-
-    def list(self):
-        """
-        """
-        return Response(status=status.HTTP_200_OK)
-
     def retrieve(self, request, pk=None):
-        """
-        """
         return Response(status=status.HTTP_200_OK)
-
-    def update(self, request, pk=None):
-        """
-        """
-        return Response(status=status.HTTP_200_OK)
-
-    def destroy(self, request, pk=None):
-        """
-        """
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @detail_route(methods=["POST"])
-    def arbitrationlog(self, request, pk=None):
-        """
-        """
-        return Response(status=status.HTTP_201_CREATED)

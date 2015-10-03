e36d8af1a9fac52947287c1015fbbc7ae6ad9bb0
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-03 19:11:56 +0200

    Remove unused import

--- yunity/utils/status.py
+++ yunity/utils/status.py
@@ -4,7 +4,6 @@ Descriptive HTTP status codes, for code readability.
 See RFC 2616 - http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 And RFC 6585 - http://tools.ietf.org/html/rfc6585
 """
-from __future__ import unicode_literals
 
 
 def is_informational(code):

88f741f407f6a92dfa345bc9885992e9f9b41869
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-03 18:57:07 +0200

    Implement GET and POST on /api/categories
    - GET lists all categories
    - POST creates a new category
    
    with @NerdyProjects

--- yunity/api/public/categories.py
+++ yunity/api/public/categories.py
@@ -2,23 +2,59 @@ from django.conf.urls import url
 from django.http import HttpRequest
 from django.views.generic import View
 
-from yunity.api.utils import ApiBase
+from yunity.api.utils import ApiBase, json_request
+from yunity.models import Category
 
 
 class Categories(ApiBase, View):
+    @classmethod
+    def category_to_json(cls, category):
+        return cls.model_to_json(category, 'name', 'id', 'parent')
+
     def get(self, request):
-        """list all categories
+        """List all categories.
+
+        response_json:
+            categories:
+                type: list
+                description: a list of {'name', 'id', 'parent'} objects describing all the categories
 
         :type request: HttpRequest
+        :rtype JsonResponse
         """
-        raise NotImplementedError
+        categories = [self.category_to_json(category) for category in Category.objects.all()]
 
-    def post(self, request):
-        """create a new category
+        return self.success({'categories': categories})
 
+    @json_request(expected_keys=['name'])
+    def post(self, data, request):
+        """Creates a new category.
+
+        request_json:
+            name:
+                type: string
+                required: true
+                description: the name of the category to create
+            parent:
+                type: integer
+                description: the id of the category under which to nest the new category
+
+        response_json:
+            id:
+                type: integer
+                description: the id of the newly created category
+
+        :type data: dict
         :type request: HttpRequest
+        :rtype JsonResponse
         """
-        raise NotImplementedError
+        name = data['name']
+        parent = data.get('parent')
+        if parent is not None:
+            parent = Category.objects.get(id=parent)
+
+        category = Category.objects.create(name=name, parent=parent)
+        return self.success({'id': category.id})
 
 
 urlpatterns = [

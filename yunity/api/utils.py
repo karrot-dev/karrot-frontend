a15d2956cfd48e0d46d5d4cf567af05641b4c8e6
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-03 18:54:32 +0200

    Implement JSON request validation decorator
    with @NerdyProjects

--- yunity/api/utils.py
+++ yunity/api/utils.py
@@ -1,7 +1,20 @@
+from functools import wraps
+from json import loads as load_json
+
 from django.http import JsonResponse
 
 
 class ApiBase(object):
+    @classmethod
+    def validation_failure(cls, message, status=400):
+        """
+        :type message: str
+        :type status: int
+        :rtype JsonResponse
+
+        """
+        return JsonResponse({'validation_failure': message}, status=status)
+
     @classmethod
     def success(cls, data, status=200):
         """
@@ -21,3 +34,25 @@ class ApiBase(object):
 
         """
         return JsonResponse({'error': error}, status=status)
+
+
+def json_request(expected_keys=None):
+    """Decorator to validate that a request is in JSON and (optionally) has some specific keys in the JSON object.
+
+    """
+    expected_keys = expected_keys or []
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(cls, request, *args, **kwargs):
+            data = load_json(request.body.decode('utf8'))
+
+            for expected_key in expected_keys:
+                value = data.get(expected_key)
+                if not value:
+                    return ApiBase.validation_failure('missing key: {}'.format(expected_key))
+
+            return func(cls, data, request, *args, **kwargs)
+
+        return wrapper
+    return decorator

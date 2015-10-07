3a7b88092a20d08e92938628684abf7dcdbd58ec
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-07 10:45:40 +0200

    Move user api tests to integration test framework

---
+++ yunity/tests/integration/resources/test_user__create_user_creates_user/final_data.py
@@ -0,0 +1,12 @@
+from yunity.models import User
+
+created_user = User.objects.filter(email='foodsharing-raphi@gmail.com')
+assert len(created_user) == 1, 'user was not created'
+created_user = created_user[0]
+
+latitude = created_user.locations[0]['latitude']
+longitude = created_user.locations[0]['longitude']
+assert (latitude, longitude) == (12.345, 67.890), 'latitude and longitude not matching'
+
+display_name = created_user.display_name
+assert display_name == 'Foodsharing Raphi', 'display name does not match'

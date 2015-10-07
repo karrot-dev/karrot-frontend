c1db823f2eaefae7d2db83484a646faefdad7f67
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-07 11:38:23 +0200

    Fix missed out adding of message key

--- yunity/utils/api.py
+++ yunity/utils/api.py
@@ -107,7 +107,7 @@ def body_as_json(expected_keys=None):
             try:
                 json_request = JsonRequest.from_http_request(request, expected_keys)
             except ValueError as e:
-                return api_base.validation_failure(str(e))
+                return api_base.validation_failure({'message': str(e)})
 
             return func(api_base, json_request, *args, **kwargs)
         return wrapper
@@ -132,7 +132,7 @@ def resource_as_list(param_name, item_type=str, delim=ids_uri_pattern_delim):
                     try:
                         parsed_param = item_type(raw_param)
                     except ValueError:
-                        return api_base.validation_failure('invalid type: {}'.format(raw_param))
+                        return api_base.validation_failure({'message': 'invalid type: {}'.format(raw_param)})
                     else:
                         parsed_params.append(parsed_param)
                 kwargs[param_name] = parsed_params

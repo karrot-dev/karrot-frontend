07509f647b596e0bbec49da031964f01b45bccf1
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-08 13:12:09 +0200

    Consistently set up users list in initial_data

--- yunity/resources/tests/integration/test_user__create_user_with_existing_mail_fails/initial_data.py
+++ yunity/resources/tests/integration/test_user__create_user_with_existing_mail_fails/initial_data.py
@@ -1,4 +1,5 @@
 from yunity.utils.factories import UserFactory
 
-UserFactory.create(email="testuser1234@mail.com",
-                   display_name="I am the existing user",)
+users = [
+    UserFactory.create(email="testuser1234@mail.com", display_name="I am the existing user"),
+]

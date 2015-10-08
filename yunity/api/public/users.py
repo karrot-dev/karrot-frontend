53810f7d60b89ed258cf5cc453a3bb342537348a
Tilmann Becker <tilmann.becker@gmail.com>
2015-10-08 12:27:52 +0200

    Catch error if user email already exists

--- yunity/api/public/users.py
+++ yunity/api/public/users.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError, transaction
 from django.http import HttpRequest
 from django.views.generic import View
 
@@ -65,14 +66,17 @@ class UserAll(ApiBase, View):
             }]
         except KeyError:
             locations = []
-
-        user = get_user_model().objects.create_user(
-            email=request.body['email'],
-            password=request.body['password'],
-            locations=locations,
-            type=category,
-            display_name=request.body['display_name'],
-        )
+        try:
+            with transaction.atomic():
+                user = get_user_model().objects.create_user(
+                    email=request.body['email'],
+                    password=request.body['password'],
+                    locations=locations,
+                    type=category,
+                    display_name=request.body['display_name'],
+                )
+        except IntegrityError as e:
+            return self.error(reason=str(e), status=status.HTTP_409_CONFLICT)
 
         return self.created({"id": user.id,
                       "display_name": user.display_name})

00bc702a3134094f7a3cf0f730fd5a9f69532331
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-09 12:41:43 +0200

    Split yunity.utils.tests into submodules

---
+++ yunity/tests/unit/test__utils_tests__misc.py
@@ -0,0 +1,27 @@
+from sys import modules
+from yunity.utils.tests.abc import BaseTestCase
+from yunity.utils.tests.misc import json_stringify, maybe_import
+
+
+class JsonStringifyTestCase(BaseTestCase):
+    def test_json_stringify_creates_json_from_dict(self):
+        self.given_data({'a': 1, 'b': [2, 3], 'c': {'d': 4}})
+        self.when_calling(json_stringify)
+        self.then_invocation_passed_with(result=b'{"a":1,"b":[2,3],"c":{"d":4}}')
+
+    def test_json_stringify_creates_none_from_none(self):
+        self.given_data(None)
+        self.when_calling(json_stringify)
+        self.then_invocation_passed_with(result=None)
+
+
+class MaybeImportTestCase(BaseTestCase):
+    def test_maybe_import_imports_existing_module(self):
+        self.given_data(__name__)
+        self.when_calling(maybe_import)
+        self.then_invocation_passed_with(result=modules[__name__])
+
+    def test_maybe_import_does_not_import_missing_module(self):
+        self.given_data('this.module.does.not.exist')
+        self.when_calling(maybe_import)
+        self.then_invocation_passed_with(result=None)

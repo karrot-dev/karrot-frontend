8837c87977d3239f52b55395f7042a540732a838
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-09 12:13:47 +0200

    Make integerid creation functions public

--- yunity/api/ids.py
+++ yunity/api/ids.py
@@ -1,7 +1,7 @@
 ids_uri_pattern_delim = ','
 
 
-def _multiple_integerid(name, minlength=1, maxlength=10, minrepetitions=1, maxrepetitions=200, delim=ids_uri_pattern_delim):
+def multiple_integerids(name, minlength=1, maxlength=10, minrepetitions=1, maxrepetitions=200, delim=ids_uri_pattern_delim):
     """
     :type name: str
     :type minlength: int
@@ -10,7 +10,7 @@ def _multiple_integerid(name, minlength=1, maxlength=10, minrepetitions=1, maxre
     :type maxrepetitions: int
     :rtype str
 
-    >>> _multiple_integerid(name='foo_list', minlength=1, maxlength=10, minrepetitions=1, maxrepetitions=3)
+    >>> multiple_integerids(name='foo_list', minlength=1, maxlength=10, minrepetitions=1, maxrepetitions=3)
     '(?P<foo_list>[0-9]{1,10}(,[0-9]{1,10}){0,2})'
 
     """
@@ -19,17 +19,17 @@ def _multiple_integerid(name, minlength=1, maxlength=10, minrepetitions=1, maxre
     return '(?P<{name}>{integerid_list})'.format(name=name, integerid_list=integerid_list)
 
 
-def _integerid(name, minlength=1, maxlength=10):
-    return _multiple_integerid(name, minlength, maxlength, minrepetitions=1, maxrepetitions=1)
+def single_integerid(name, minlength=1, maxlength=10):
+    return multiple_integerids(name, minlength, maxlength, minrepetitions=1, maxrepetitions=1)
 
 
-category_ids_uri_pattern = _multiple_integerid('categoryids')
-chat_id_uri_pattern = _integerid('chatid')
-feedback_id_uri_pattern = _integerid('feedbackid')
-opportunity_id_uri_pattern = _integerid('opportunityid')
-participate_id_uri_pattern = _integerid('participateid')
-take_id_uri_pattern = _integerid('takeid')
-user_id_uri_pattern = _integerid('userid')
-multiple_user_id_uri_pattern = _multiple_integerid('userids')
-valuable_id_uri_pattern = _integerid('valuableid')
-wallpost_id_uri_pattern = _integerid('wallpostid')
+category_ids_uri_pattern = multiple_integerids('categoryids')
+chat_id_uri_pattern = single_integerid('chatid')
+feedback_id_uri_pattern = single_integerid('feedbackid')
+opportunity_id_uri_pattern = single_integerid('opportunityid')
+participate_id_uri_pattern = single_integerid('participateid')
+take_id_uri_pattern = single_integerid('takeid')
+user_id_uri_pattern = single_integerid('userid')
+multiple_user_id_uri_pattern = multiple_integerids('userids')
+valuable_id_uri_pattern = single_integerid('valuableid')
+wallpost_id_uri_pattern = single_integerid('wallpostid')

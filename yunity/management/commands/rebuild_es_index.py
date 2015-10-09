99190fba7ac9f497d3abee205c4040e2ce8092e8
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-09 16:07:31 +0200

    Resurect rebuild_es_index command

---
+++ yunity/management/commands/rebuild_es_index.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from yunity.utils.elasticsearch import es_client, rebuild_index
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list
+
+    help = (
+        'drops and rebuilds the Elasticsearch index from Postgres, check cache'
+        ' keys `rebuild_index_canon` and `rebuild_index_bs` for progress'
+    )
+
+    def handle(self, *args, **options):
+        es = es_client()
+        rebuild_index(es)
+        self.stdout.write('Index rebuilt!', ending='\n')

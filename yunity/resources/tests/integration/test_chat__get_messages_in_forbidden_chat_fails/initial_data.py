9a2a7afef649e7749f27676cde5fe9341afc359c
Tilmann Becker <tilmann.becker@gmail.com>
2015-10-09 17:22:20 +0200

    Add test for Chat API - access messages in forbidden chat fails
    with @NerdyProjects

---
+++ yunity/resources/tests/integration/test_chat__get_messages_in_forbidden_chat_fails/initial_data.py
@@ -0,0 +1,25 @@
+from yunity.models import Chat, Message
+from yunity.utils.tests.mock import MockUser
+
+users = MockUser.create_batch(3)
+request_user = users[2]
+
+chat = Chat.objects.create()
+chatid = chat.id
+
+Message.objects.create(
+    content="Hello Matthias!",
+    type="TEXT",
+    sent_by=users[0],
+    in_conversation=chat
+)
+
+Message.objects.create(
+    content="Hi Tilmann! How are you?",
+    type="TEXT",
+    sent_by=users[1],
+    in_conversation=chat
+)
+
+
+

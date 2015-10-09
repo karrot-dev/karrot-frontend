029138ca9c8590244744fe8dbf2f9809c6b35256
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-09 11:28:39 +0200

    Make AbstractvalidationTestCase more general
    The class can be re-used for all sorts of given/when/then tests really!

--- yunity/tests/test__utils__validation.py
+++ yunity/tests/test__utils__validation.py
@@ -1,79 +1,79 @@
-from yunity.utils.test import AbstractValidationTestCase
+from yunity.utils.test import BaseTestCase
 from yunity.utils.validation import ShorterThan, HasKey, IsIn, OfType, Each, AndThen
 
 
-class TestValidationUtil(AbstractValidationTestCase):
+class TestValidationUtil(BaseTestCase):
     def test_shorter_than_rejects_item_without_length(self):
         self.given_data(123)
         self.when_calling(ShorterThan(100))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_shorter_than_rejects_long_item(self):
         self.given_data('123')
         self.when_calling(ShorterThan(2))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_shorter_than_accepts_short_item(self):
         self.given_data('123')
         self.when_calling(ShorterThan(5))
-        self.then_validation_passed()
+        self.then_invocation_passed_with_any_result()
 
     def test_has_key_rejects_item_without_keys(self):
         self.given_data('123')
         self.when_calling(HasKey('thekey'))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_has_key_rejects_item_with_missing_key(self):
         self.given_data({'foo': 123})
         self.when_calling(HasKey('thekey'))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_has_key_accepts_item_with_key(self):
         self.given_data({'thekey': 123})
         self.when_calling(HasKey('thekey'))
-        self.then_validation_passed()
+        self.then_invocation_passed_with_any_result()
 
     def test_is_in_rejects_not_contained_item(self):
         self.given_data('notfound')
         self.when_calling(IsIn('this', 'and', 'that'))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_is_in_accepts_contained_item(self):
         self.given_data('that')
         self.when_calling(IsIn('this', 'and', 'that'))
-        self.then_validation_passed()
+        self.then_invocation_passed_with_any_result()
 
     def test_of_type_rejects_item_of_wrong_type(self):
         self.given_data({'my': 'dict'})
         self.when_calling(OfType(int))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_of_type_accepts_item_of_same_type(self):
         self.given_data({'my': 'dict'})
         self.when_calling(OfType(dict))
-        self.then_validation_passed()
+        self.then_invocation_passed_with_any_result()
 
     def test_each_rejects_item_that_is_not_iterable(self):
         self.given_data(123)
         self.when_calling(Each(OfType(int)))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_each_rejects_item_with_members_that_do_not_validate(self):
         self.given_data([1, 2, 'mixed', 'types', {}, 'yay'])
         self.when_calling(Each(OfType(int)))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_each_accepts_item_with_members_that_validate(self):
         self.given_data([1, 2, 3])
         self.when_calling(Each(OfType(int)))
-        self.then_validation_passed()
+        self.then_invocation_passed_with_any_result()
 
     def test_and_then_rejects_sequence_that_does_not_validate(self):
         self.given_data('without key')
         self.when_calling(AndThen(HasKey('thekey'), OfType(int)))
-        self.then_validation_failed()
+        self.then_invocation_failed_with(ValueError)
 
     def test_and_then_accepts_sequence_that_does_validate(self):
         self.given_data({'thekey': 1})
         self.when_calling(AndThen(HasKey('thekey'), OfType(int)))
-        self.then_validation_passed()
+        self.then_invocation_passed_with_any_result()
--- yunity/utils/test.py
+++ yunity/utils/test.py
@@ -72,24 +72,37 @@ def maybe_import(resource):
         pass
 
 
-class AbstractValidationTestCase(TestCase):
+class BaseTestCase(TestCase):
     def setUp(self):
-        self.data = None
+        self.args = []
+        self.kwargs = {}
         self.result = None
         self.exception = None
 
-    def given_data(self, data):
-        self.data = data
+    def given_data(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
 
     def when_calling(self, function):
         try:
-            self.result = function(self.data)
+            self.result = function(*self.args, **self.kwargs)
         except Exception as e:
             self.exception = e
 
-    def then_validation_failed(self):
-        self.assertIsInstance(self.exception, ValueError)
+    def then_invocation_failed_with(self, exception):
+        self.assertIsInstance(self.exception, exception)
 
-    def then_validation_passed(self):
-        self.assertIsNotNone(self.result, 'did not get a result')
+    def then_there_was_no_exception(self):
         self.assertIsNone(self.exception, 'got an unexpected exception')
+
+    def then_invocation_passed_with_no_result(self):
+        self.then_there_was_no_exception()
+        self.assertIsNone(self.result, 'got an unexpected result')
+
+    def then_invocation_passed_with_any_result(self):
+        self.then_there_was_no_exception()
+        self.assertIsNotNone(self.result, 'did not get a result')
+
+    def then_invocation_passed_with(self, result):
+        self.then_there_was_no_exception()
+        self.assertEqual(self.result, result, 'results do not match')

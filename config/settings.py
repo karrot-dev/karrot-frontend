bf82f734691da4a90e0ff069b9feaa1d2b7b4013
Nick Sellen <git@nicksellen.co.uk>
2015-10-10 10:33:10 +0200

    Use redis db 0 for django cache/sessions

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = []
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }

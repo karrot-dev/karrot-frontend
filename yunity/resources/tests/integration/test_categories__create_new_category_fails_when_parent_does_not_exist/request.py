fd4e83e1420ef3c01e76f4c6409af8b1c86fdd75
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-10 11:27:44 +0200

    Add test: fail category creation if no parent
    with @NerdyProjects

---
+++ yunity/resources/tests/integration/test_categories__create_new_category_fails_when_parent_does_not_exist/request.py
@@ -0,0 +1,8 @@
+request = {
+    "endpoint": "/api/categories/",
+    "method": "post",
+    "body": {
+        "name": "some name",
+        "parent": 4513268127419,  # a clearly non-existing parent id
+    }
+}

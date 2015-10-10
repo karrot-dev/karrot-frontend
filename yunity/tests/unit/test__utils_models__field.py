34aafdcf1e2a0f607050e9e5bfddea8dfbc63e6f
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-10 17:38:27 +0200

    Add test for MaxLengthCharField

---
+++ yunity/tests/unit/test__utils_models__field.py
@@ -0,0 +1,7 @@
+from yunity.utils.models.field import MaxLengthCharField
+from yunity.utils.tests.abc import BaseTestCase
+
+
+class MaxLengthCharFieldTestCase(BaseTestCase):
+    def test_max_length_char_field_always_has_max_length(self):
+        self.assertEqual(MaxLengthCharField().max_length, 255)

77dc21dd8a14881c9baef835ce4960c5872dfaa3
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-10 16:59:56 +0200

    Remove some unused functions

--- yunity/utils/status.py
+++ yunity/utils/status.py
@@ -6,26 +6,6 @@ And RFC 6585 - http://tools.ietf.org/html/rfc6585
 """
 
 
-def is_informational(code):
-    return 100 <= code <= 199
-
-
-def is_success(code):
-    return 200 <= code <= 299
-
-
-def is_redirect(code):
-    return 300 <= code <= 399
-
-
-def is_client_error(code):
-    return 400 <= code <= 499
-
-
-def is_server_error(code):
-    return 500 <= code <= 599
-
-
 HTTP_100_CONTINUE = 100
 HTTP_101_SWITCHING_PROTOCOLS = 101
 HTTP_200_OK = 200

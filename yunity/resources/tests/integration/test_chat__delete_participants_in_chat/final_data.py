7901ea10af343861492504dc31b9e3dbdd581b0a
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-12 13:54:02 +0200

    Fix test setup and make it more robust
    - use ids in resources, not objects
    - reference objects by id, not ordering

--- yunity/resources/tests/integration/test_chat__delete_participants_in_chat/final_data.py
+++ yunity/resources/tests/integration/test_chat__delete_participants_in_chat/final_data.py
@@ -1,4 +1,5 @@
 from yunity.models import Chat
+from .initial_data import chatid
 
-chat = Chat.objects.first()
+chat = Chat.objects.get(id=chatid)
 assert chat.participants.count() == 2, "Participant was not deleted"

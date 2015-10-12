c07ea34e5c33d91ebd1b61596886daff0b398c26
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-10-12 14:59:23 +0200

    Parse empty response as empty json response
    The Django middleware "fixes" responses passing an empty dict as data an
    empty string response.

--- yunity/tests/unit/test__utils__response.py
+++ yunity/tests/unit/test__utils__response.py
@@ -11,6 +11,11 @@ class ContentJsonTestCase(BaseTestCase):
         self.when_calling(content_json)
         self.then_invocation_passed_with(result={"foo": 1})
 
+    def test_content_json_returns_empty_for_empty_content(self):
+        self.given_raw_response(content="")
+        self.when_calling(content_json)
+        self.then_invocation_passed_with(result={})
+
     def test_content_json_fails_when_response_content_is_not_valid_json(self):
         self.given_raw_response(content='not a json response')
         self.when_calling(content_json)
--- yunity/utils/response.py
+++ yunity/utils/response.py
@@ -6,7 +6,11 @@ def content_json(response):
     :type response: HttpResponse
     :rtype: dict
     """
+    content = response.content.decode("utf-8")
+    if not content:
+        return {}
+
     try:
-        return load_json(response.content.decode("utf-8"))
+        return load_json(content)
     except ValueError:
         raise ValueError('invalid json content in response')

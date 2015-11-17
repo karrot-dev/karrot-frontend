8024966753e6c2828f834eee33126f984dcb0957
Matthias Larisch <mail@matthias-larisch.de>
2015-11-17 13:39:54 +0100

    Add API endpoint to create private 1-on-1 conversations
    A separate endpoint at POST /user/id/chat to get or create a 1-on-1 chat
    between the logged in and the given user.

--- yunity/api/public/users.py
+++ yunity/api/public/users.py
@@ -1,15 +1,17 @@
 from django.conf.urls import url
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
+from django.db.models import Count
 from django.http import HttpRequest
 from django.views.generic import View
 
 from yunity.api.ids import user_id_uri_pattern, multiple_user_id_uri_pattern
 from yunity.api import types, serializers
+from yunity.models import Conversation as ConversationModel
 from yunity.resources.http.status import HTTP_409_CONFLICT
 from yunity.utils.api.abc import ApiBase
 from yunity.utils.api.decorators import json_request, request_parameter, uri_resource, permissions_required_for, \
-    rollback_on
+    rollback_on, login_required
 
 
 class Users(ApiBase, View):
@@ -185,7 +187,47 @@ class User(ApiBase, View):
         return self.created(serializers.user(users))
 
 
+class UserChat(ApiBase, View):
+    @uri_resource('user', of_type=get_user_model())
+    @login_required
+    def post(self, request, user):
+        """get the chat between the logged in and the given user.
+        ---
+        tags:
+            - Chat
+        parameters:
+            - in: path
+              name: user
+              type: integer
+
+        responses:
+            201:
+                description: Chat information
+                schema:
+                    type: object
+                    properties:
+                      chat:
+                        $ref: '#/definitions/chat_information'
+            404:
+                description: The user does not exist
+        ...
+
+        :type request: HttpRequest
+        :type user: [UserModel]
+        """
+
+        participants = [request.user.id, user.id]
+        chat = ConversationModel.objects.filter(participants__id__in=participants).annotate(c=Count('participants')).filter(c=2)
+        if not chat:
+            chat = ConversationModel.objects.create()
+            chat.participants = participants
+            chat.save()
+
+        return self.created({"chat": serializers.conversation(chat)})
+
+
 urlpatterns = [
     url(r'^$', Users.as_view()),
     url(r'^{users}/?$'.format(users=multiple_user_id_uri_pattern), User.as_view()),
+    url(r'^{user}/chat/?$'.format(user=user_id_uri_pattern), UserChat.as_view()),
 ]

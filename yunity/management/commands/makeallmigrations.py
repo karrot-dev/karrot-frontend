8774f9a646c7f5f4e211b3b760dd88b440672310
Nick Sellen <git@nicksellen.co.uk>
2016-01-17 18:59:04 +0100

    Added in migration options to makeallmigrations

--- yunity/management/commands/makeallmigrations.py
+++ yunity/management/commands/makeallmigrations.py
@@ -10,6 +10,21 @@ from config import settings
 class Command(BaseCommand):
     help = 'Make all migrations'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--dry-run', action='store_true', dest='dry_run', default=False,
+            help="Just show what migrations would be made; don't actually write them.")
+        parser.add_argument('--merge', action='store_true', dest='merge', default=False,
+            help="Enable fixing of migration conflicts.")
+        parser.add_argument('--empty', action='store_true', dest='empty', default=False,
+            help="Create an empty migration.")
+        parser.add_argument('--noinput', '--no-input',
+            action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.')
+        parser.add_argument('-n', '--name', action='store', dest='name', default=None,
+            help="Use this name for migration file(s).")
+        parser.add_argument('-e', '--exit', action='store_true', dest='exit_code', default=False,
+            help='Exit with error code 1 if no changes needing migrations are found.')
+
     def handle(self, *args, **options):
         app_labels = []
         for app_name in settings.INSTALLED_APPS:
@@ -17,6 +32,6 @@ class Command(BaseCommand):
             if m:
                 app_labels.append(m.group(1))
         if len(app_labels) > 0:
-            MakeMigrationsCommand().handle(*app_labels, verbosity=1, interactive=False)
+            MakeMigrationsCommand().handle(*app_labels, **options)
         else:
             print('no apps matched yunity.*')

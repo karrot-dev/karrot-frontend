65378ef52058ba0c4518d7c7432fa550336038fc
Matthias Larisch <mail@matthias-larisch.de>
2016-01-18 13:25:39 +0100

    Implement static and group permissions in model
    Allow storing static (registered user, public) permissions in model.
    Add a GroupTreePermission model to store permissions that apply to
    everybody in a group of that subtree.
    
    with @nicksellen

--- yunity/permissions/models.py
+++ yunity/permissions/models.py
@@ -2,8 +2,10 @@ from config import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import ForeignKey, CASCADE, PositiveIntegerField, TextField
+from django_enumfield import enum
 from yunity.base.hub_models import Hub
 from yunity.base.models import BaseModel
+from yunity.base.other_models import Group
 
 
 class PermissionMixin():
@@ -20,6 +22,18 @@ class Permission(BaseModel):
     target = GenericForeignKey('target_content_type', 'target_id')
 
     action = TextField()
+
+class ConstantPermissionType(enum.Enum):
+    PUBLIC = 0
+    REGISTERED_USERS = 1
+
+
+class ConstantPermission(Permission):
+    type = enum.EnumField(ConstantPermissionType)
+
+
+class GroupTreePermission(Permission):
+    group = ForeignKey(Group, on_delete=CASCADE)
 
 
 class HubPermission(Permission):

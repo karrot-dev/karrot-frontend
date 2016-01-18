cd3ce79dc666f0973f26c53a622ed9ed798f2211
Matthias Larisch <mail@matthias-larisch.de>
2016-01-18 11:17:14 +0100

    Implement group hierarchy lookups
    groups now know about their
    - parent
    - root node
    - children
    - recursive children
    
    with @nicksellen

--- yunity/base/other_models.py
+++ yunity/base/other_models.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.db.models import ForeignKey, TextField, FloatField, ManyToManyField, BooleanField, CASCADE
 
 from yunity.base.hub_models import Hub, HubMixin
@@ -12,9 +14,22 @@ class Item(BaseModel):
 
 
 class Group(BaseModel, HubMixin):
+    @classmethod
+    def all_children_of(cls, *groups):
+        return list(chain.from_iterable([c.all_children() for c in groups]))
+
+    def parents(self):
+        return [self.parent] + self.parent.parents() if self.parent else []
+
+    def root(self):
+        return self.parent.root() if self.parent else self
+
+    def all_children(self):
+        return self.__class__.all_children_of(*self.children)
+
     name = MaxLengthCharField()
     description = TextField(null=True)
-    parent = ForeignKey('base.Group', null=True, on_delete=CASCADE)
+    parent = ForeignKey('base.Group', null=True, on_delete=CASCADE, related_name='children')
 
     is_content_included_in_parent = BooleanField(default=False)
 

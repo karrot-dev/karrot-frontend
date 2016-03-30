54c8ce65d7a4dd401801c5e450caf7b5c521a263
Matthias Larisch <mail@matthias-larisch.de>
2016-03-30 18:22:35 +0200

    Use GenericViewSet to fix doc
    django-rest-swagger extracts serializers from helpers that are added in
    the GenericViewSet

--- yunity/api/public/users.py
+++ yunity/api/public/users.py
@@ -11,12 +11,14 @@ class UsersViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class UserChatViewSet(viewsets.ViewSet):
+class UserChatViewSet(viewsets.GenericViewSet):
     serializer_class = ConversationSerializer
 
     def list(self, request, user_pk=None):
+        """ Gets an existing 1:1 conversation between the logged in user and the given user.
+        If there is no existing conversation, none is returned. It can be created using the /chat/ endpoint."""
         participants = [request.user.id, user_pk]
         queryset = ConversationModel.objects.filter(type=ConversationType.ONE_ON_ONE).filter(
             participants__id__in=participants).annotate(c=Count('participants')).filter(c=2).first()
-        serializer = ConversationSerializer(queryset)
+        serializer = self.get_serializer(queryset)
         return Response(serializer.data)

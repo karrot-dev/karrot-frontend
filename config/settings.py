4809c82d19f0847fbd711d09df3116dd03747889
Tilmann Becker <tilmann.becker@gmail.com>
2016-05-29 22:20:16 +0200

    Temporarily set default permissions class to "AllowAny"

--- config/settings.py
+++ config/settings.py
@@ -61,17 +61,24 @@ INSTALLED_APPS = (
     'yunity.groups',
 )
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
 MIDDLEWARE_CLASSES = (
     'yunity.utils.session.RealtimeClientMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
 )
 
 ROOT_URLCONF = 'config.urls'

44adf9c3d3be014936d6db2e6fb2fc55d82ca82a
Nick Sellen <git@nicksellen.co.uk>
2016-06-11 18:44:19 +0200

    Disabled tests while getting deployment to work

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@
 set -x
 set -e
 
+echo "------------------------------------"
+env
+echo "------------------------------------"
+
 scp deploy/remote.sh deploy@yuca.yunity.org:
 ssh deploy@yuca.yunity.org sh deploy.remote.sh

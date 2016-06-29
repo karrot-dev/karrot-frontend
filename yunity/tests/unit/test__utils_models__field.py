b0d878fc120be515b296b14e6067c2f6f407d801
Matthias Larisch <mail@matthias-larisch.de>
2016-06-29 08:36:48 +0200

    Remove MaxLengthCharField-Test

--- yunity/tests/unit/test__utils_models__field.py
+++
@@ -1,7 +0,0 @@
-from yunity.base.base_models import MaxLengthCharField
-from yunity.utils.tests.abc import BaseRequestTestCase
-
-
-class MaxLengthCharFieldTestCase(BaseRequestTestCase):
-    def test_max_length_char_field_always_has_max_length(self):
-        self.assertEqual(MaxLengthCharField().max_length, 255)

cd34a52ef179fcd71ba722a615b1ee23b1d83a79
Tilmann Becker <tilmann.becker+github@gmail.com>
2016-07-21 21:23:52 +0200

    Remove conversations-by-user endpoint (#161)
    The same functionality can be achieved by using the the conversations list endpoint and filter by chat participants.

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 from rest_framework_nested import routers
 from yunity.api.public.auth import AuthViewSet
-from yunity.api.public.chats import ChatViewSet, ChatMessageViewSet, UserChatViewSet
+from yunity.api.public.chats import ChatViewSet, ChatMessageViewSet
 from yunity.api.public.groups import GroupViewSet
 from yunity.api.public.users import UserViewSet
 from yunity.stores.api import StoreList, StoreSummary, PickupDatesViewSet
@@ -35,7 +35,6 @@ router.register(r'users', UserViewSet)
 router.register(r'conversations', ChatViewSet)
 chat_router = routers.NestedSimpleRouter(router, r'conversations', lookup='conversations')
 chat_router.register(r'messages', ChatMessageViewSet, base_name='conversations-messages')
-router.register(r'conversations-by-user', UserChatViewSet)
 
 # pickup date endpoints
 router.register(r'pickup-dates', PickupDatesViewSet)

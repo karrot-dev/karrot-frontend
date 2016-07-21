a46008e7f1e9428c52dad254d9b31fb0702e6fae
Sneezoo <Sneezoo@users.noreply.github.com>
2016-07-21 20:10:17 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+#Yunity Angular
+This is the proposal for yunity webapp using Angular Version 1.
+Angular Material is the UI framework of choice.
+
+We use the [NG6-starter](https://github.com/AngularClass/NG6-starter).
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path    = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'sourcemap',
+  entry: {},
+  module: {
+    loaders: [
+       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+       { test: /\.html$/, loader: 'raw' },
+       { test: /\.styl$/, loader: 'style!css!stylus' },
+       { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  plugins: [
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+      inject: 'body',
+      hash: true
+    }),
+
+    // Automatically move all modules defined outside of application directory to vendor bundle.
+    // If you are using more complicated project structure, consider to specify common chunks manually.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
+      }
+    })
+  ]
+};

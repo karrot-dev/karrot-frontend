cf61a872b62037ebf067ca4c08058f69d2d1cb88
Tilmann Becker <tilmann.becker+github@gmail.com>
2016-07-23 01:29:37 +0200

    Store API: Remove pickups field (#163)
    The pickups can also be accessed through the pickup-dates endpoint, filtering by store ID.
    Reduces complexity in the backend.

--- yunity/stores/tests/test_serializer.py
+++ yunity/stores/tests/test_serializer.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from yunity.groups.factories import Group
-from yunity.stores.factories import Store, PickupDate
+from yunity.stores.factories import Store
 from yunity.stores.serializers import StoreSerializer
-from django.utils import timezone
 
 
 class TestStoreSerializer(TestCase):
@@ -16,9 +15,3 @@ class TestStoreSerializer(TestCase):
     def test_store_instantiation(self):
         serializer = StoreSerializer(self.store)
         self.assertEqual(serializer.data['name'], self.store.name)
-        self.assertEqual(len(serializer.data['pickups']), 0)
-
-    def test_storesummary_pickups(self):
-        PickupDate(store=self.store, date=timezone.now())
-        serializer = StoreSerializer(self.store)
-        self.assertEqual(len(serializer.data['pickups']), 1)

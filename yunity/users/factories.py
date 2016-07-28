ed7d0c5f8b64185f9fc612b44e4182b12a0fa62e
Tilmann Becker <tilmann.becker@gmail.com>
2016-07-28 03:36:36 +0200

    Comment about display_name == password

--- yunity/users/factories.py
+++ yunity/users/factories.py
@@ -16,6 +16,8 @@ class User(DjangoModelFactory):
     first_name = LazyAttribute(lambda _: faker.name())
     last_name = LazyAttribute(lambda _: faker.name())
     email = LazyAttribute(lambda _: faker.email())
+
+    # Use display_name as password, as it is readable
     password = PostGeneration(lambda obj, *args, **kwargs: obj.set_password(obj.display_name))
 
     wall = SubFactory(Wall)

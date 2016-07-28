4d139e80b71bff3d0f206401b5e961684eb3e379
Tilmann Becker <tilmann.becker@gmail.com>
2016-07-28 19:52:05 +0200

    Search users by display_name, first_name, last_name

--- yunity/users/api.py
+++ yunity/users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from rest_framework import filters
 from rest_framework import viewsets
 from yunity.users.serializers import UserSerializer
 
@@ -6,3 +7,5 @@ from yunity.users.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('display_name', 'first_name', 'last_name')

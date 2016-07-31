17357df96a55a8210a5d0a59902c3ac8d5301a9a
Tilmann Becker <tilmann.becker@gmail.com>
2016-08-01 01:55:53 +0200

    User API tests for authorization behavior

--- yunity/users/tests/test_api.py
+++ yunity/users/tests/test_api.py
@@ -1,5 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from yunity.users.factories import User
 from yunity.utils.tests.fake import faker
 
 
@@ -7,6 +9,8 @@ class TestUsersAPI(APITestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.user = User()
+        cls.user2 = User()
         cls.url = '/api/users/'
         cls.user_data = {'display_name': faker.name(),
                          'first_name': faker.name(),
@@ -17,8 +21,68 @@ class TestUsersAPI(APITestCase):
                          'latitude': faker.latitude(),
                          'longitude': faker.longitude()}
 
-    def test_create(self):
+    def test_create_user(self):
         response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['email'], self.user_data['email'])
         self.assertAlmostEqual(response.data['latitude'], float(self.user_data['latitude']))
+
+    def test_get_users_forbidden(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_users_allowed(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_patch_user_forbidden(self):
+        url = self.url + str(self.user.id) + '/'
+        response = self.client.patch(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_patch_different_user_forbidden(self):
+        self.client.force_login(user=self.user)
+        url = self.url + str(self.user2.id) + '/'
+        response = self.client.patch(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_patch_self_allowed(self):
+        self.client.force_login(user=self.user)
+        url = self.url + str(self.user.id) + '/'
+        response = self.client.patch(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put_user_forbidden(self):
+        url = self.url + str(self.user.id) + '/'
+        response = self.client.put(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_put_different_user_forbidden(self):
+        self.client.force_login(user=self.user)
+        url = self.url + str(self.user2.id) + '/'
+        response = self.client.put(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_put_self_allowed(self):
+        self.client.force_login(user=self.user)
+        url = self.url + str(self.user.id) + '/'
+        response = self.client.put(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_remove_user_forbidden(self):
+        url = self.url + str(self.user.id) + '/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_remove_different_user_forbidden(self):
+        self.client.force_login(user=self.user)
+        url = self.url + str(self.user2.id) + '/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_remove_self_allowed(self):
+        self.client.force_login(user=self.user)
+        url = self.url + str(self.user.id) + '/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)

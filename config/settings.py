f76984da00eec4e2f41dc63d2eb85181dbb8e63c
Tilmann Becker <tilmann.becker@gmail.com>
2016-08-02 16:06:43 +0200

    Use recommended Argon2 password hash
    Needs another package for now:
    https://docs.djangoproject.com/en/1.10/topics/auth/passwords/#argon2-usage

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

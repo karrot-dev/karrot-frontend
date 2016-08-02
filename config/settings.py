2a0b1cd97c4b7941005d7e3714bb07d8e4acee01
Tilmann Becker <tilmann.becker@gmail.com>
2016-08-02 14:29:29 +0200

    Set TEMPLATES variable to get ready for Django 1.10

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,25 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'config.urls'
 
-TEMPLATES = []
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 CACHES = {
     "default": {

8ec80ca79029cc1791605fecc672c47d142dd1b1
Tilmann Becker <tilmann.becker@gmail.com>
2016-08-10 21:45:35 +0200

    Enable AdminRenderer for DRF

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,11 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.AdminRenderer',
     )
 }
 

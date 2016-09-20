39fffadf97b077f3cdeb9ac77440678836aa60e8
Tilmann Becker <tilmann.becker@gmail.com>
2016-09-20 22:27:31 +0200

    WIP: test for user service

--- client/app/common/user/user.spec.js
+++ client/app/common/user/user.spec.js
@@ -1,1 +1,32 @@
-//TODO: Add tests
+import UserService from './user';
+
+describe('user service', () => {
+  var $httpBackend, requestHandler;
+
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+
+    var userData = [{
+        "id": 1,
+        "display_name": "Mr T",
+        "first_name": "tilmann",
+        "last_name": "becker",
+        "email": "til@man.com",
+        "address": null,
+        "latitude": null,
+        "longitude": null
+    }];
+    requestHandler = $httpBackend.when('GET', '/api/users')
+      .respond(userData);
+  }));
+
+  afterEach(function() {
+  $httpBackend.verifyNoOutstandingExpectation();
+  $httpBackend.verifyNoOutstandingRequest();
+});
+
+  it('lists users', () => {
+    $httpBackend.expectGET('/api/users');
+    UserService.users();
+  });
+})

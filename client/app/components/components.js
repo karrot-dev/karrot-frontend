7ef623b1bd41dc50533d7e05b01181885c9a078d
D0nPiano <spam-mails@abwesend.de>
2016-10-14 01:40:49 +0200

    Fixed pickuplist lint errors

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,9 +1,9 @@
-import angular from 'angular';
-import login from './login/login';
-import home from './home/home';
-import signup from './signup/signup';
-import pickupList from './pickupList/pickupList';
-import pickupListItem from './pickupList/pickupListItem/pickupListItem';
+import angular from "angular";
+import login from "./login/login";
+import home from "./home/home";
+import signup from "./signup/signup";
+import pickupList from "./pickupList/pickupList";
+import pickupListItem from "./pickupList/pickupListItem/pickupListItem";
 
 let componentModule = angular.module('app.components', [
   login,

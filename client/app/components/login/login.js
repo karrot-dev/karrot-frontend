11ae2610ebbfa850225c12b4bafbda1317a5a279
Tilmann Becker <tilmann.becker@gmail.com>
2016-10-15 17:36:13 +0200

    Move module import away from test

--- client/app/components/login/login.js
+++ client/app/components/login/login.js
@@ -2,10 +2,12 @@ import angular from "angular";
 import uiRouter from "angular-ui-router";
 import loginComponent from "./login.component";
 import ngMessages from "angular-messages";
+import Authentication from "../../common/authentication/authentication";
 
 let loginModule = angular.module("login", [
   uiRouter,
-  ngMessages
+  ngMessages,
+  Authentication
 ])
 
 .config(($stateProvider, $urlRouterProvider, hookProvider) => {
--- client/app/components/login/login.spec.js
+++ client/app/components/login/login.spec.js
@@ -2,11 +2,9 @@ import LoginModule from "./login";
 import LoginController from "./login.controller";
 import LoginComponent from "./login.component";
 import LoginTemplate from "./login.html";
-import Authentication from "../../common/authentication/authentication.js";
 
 describe("Login", () => {
 
-  beforeEach(window.module(Authentication));
   beforeEach(window.module(LoginModule));
   beforeEach(inject((/*$injector*/) => {
   }));

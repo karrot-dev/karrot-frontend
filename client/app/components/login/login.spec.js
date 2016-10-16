80885a0c01b3147b9b011144f0792f15240f0542
Tilmann Becker <tilmann.becker@gmail.com>
2016-10-16 16:30:05 +0200

    Test for login module
    The login controller was hard to test. @nicksellen and me tried mocking
    $state and Authentication, but it was quite hard (also due to the
    hookProvider). So I ended up using $httpBackend and a spy on $state.

--- client/app/components/login/login.spec.js
+++ client/app/components/login/login.spec.js
@@ -2,28 +2,83 @@ import LoginModule from "./login";
 import LoginController from "./login.controller";
 import LoginComponent from "./login.component";
 import LoginTemplate from "./login.html";
+import sinon from "sinon";
+
+const { module } = angular.mock;
 
 describe("Login", () => {
-
-  beforeEach(window.module(LoginModule));
-  beforeEach(inject((/*$injector*/) => {
-  }));
+  beforeEach(() => {
+    module(LoginModule);
+    module(($stateProvider) => {
+      $stateProvider
+        .state("home", { url: "/" });
+    });
+  });
 
   describe("Module", () => {
-    // top-level specs: i.e., routes, injection, naming
+    it("is named login", () => {
+      expect(LoginModule).to.equal("login");
+    });
   });
 
   describe("Controller", () => {
-    // controller specs
-  });
+    let $componentController;
+    beforeEach(inject((_$componentController_) => {
+      $componentController = _$componentController_;
+    }));
 
-  describe("Template", () => {
-    // template specs
-    // tip: use regex to ensure correct bindings are used e.g., {{  }}
+    it("should exist", () => {
+      let ctrl = $componentController("login", {});
+      expect(ctrl).to.exist;
+    });
+
+    context("login", () => {
+      let $httpBackend, stateSpy;
+      beforeEach(() => {
+        inject((_$httpBackend_, _$state_) => {
+          $httpBackend = _$httpBackend_;
+          stateSpy = sinon.spy(_$state_, "go");
+        });
+      });
+
+      afterEach(() => {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+      });
+
+      let loginData = {
+        "id": 7,
+        "display_name": "asdflo",
+        "email": "asdf.asdf@asdf.asdf"
+      };
+
+      it("logs user in", () => {
+        let email = "example@example.com";
+        let password = "123";
+        $httpBackend.expectPOST("/api/auth/", { email, password }).respond(200, loginData);
+        $httpBackend.expectGET("/api/auth/status/").respond(200, loginData);
+        let ctrl = $componentController("login", {});
+        Object.assign(ctrl, { email, password });
+        ctrl.login();
+        $httpBackend.flush();
+        assert(stateSpy.calledWith("home"), "state not called with home");
+      });
+
+      it("rejects wrong password", () => {
+        let email = "example@example.com";
+        let password = "";
+        $httpBackend.expectPOST("/api/auth/", { email, password }).respond(400);
+        $httpBackend.expectGET("/api/auth/status/").respond(200, loginData);
+        let ctrl = $componentController("login", {});
+        Object.assign(ctrl, { email, password });
+        ctrl.login();
+        $httpBackend.flush();
+      });
+    });
+
   });
 
   describe("Component", () => {
-      // component/directive specs
     let component = LoginComponent;
 
     it("includes the intended template",() => {

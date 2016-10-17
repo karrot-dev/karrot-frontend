d82628e817c82960dc39f930af53dd2e964ed73a
Xbaal <becker.konrad@gmail.com>
2016-10-17 22:22:46 +0200

    added page generator

---
+++ generator/page/temp.component.js
@@ -0,0 +1,11 @@
+import template from "./<%= name %>.html";
+import controller from "./<%= name %>.controller";
+import "./<%= name %>.styl";
+
+let <%= name %>Component = {
+  bindings: {},
+  template,
+  controller
+};
+
+export default <%= name %>Component;
---
+++ generator/page/temp.spec.js
@@ -0,0 +1,50 @@
+import <%= upCaseName %>Module from "./<%= name %>";
+import <%= upCaseName %>Controller from "./<%= name %>.controller";
+import <%= upCaseName %>Component from "./<%= name %>.component";
+import <%= upCaseName %>Template from "./<%= name %>.html";
+
+const { module } = angular.mock;
+
+describe("<%= upCaseName %>", () => {
+  beforeEach(module(<%= upCaseName %>Module));
+
+  describe("Module", () => {
+    // top-level specs: i.e., routes, injection, naming
+    it("is named <%= name %>", () => {
+      expect(<%= upCaseName %>Module).to.equal("<%= name %>");
+    });
+  });
+
+  describe("Controller", () => {
+    let $componentController;
+    beforeEach(inject((_$componentController_) => {
+      $componentController = _$componentController_;
+    }));
+
+    it("should exist", () => {
+      let ctrl = $componentController("<%= name %>", {});
+      expect(ctrl).to.exist;
+    });
+  });
+
+  describe("Template", () => {
+    // template specs
+    // tip: use regex to ensure correct bindings are used e.g., {{  }}
+    it("has name in template [REMOVE]", () => {
+      expect(<%= upCaseName %>Template).to.match(/{{\s?\$ctrl\.name\s?}}/g);
+    });
+  });
+
+  describe("Component", () => {
+      // component/directive specs
+      let component = <%= upCaseName %>Component;
+
+      it("includes the intended template",() => {
+        expect(component.template).to.equal(<%= upCaseName %>Template);
+      });
+
+      it("invokes the right controller", () => {
+        expect(component.controller).to.equal(<%= upCaseName %>Controller);
+      });
+  });
+});

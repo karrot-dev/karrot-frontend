c8ed606157ec176c5098c8a2a681b665f51ad415
Tilmann Becker <tilmann.becker@gmail.com>
2016-10-23 16:08:08 +0200

    Make error handling work
    Actually, it should have worked before. But now it's clean.

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -14,6 +14,7 @@ class LoginController {
   login() {
     this.Authentication.login(this.email, this.password)
     .then(() => {
+      this.error.wrong = false;
       this.$state.go("home");
     })
     .catch(() => {

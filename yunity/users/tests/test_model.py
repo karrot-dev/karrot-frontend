988add62643a8da5f7e9f2e6972a178273d01f3e
Tilmann Becker <tilmann.becker@gmail.com>
2016-10-26 16:39:20 +0200

    Restrict display_name to 80 characters

--- yunity/users/tests/test_model.py
+++ yunity/users/tests/test_model.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db import DataError
 from django.db import IntegrityError
 from django.test import TestCase
 
@@ -17,3 +18,9 @@ class TestUserModel(TestCase):
         get_user_model().objects.create_user(**self.exampleuser)
         with self.assertRaises(IntegrityError):
             get_user_model().objects.create_user(**self.exampleuser)
+
+    def test_create_fails_if_name_too_long(self):
+        with self.assertRaises(DataError):
+            too_long = self.exampleuser
+            too_long['display_name'] = 'a' * 81
+            get_user_model().objects.create_user(**too_long)

ead9828aee661e9542d2e68efb2729e6dd01c64c
Tilmann Becker <tilmann.becker@gmail.com>
2016-10-27 21:50:37 +0200

    Add a nice litte test

--- client/app/components/_languageChooser/languageChooser.spec.js
+++ client/app/components/_languageChooser/languageChooser.spec.js
@@ -13,13 +13,29 @@ describe("LanguageChooser", () => {
 
   describe("Controller", () => {
     let $componentController;
-    beforeEach(inject((_$componentController_) => {
-      $componentController = _$componentController_;
+    beforeEach(inject(($injector) => {
+      $componentController = $injector.get("$componentController");
     }));
 
     it("should exist", () => {
       let ctrl = $componentController("languageChooser", {});
       expect(ctrl).to.exist;
     });
+
+    context("stubbed translate", () => {
+      let $translate;
+      beforeEach(inject(($injector) => {
+        $componentController = $injector.get("$componentController");
+        $translate = $injector.get("$translate");
+        sinon.stub($translate, "use");
+      }));
+
+      it("calls use method", () => {
+        let ctrl = $componentController("languageChooser", {});
+        ctrl.changeLanguage("de");
+        expect($translate.use).to.have.been.calledWith("de");
+      });
+    });
+
   });
 });

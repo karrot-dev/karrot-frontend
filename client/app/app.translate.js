fbc7510ffe3e0b377080ca55a1fef15d25ea57aa
Tilmann Becker <tilmann.becker@gmail.com>
2016-11-02 20:47:31 +0100

    Save chosen language in cookie

--- client/app/app.translate.js
+++ client/app/app.translate.js
@@ -4,6 +4,7 @@ import localeDE from "./locales/locale-de.json";
 let AppTranslate = ($translateProvider) => {
   "ngInject";
   $translateProvider
+  .useCookieStorage()
   .useSanitizeValueStrategy("escape")
   .translations("en", localeEN)
   .translations("de", localeDE)

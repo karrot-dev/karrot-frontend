c6e00e384e923b9f5733a7c9f07cb85f652b05cc
Tilmann Becker <tilmann.becker@gmail.com>
2016-11-16 11:54:04 +0100

    Add missing dependency
    Ok, I get it! Components are separate, need to have all their deps
    listed. Still, it looks like huge boilerplate.

--- client/app/components/_groupMenu/groupMenu.js
+++ client/app/components/_groupMenu/groupMenu.js
@@ -1,10 +1,12 @@
 import angular from "angular";
 import uiRouter from "angular-ui-router";
+import ngMaterial from "angular-material";
 import groupMenuComponent from "./groupMenu.component";
 import Group from "../../common/group/group";
 
 let groupMenuModule = angular.module("groupMenu", [
   uiRouter,
+  ngMaterial,
   Group
 ])
 

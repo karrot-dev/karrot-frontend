e6098f0a77ad718e4ae895b53f0e6f4ce2dcc067
Tilmann Becker <tilmann.becker@gmail.com>
2016-11-17 12:51:41 +0100

    Complete test

--- client/app/components/_storeList/storeList.spec.js
+++ client/app/components/_storeList/storeList.spec.js
@@ -1,22 +1,7 @@
 import StoreListModule from "./storeList";
 
 describe("StoreList", () => {
-  let $componentController, $httpBackend;
-
   let { module } = angular.mock;
-
-  beforeEach(() => {
-    module(StoreListModule);
-    inject(($injector) => {
-      $httpBackend = $injector.get("$httpBackend");
-      $componentController = $injector.get("$componentController");
-    });
-  });
-
-  afterEach(() => {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-  });
 
   let storeOne = {
     "id": 1,
@@ -29,15 +14,32 @@ describe("StoreList", () => {
   };
 
   describe("Controller", () => {
+    let $componentController, $httpBackend, $q, $mdDialog, $rootScope;
+
+    beforeEach(() => {
+      module(StoreListModule);
+      inject(($injector) => {
+        $httpBackend = $injector.get("$httpBackend");
+        $componentController = $injector.get("$componentController");
+        $q = $injector.get("$q");
+        $mdDialog = $injector.get("$mdDialog");
+        sinon.stub($mdDialog, "show");
+        $rootScope = $injector.get("$rootScope");
+      });
+    });
+
+    afterEach(() => {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
     it("check binding of complete stores",() => {
       let $ctrl = $componentController("storeList", {
       }, {
         stores: [storeOne]
       });
-
       expect($ctrl.storeData).to.deep.equal([storeOne]);
     });
-
 
     it("maps stores-array",() => {
       $componentController("storeList", {
@@ -51,7 +53,13 @@ describe("StoreList", () => {
 
     it("opens dialog to create store", () => {
       let $ctrl = $componentController("storeList", {}, { stores: [] } );
+      $mdDialog.show.returns($q((resolve) => {
+        resolve({ id: 999 });
+      }));
       $ctrl.openCreateStorePanel();
+      $rootScope.$apply();
+      expect($mdDialog.show).to.have.been.called;
+      expect($ctrl.storeData).to.deep.equal([{ id: 999 }]);
     });
   });
 });

1307d4f987bcbcc437351f29a201f411fbcb75a5
Tilmann Becker <tilmann.becker@gmail.com>
2016-12-01 22:09:25 +0100

    Test the store map
    It was really tricky to test the onChanges function. I need a parent
    scope for the bindings, so a data change will hit the trigger.

--- client/app/components/_storeList/_storeMap/storeMap.spec.js
+++ client/app/components/_storeList/_storeMap/storeMap.spec.js
@@ -12,14 +12,25 @@ describe("StoreMap", () => {
   });
 
   describe("Controller", () => {
-    let $componentController;
+    let $rootScope, $compile;
     beforeEach(inject(($injector) => {
-      $componentController = $injector.get("$componentController");
+      $rootScope = $injector.get("$rootScope");
+      $compile = $injector.get("$compile");
     }));
 
-    it("should exist", () => {
-      let $ctrl = $componentController("storeMap", {});
-      expect($ctrl).to.exist;
+    it("updates markers", () => {
+      let $scope = $rootScope.$new();
+      $scope.storeData = [];
+      let component = $compile("<store-map store-data='storeData'></store-map>")($scope);
+      let $ctrl = component.isolateScope().$ctrl;
+      expect($ctrl.hasMarkers()).to.be.false;
+
+      $scope.storeData = [{ id: 99, latitude: 1.99, longitude: 2.99, name: "test1" }];
+      $scope.$apply();
+      expect($ctrl.hasMarkers()).to.be.true;
+      expect($ctrl.markers).to.deep.equal({
+        99: { lat: 1.99, lng: 2.99, message: "test1", draggable: false }
+      });
     });
   });
 });

e29e630fd6efd5eebba97b3b1ec8b9125f50da1d
Tilmann Becker <tilmann.becker@gmail.com>
2016-12-11 10:19:58 +0100

    Use $onChanges to react to updated content

--- client/app/components/_expandablePanel/expandablePanel.controller.js
+++ client/app/components/_expandablePanel/expandablePanel.controller.js
@@ -5,25 +5,27 @@ const md = markdown({
 });
 
 export default class ExpandablePanelController {
-  constructor($timeout, $sce) {
+  constructor($sce) {
     "ngInject";
     Object.assign(this, {
+      $sce,
       expandable: false,
       expanded: false,
       parsed: ""
     });
-    $timeout( () => {
-
+  }
+  $onChanges(changes) {
+    if (changes.content && changes.content.currentValue) {
+      let content = changes.content.currentValue;
       // more then 10 lines?
-      if (this.content.match(/\n/mg).length > 9) {
+      if (content.match(/\n/mg).length > 9) {
         this.expandable = true;
       }
 
       if (this.markdown) {
-        this.parsed = $sce.trustAsHtml(md.render(this.content));
+        this.parsed = this.$sce.trustAsHtml(md.render(content));
       }
-
-    });
+    }
   }
   toggle() {
     this.expanded = !this.expanded;

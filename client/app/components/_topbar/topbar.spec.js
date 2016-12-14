a562393ad7ccabb76d8019356ad60d89f7a42d47
Tilmann Becker <tilmann.becker@gmail.com>
2016-12-14 20:53:39 +0100

    Add test

--- client/app/components/_topbar/topbar.spec.js
+++ client/app/components/_topbar/topbar.spec.js
@@ -4,8 +4,6 @@ import TopbarController from "./topbar.controller";
 const { module } = angular.mock;
 
 describe("Topbar", () => {
-  let makeController;
-
   beforeEach(module(TopbarModule));
 
   let $log;
@@ -17,16 +15,26 @@ describe("Topbar", () => {
     $log.assertEmpty();
   });
 
-  beforeEach(inject(() => {
-    makeController = () => {
-      return new TopbarController();
-    };
-  }));
-
   describe("Controller", () => {
-    it("exists", () => {
-      let $ctrl = makeController();
-      expect($ctrl).to.exist;
+    let $ctrl, Authentication, $q, $rootScope;
+
+    beforeEach(inject(($injector, _$componentController_) => {
+      Authentication = $injector.get("Authentication");
+      sinon.stub(Authentication, "update");
+      $q = $injector.get("$q");
+      $rootScope = $injector.get("$rootScope");
+      $ctrl = _$componentController_("topbar", {});
+    }));
+
+    it("calls $onInit", () => {
+      let userData = { id: 5, "display_name": "abc" };
+      Authentication.update.returns($q((resolve) => {
+        resolve(userData);
+      }));
+      $ctrl.$onInit();
+      $rootScope.$apply();
+      expect(Authentication.update).has.been.called;
+      expect($ctrl.loggedInUser).to.deep.equal(userData);
     });
   });
 });

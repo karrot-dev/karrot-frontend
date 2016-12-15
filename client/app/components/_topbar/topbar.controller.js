42d5cc737aa931f0dc494811d8840246f3fdb7d8
D0nPiano <spam-mails@abwesend.de>
2016-12-16 00:58:32 +0100

    Added SideNav to Topbar

--- client/app/components/_topbar/topbar.controller.js
+++ client/app/components/_topbar/topbar.controller.js
@@ -1,8 +1,20 @@
 import logo from "./carrot-logo.svgimage";
 
 class TopbarController {
-  constructor(Authentication) {
+  constructor(Authentication, $mdMedia, $scope, $mdSidenav) {
     "ngInject";
+    $scope.screenIsSmall = !$mdMedia("gt-sm");
+    $scope.$watch(() => {
+        return $mdMedia("gt-sm");
+    }, (big) => {
+        $scope.screenIsSmall = !big;
+    });
+
+    $scope.toggleRight = () => {
+      $mdSidenav('right')
+          .toggle()
+      };
+    
     Object.assign(this, {
       logo,
       Authentication
@@ -14,6 +26,17 @@ class TopbarController {
       this.loggedInUser = data;
     });
   }
+  
+  buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
+    }
 }
 
 export default TopbarController;

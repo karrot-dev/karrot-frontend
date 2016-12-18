981d92abdc1e67a6d220c2bc7475debba22e84c6
Tilmann Becker <tilmann.becker@gmail.com>
2016-12-18 21:16:36 +0100

    Add password field to group

--- yunity/groups/models.py
+++ yunity/groups/models.py
@@ -1,3 +1,4 @@
+from django.db.models import Model
 from django.db.models import TextField, ManyToManyField, CharField
 from yunity.base.base_models import BaseModel, LocationModel
 from config import settings
@@ -7,3 +8,4 @@ class Group(BaseModel, LocationModel):
     name = CharField(max_length=settings.NAME_MAX_LENGTH)
     description = TextField(blank=True)
     members = ManyToManyField(settings.AUTH_USER_MODEL, related_name='groups')
+    password = CharField(max_length=255, blank=True)

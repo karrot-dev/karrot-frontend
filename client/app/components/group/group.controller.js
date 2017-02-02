96153a414f0aee50d5c0358922da3a506a59c795
D0nPiano <spam-mails@abwesend.de>
2017-02-02 23:39:21 +0100

    Fixed group breadcrumb redirection

--- client/app/components/group/group.controller.js
+++ client/app/components/group/group.controller.js
@@ -1,10 +1,18 @@
 class GroupController {
-  constructor($state, $mdMedia) {
+  constructor($state, $scope, $mdMedia) {
     "ngInject";
 
     if ($state.current.name === "group"){
       $state.go("group.groupDetail.pickups");
     }
+    
+    $scope.$watch(() => {
+        return $state.$current.name;
+    }, (newVal) => {
+      if (newVal === "group"){
+        $state.go("group.groupDetail.pickups");
+      }
+    }) ;
 
     Object.assign(this, {
       $state,

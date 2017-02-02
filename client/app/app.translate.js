8c3b473ec358749107585372db2dd69f21945575
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-02 13:06:39 +0100

    chore: add Esperanto and Espanol
    Also show full language names in the chooser

--- client/app/app.translate.js
+++ client/app/app.translate.js
@@ -2,6 +2,8 @@ import localeEN from "./locales/locale-en.json";
 import localeDE from "./locales/locale-de.json";
 import localeFR from "./locales/locale-fr.json";
 import localeSV from "./locales/locale-sv.json";
+import localeES from "./locales/locale-es.json";
+import localeEO from "./locales/locale-eo.json";
 
 let AppTranslate = ($translateProvider) => {
   "ngInject";
@@ -12,11 +14,14 @@ let AppTranslate = ($translateProvider) => {
   .translations("de", localeDE)
   .translations("fr", localeFR)
   .translations("sv", localeSV)
+  .translations("eo", localeEO)
+  .translations("es", localeES)
   .fallbackLanguage("en")
-  .registerAvailableLanguageKeys(["en", "de", "fr", "sv"], {
+  .registerAvailableLanguageKeys(["en", "de", "fr", "sv", "eo", "es"], {
     "en_*": "en",
     "de_*": "de",
-    "fr_*": "fr"
+    "fr_*": "fr",
+    "es_*": "es"
   })
   .determinePreferredLanguage();
 };

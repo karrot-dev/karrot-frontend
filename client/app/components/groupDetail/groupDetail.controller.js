383ff7b5292e4847276b63a2a518d0aefbd12eaf
Crhistian Ramirez <cramirez@four51.com>
2017-02-05 20:37:48 -0600

    add confirm dialog before leaving a group

--- client/app/components/groupDetail/groupDetail.controller.js
+++ client/app/components/groupDetail/groupDetail.controller.js
@@ -1,10 +1,12 @@
 class GroupDetailController {
-  constructor(Group, CurrentGroup, $state) {
+  constructor(Group, CurrentGroup, $state, $mdDialog, $document) {
     "ngInject";
     Object.assign(this, {
       Group,
       CurrentGroup,
       $state,
+      $document,
+      $mdDialog,
       error: {
         leaveGroup: false
       }
@@ -21,17 +23,22 @@ class GroupDetailController {
     };
   }
 
-  leaveGroup() {
-    this.Group.leave(this.groupData.id)
-      .then(() => {
-        if (this.CurrentGroup.value.id === this.groupData.id) {
-          this.CurrentGroup.clear();
-        }
-        this.$state.go("home");
-      })
-      .catch(() => {
-        this.error.leaveGroup = true;
-      });
+  leaveGroup($event) {
+    this.$mdDialog.show({
+      contentElement: "#confirmLeaveDialog",
+      parent: angular.element(this.$document.body),
+      targetEvent: $event
+    })
+    .then(() => this.Group.leave(this.groupData.id))
+    .then(() => {
+      if (this.CurrentGroup.value.id === this.groupData.id) {
+        this.CurrentGroup.clear();
+      }
+      this.$state.go("home");
+    })
+    .catch(() => {
+      this.error.leaveGroup = true;
+    });
   }
 
   updateGroupData() {

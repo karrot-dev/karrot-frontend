6bc31b1461ab63b7eb429dd7577dc259c2143471
Andreas Langecker <a.langecker@posteo.de>
2017-02-11 23:42:47 +0100

    joinGroup: added tests

--- client/app/components/_joinGroup/joinGroup.spec.js
+++ client/app/components/_joinGroup/joinGroup.spec.js
@@ -49,5 +49,38 @@ describe("JoinGroup", () => {
       // check if we get a Promise back
       expect(response.then).to.exist;
     });
+
+    it("should change active group", () => {
+      let $ctrl = $componentController("joinGroup", {});
+      $ctrl.active = {id:1337};
+      $ctrl.toggle({id:5555});
+      expect($ctrl.active.id).to.equal(5555);
+    });
+
+    it("should close on click on active group", () => {
+      let $ctrl = $componentController("joinGroup", {});
+      $ctrl.active = {id:1337};
+      $ctrl.toggle({id:1337});
+      expect($ctrl.active).to.be.null;
+    });
+
+    it("should toggle check if group is protected", () => {
+      let $ctrl = $componentController("joinGroup", {});
+      $ctrl.active = {id:1337, protected:true};
+      $ctrl.toggleCheck();
+      expect($ctrl.check).to.be.true;
+    });
+    it("should join group directly if group is not protected", () => {
+      let $ctrl = $componentController("joinGroup", {});
+      $ctrl.active = {id:1337, protected:false};
+      $ctrl.toggleCheck()
+      $httpBackend.expectPOST("/api/groups/1337/join/").respond();
+      $httpBackend.flush();
+
+      // dont go to check
+      expect($ctrl.check).to.be.false;
+      // window should get closed
+      expect($mdDialog.hide).to.have.been.calledWith(1337);
+    });
   });
 });

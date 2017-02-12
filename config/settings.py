951944ce2b25f770082d68838342e3df09c9a411
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-12 15:35:20 +0100

    fix: remove unused password validators

--- config/settings.py
+++ config/settings.py
@@ -117,24 +117,6 @@ SESSION_CACHE_ALIAS = "default"
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Password validation
-# https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 EMAIL_BACKEND = "anymail.backends.sparkpost.SparkPostBackend"
 
 # Internationalization

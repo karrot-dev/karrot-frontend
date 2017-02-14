f1499f91147bb0239470457f88d55bfe22252c99
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-14 13:24:57 +0100

    feat: address editing for groupEdit

--- client/app/components/group/groupEdit/groupEdit.controller.js
+++ client/app/components/group/groupEdit/groupEdit.controller.js
@@ -1,10 +1,17 @@
 class GroupEditController {
-  constructor($state, GroupService) {
+  constructor($state, GroupService, Geocoding) {
     "ngInject";
     Object.assign(this, {
       $state,
       GroupService,
-      editData: angular.copy($state.groupData)
+      Geocoding,
+      editData: angular.copy($state.groupData),
+      mapCenter: {},
+      mapDefaults: {
+        scrollWheelZoom: false,
+        zoomControl: false,
+        dragging: false
+      }
     });
   }
 
@@ -16,6 +23,20 @@ class GroupEditController {
     }).catch((err) => {
       this.error = err.data;
     });
+  }
+
+  geoLookup(query) {
+    return this.Geocoding.lookupAddress(query);
+  }
+
+  setGeo(item) {
+    if (!item) return;
+    this.mapCenter.zoom = 10;
+    this.editData.lat = item.lat;
+    this.editData.lng = item.lng;
+    this.editData.address = item.name;
+    this.mapCenter.lat = item.lat;
+    this.mapCenter.lng = item.lng;
   }
 }
 
--- client/app/components/group/groupEdit/groupEdit.js
+++ client/app/components/group/groupEdit/groupEdit.js
@@ -3,11 +3,17 @@ import uiRouter from "angular-ui-router";
 import Authentication from "../../../common/authentication/authentication";
 import groupEditComponent from "./groupEdit.component";
 import GroupService from "../../../common/group/group";
+import Geocoding from "../../../common/geocoding/geocoding";
+import "leaflet";
+import "ui-leaflet";
+import "leaflet/dist/leaflet.css"; // looks in node_modules
 
 let groupEditModule = angular.module("groupEdit", [
   uiRouter,
   Authentication,
-  GroupService
+  GroupService,
+  Geocoding,
+  "ui-leaflet"
 ])
 
 .component("groupEdit", groupEditComponent)

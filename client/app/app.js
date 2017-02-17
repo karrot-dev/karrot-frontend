cccbc9ddc951dd24d8433873304df1a64d88929a
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-17 14:14:30 +0100

    feat: add $mdToast for HTTP5xx and timeout
    the parent container should not be scrollable, so I added a md-content
    below `<body>`

--- client/app/app.js
+++ client/app/app.js
@@ -18,6 +18,7 @@ import AppMaterial from "./app.material";
 import AppLocalizeConfig from "./app.localizeConfig";
 import AppLocalizeRun from "./app.localizeRun";
 import AppXEditableConfig from "./app.xeditable";
+import AppHTTPErrorHandler from "./app.HTTPErrorHandler";
 
 // styles
 import "angular-xeditable/dist/css/xeditable.css";
@@ -68,6 +69,7 @@ angular.module("app", [
 .config(AppLocalizeConfig)
 .run(AppLocalizeRun)
 .config(AppMaterial)
+.config(AppHTTPErrorHandler)
 .config((cfpLoadingBarProvider) => {
   "ngInject";
   cfpLoadingBarProvider.includeSpinner = false;

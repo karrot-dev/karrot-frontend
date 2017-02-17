e1bdc5ebd402c2a6b1a9efea0ce2f6d7d512cbfc
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-17 17:28:48 +0100

    fix: use $mdMenu.open
    Removes the usage of deprecated mdOpenMenu

--- client/app/components/_topbar/_groupMenu/groupMenu.controller.js
+++ client/app/components/_topbar/_groupMenu/groupMenu.controller.js
@@ -11,11 +11,12 @@ class GroupMenuController {
     });
   }
 
-  openMenu($mdOpenMenu) {
+  openMenu($mdMenu, $event) {
+    // using $mdMenu service from scope
     this.GroupService.listMy().then((data) => {
       this.groups = data;
     });
-    $mdOpenMenu();
+    $mdMenu.open($event);
   }
 
   openJoinGroupDialog($event) {
--- client/app/components/_topbar/_languageChooser/languageChooser.js
+++ client/app/components/_topbar/_languageChooser/languageChooser.js
@@ -1,10 +1,12 @@
 import angular from "angular";
 import uiRouter from "angular-ui-router";
+import ngMaterial from "angular-material";
 import translate from "angular-translate";
 import languageChooserComponent from "./languageChooser.component";
 
 let languageChooserModule = angular.module("languageChooser", [
   uiRouter,
+  ngMaterial,
   translate
 ])
 

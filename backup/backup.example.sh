585d97ed4b1aa3796909aaa2b12314dbbaf93eb9
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-18 14:43:35 +0100

    feat: add backup and archival script
    To be modified on deployment and added to crontab

---
+++ backup/backup.example.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# settings
+dbname=fstool
+filename=foodsaving-$(date -I).db.enc
+testdb=fstool_test_backup_$(date -I)
+
+# dump database and encrypt
+pg_dump -Fc $dbname | gpg --batch --no-tty --quiet -e -r #your GPG key id# > $filename && \
+echo dumped into $filename && \
+# check if it decrypts and imports into new database
+createdb $testdb && \
+gpg --batch --no-tty --quiet -d $filename | pg_restore -d $testdb && \
+dropdb $testdb && \
+echo checked decrypting and import into $testdb
+
+# copy to offsite
+scp $filename #offsite location#:backup && \
+echo copied to offsite location

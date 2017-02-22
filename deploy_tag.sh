9236b72acec8aa270b02dbc242c0716002bebc53
Nick Sellen <git@nicksellen.co.uk>
2017-02-22 18:45:58 +0100

    Create archive.tar.gz when releasing a tag
    Will be downloadable from URL/archive.tar.gz

--- deploy_tag.sh
+++ deploy_tag.sh
@@ -15,5 +15,9 @@ echo "deploying frontend branch [$CIRCLE_TAG] to [$HOST] in [$DIR] dir"
 # write the release version into "version" file
 echo $CIRCLE_TAG > dist/version
 
+# create a downloadable archive too
+(cd dist && tar czf ../archive.tar.gz *)
+
+# send it all to the host
 rsync -avz --delete dist/ deploy@$HOST:foodsaving-frontend/$DIR/
 

c43b79832e4ae03fb6515ec5ef411d190c295371
Andreas Langecker <a.langecker@posteo.de>
2017-02-22 14:59:11 +0100

    groupHistory: uses now data from history.service

--- client/app/components/group/_history/history.controller.js
+++ client/app/components/group/_history/history.controller.js
@@ -1,7 +1,20 @@
 class HistoryController {
-  constructor() {
+  constructor(History) {
     "ngInject";
     Object.assign(this, {
+      History,
+      list: []
+    });
+  }
+  $onInit() {
+    this.History.get().then( (res) => {
+      this.list = res
+        .sort( (a, b) => a.date < b.date)
+        .map( (entry) => {
+          entry.translate = "HISTORY." + entry.typus;
+          entry.compareDate = entry.date.toISOString().substr(0,10);
+          return entry;
+        });
     });
   }
 }

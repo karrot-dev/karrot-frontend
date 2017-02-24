38e9dfba0642eef16496b7f54c87c6fd55e40e9b
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-24 04:16:06 +0100

    fix: use full search text to allow for low faker entropy

--- foodsaving/groups/tests/test_groups_api_filter.py
+++ foodsaving/groups/tests/test_groups_api_filter.py
@@ -4,7 +4,7 @@ from foodsaving.groups.factories import GroupFactory
 from foodsaving.users.factories import UserFactory
 
 
-class TestStoresAPIFilter(APITestCase):
+class TestGroupsAPIFilter(APITestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -49,7 +49,7 @@ class TestStoresAPIFilter(APITestCase):
         self.assertEqual(response.data[0]['name'], self.group.name)
 
     def test_search_description(self):
-        response = self.client.get(self.url, {'search': self.group.public_description[:10]})
+        response = self.client.get(self.url, {'search': self.group.public_description})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], self.group.name)

c447a16582188a5e02b11c9bb96e46a0b981eb74
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-25 23:08:40 +0100

    feat: pagination for history
    History can get big, so reduce the default size of response.
    Uses http://www.django-rest-framework.org/api-guide/pagination/#limitoffsetpagination
    
    This puts a navigation around the response data, so actual history data
    is now in response.data.results

--- foodsaving/history/api.py
+++ foodsaving/history/api.py
@@ -2,13 +2,14 @@ from django_filters.rest_framework import filters
 from django_filters.rest_framework import FilterSet
 from rest_framework import viewsets
 from rest_framework.filters import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
 from foodsaving.history.models import History, HistoryTypus
 from foodsaving.history.serializers import HistorySerializer
 
 
-def method(qs, field, value):
+def filter_history_typus(qs, field, value):
     return qs.filter(**{field: getattr(HistoryTypus, value)})
 
 
@@ -16,7 +17,12 @@ class HistoryFilter(FilterSet):
     class Meta:
         model = History
         fields = ('group', 'store', 'users', 'typus')
-    typus = filters.ChoiceFilter(choices=HistoryTypus.items(), method=method)
+    typus = filters.ChoiceFilter(choices=HistoryTypus.items(), method=filter_history_typus)
+
+
+class HistoryPagination(LimitOffsetPagination):
+    default_limit = 50
+    max_limit = 1000
 
 
 class HistoryViewSet(viewsets.ReadOnlyModelViewSet):
@@ -28,6 +34,7 @@ class HistoryViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filter_class = HistoryFilter
     permission_classes = (IsAuthenticated,)
+    pagination_class = HistoryPagination
 
     def get_queryset(self):
         return self.queryset.filter(group__members=self.request.user)

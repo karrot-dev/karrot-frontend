8dae477c8a65fbfc3e4548614c9e29198f21855b
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-25 23:25:46 +0100

    test: adapt test to pagination

--- foodsaving/history/tests/test_api.py
+++ foodsaving/history/tests/test_api.py
@@ -11,7 +11,14 @@ from foodsaving.stores.factories import StoreFactory, PickupDateFactory, PickupD
 history_url = '/api/history/'
 
 
-class TestHistoryAPICreateGroup(APITestCase):
+class PaginatedResponseTestCase(APITestCase):
+    def get_results(self, *args, **kwargs):
+        response = self.client.get(*args, **kwargs)
+        response.data = response.data['results']
+        return response
+
+
+class TestHistoryAPICreateGroup(PaginatedResponseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.member = UserFactory()
@@ -19,11 +26,11 @@ class TestHistoryAPICreateGroup(APITestCase):
     def test_create_group(self):
         self.client.force_login(self.member)
         self.client.post('/api/groups/', {'name': 'xyzabc', 'timezone': 'Europe/Berlin'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'GROUP_CREATE')
 
 
-class TestHistoryAPIOrdering(APITestCase):
+class TestHistoryAPIOrdering(PaginatedResponseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.member = UserFactory()
@@ -32,11 +39,11 @@ class TestHistoryAPIOrdering(APITestCase):
         self.client.force_login(self.member)
         self.client.post('/api/groups/', {'name': 'Group 1', 'timezone': 'Europe/Berlin'})
         self.client.post('/api/groups/', {'name': 'Group 2', 'timezone': 'Europe/Berlin'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['payload']['name'], 'Group 2')
 
 
-class TestHistoryAPIWithExistingGroup(APITestCase):
+class TestHistoryAPIWithExistingGroup(PaginatedResponseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.member = UserFactory()
@@ -46,21 +53,21 @@ class TestHistoryAPIWithExistingGroup(APITestCase):
     def test_modify_group(self):
         self.client.force_login(self.member)
         self.client.patch(self.group_url, {'name': 'something new'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'GROUP_MODIFY')
         self.assertEqual(response.data[0]['payload']['name'], 'something new')
 
     def test_dont_modify_group(self):
         self.client.force_login(self.member)
         self.client.patch(self.group_url, {'name': self.group.name})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(len(response.data), 0)
 
     def test_join_group(self):
         user = UserFactory()
         self.client.force_login(user)
         self.client.post(self.group_url + 'join/')
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'GROUP_JOIN')
 
     def test_leave_group(self):
@@ -70,17 +77,17 @@ class TestHistoryAPIWithExistingGroup(APITestCase):
         self.client.post(self.group_url + 'leave/')
 
         self.client.force_login(self.member)
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'GROUP_LEAVE')
 
     def test_create_store(self):
         self.client.force_login(self.member)
         self.client.post('/api/stores/', {'name': 'xyzabc', 'group': self.group.id})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'STORE_CREATE')
 
 
-class TestHistoryAPIWithExistingStore(APITestCase):
+class TestHistoryAPIWithExistingStore(PaginatedResponseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.member = UserFactory()
@@ -91,7 +98,7 @@ class TestHistoryAPIWithExistingStore(APITestCase):
     def test_modify_store(self):
         self.client.force_login(self.member)
         self.client.patch(self.store_url, {'name': 'newnew'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(len(response.data), 1, response.data)
         self.assertEqual(response.data[0]['typus'], 'STORE_MODIFY')
         self.assertEqual(response.data[0]['payload']['name'], 'newnew')
@@ -99,13 +106,13 @@ class TestHistoryAPIWithExistingStore(APITestCase):
     def test_dont_modify_store(self):
         self.client.force_login(self.member)
         self.client.patch(self.store_url, {'name': self.store.name})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(len(response.data), 0)
 
     def test_delete_store(self):
         self.client.force_login(self.member)
         self.client.delete(self.store_url, {'name': 'new'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'STORE_DELETE')
 
     def test_create_pickup(self):
@@ -114,7 +121,7 @@ class TestHistoryAPIWithExistingStore(APITestCase):
             'date': timezone.now() + relativedelta(days=1),
             'store': self.store.id
         })
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'PICKUP_CREATE')
 
     def test_create_series(self):
@@ -124,11 +131,11 @@ class TestHistoryAPIWithExistingStore(APITestCase):
             'rule': 'FREQ=WEEKLY',
             'store': self.store.id
         })
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'SERIES_CREATE')
 
 
-class TestHistoryAPIWithExistingPickups(APITestCase):
+class TestHistoryAPIWithExistingPickups(PaginatedResponseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.member = UserFactory()
@@ -142,56 +149,56 @@ class TestHistoryAPIWithExistingPickups(APITestCase):
     def test_modify_pickup(self):
         self.client.force_login(self.member)
         self.client.patch(self.pickup_url, {'max_collectors': '11'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'PICKUP_MODIFY')
         self.assertEqual(response.data[0]['payload']['max_collectors'], '11')
 
     def test_dont_modify_pickup(self):
         self.client.force_login(self.member)
         self.client.patch(self.pickup_url, {'date': self.pickup.date})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(len(response.data), 0)
 
     def test_delete_pickup(self):
         self.client.force_login(self.member)
         self.client.delete(self.pickup_url)
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'PICKUP_DELETE')
 
     def test_modify_series(self):
         self.client.force_login(self.member)
         self.client.patch(self.series_url, {'max_collectors': '11'})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'SERIES_MODIFY')
         self.assertEqual(response.data[0]['payload']['max_collectors'], '11')
 
     def test_dont_modify_series(self):
         self.client.force_login(self.member)
         self.client.patch(self.series_url, {'rule': self.series.rule})
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(len(response.data), 0)
 
     def test_delete_series(self):
         self.client.force_login(self.member)
         self.client.delete(self.series_url)
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'SERIES_DELETE')
 
     def test_join_pickup(self):
         self.client.force_login(self.member)
         self.client.post(self.pickup_url + 'add/')
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'PICKUP_JOIN')
 
     def test_leave_pickup(self):
         self.client.force_login(self.member)
         self.pickup.collectors.add(self.member)
         self.client.post(self.pickup_url + 'remove/')
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'PICKUP_LEAVE')
 
 
-class TestHistoryAPIWithDonePickup(APITestCase):
+class TestHistoryAPIWithDonePickup(PaginatedResponseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.member = UserFactory()
@@ -207,13 +214,13 @@ class TestHistoryAPIWithDonePickup(APITestCase):
 
     def test_pickup_done(self):
         self.client.force_login(self.member)
-        response = self.client.get(history_url)
+        response = self.get_results(history_url)
         self.assertEqual(response.data[0]['typus'], 'PICKUP_DONE')
         self.assertLess(parse(response.data[0]['date']), timezone.now() - relativedelta(hours=22))
 
     def test_filter_pickup_done(self):
         self.client.force_login(self.member)
-        response = self.client.get(history_url, {'typus': 'PICKUP_DONE'})
+        response = self.get_results(history_url, {'typus': 'PICKUP_DONE'})
         self.assertEqual(response.data[0]['typus'], 'PICKUP_DONE')
-        response = self.client.get(history_url, {'typus': 'GROUP_JOIN'})
+        response = self.get_results(history_url, {'typus': 'GROUP_JOIN'})
         self.assertEqual(len(response.data), 0)

5a75b77789c17cc237673c686015ab42243bd844
Andreas Langecker <a.langecker@posteo.de>
2017-02-26 13:38:33 +0100

    fixes grep regex

--- scripts/localeDiff.sh
+++ scripts/localeDiff.sh
@@ -13,6 +13,6 @@ SCRIPT=$(cat <<-END
 END
 )
 echo $SCRIPT | node | sort > /tmp/locale_current
-grep -REoh "translate=\".*?\"" client | sed 's/translate="\([^"]*\)"/\1/p' | grep -v \{\{ | sort -u  > /tmp/locale_inuse
+grep -REoh "translate=\"[^\"]*?\"" client | sed 's/translate="\([^"]*\)"/\1/p' | grep -v \{\{ | sort -u  > /tmp/locale_inuse
 
 diff /tmp/locale_current /tmp/locale_inuse

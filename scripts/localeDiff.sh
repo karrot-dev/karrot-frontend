f1585687df074ea2ff998b1ad846c499c13dcfa4
Andreas Langecker <a.langecker@posteo.de>
2017-02-26 13:34:31 +0100

    created script for translation key check

---
+++ scripts/localeDiff.sh
@@ -0,0 +1,18 @@
+SCRIPT=$(cat <<-END
+    const locales = require('./client/app/locales/locale-en.json');
+    function print(prefix, obj) {
+      for(let key in obj) {
+        if(obj[key] instanceof Object) {
+          print(prefix+key+'.', obj[key]);
+        } else {
+          console.log(prefix+key);
+        }
+      }
+    }
+    print('', locales);
+END
+)
+echo $SCRIPT | node | sort > /tmp/locale_current
+grep -REoh "translate=\".*?\"" client | sed 's/translate="\([^"]*\)"/\1/p' | grep -v \{\{ | sort -u  > /tmp/locale_inuse
+
+diff /tmp/locale_current /tmp/locale_inuse

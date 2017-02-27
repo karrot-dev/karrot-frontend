44e4a437343eb0e7dfd14bb8783426868503f695
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-27 20:32:06 +0100

    refactor: remove unused profile picture

--- client/app/components/_profilePicture/profilePicture.controller.js
+++ client/app/components/_profilePicture/profilePicture.controller.js
@@ -9,10 +9,6 @@ class ProfilePictureController {
   $onInit() {
     this.User.get(this.userId).then((res) => {
       this.name = res.display_name;
-      if (res.profile_picture) {
-        // not implemented in the backend yet
-        this.picture = res.profile_picture;
-      }
     }).catch(() => {
       this.$translate("PROFILE.INACCESSIBLE_OR_DELETED").then((text) => {
         this.name = text;

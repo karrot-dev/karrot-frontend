9193dd130fa0afdbf72e4078edea44de7c580825
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-28 13:10:55 +0100

    test: landing page and group map

--- client/app/components/landingPage/_landingPageMap/landingPageMap.controller.js
+++ client/app/components/landingPage/_landingPageMap/landingPageMap.controller.js
@@ -22,11 +22,10 @@ class LandingPageMapController {
     this.GroupService.list().then((allGroups) => {
       angular.forEach(allGroups, (group) => {
         if (group.latitude && group.longitude){
-          const memberCount = group.members.length;
           this.markers[group.id] = {
             lat: group.latitude,
             lng: group.longitude,
-            message: `<group-popup name='${group.name}' member-count='${memberCount}'></group-popup>`
+            message: `<group-popup name='${group.name}' member-count='${group.members.length}'></group-popup>`
           };
         }
       });

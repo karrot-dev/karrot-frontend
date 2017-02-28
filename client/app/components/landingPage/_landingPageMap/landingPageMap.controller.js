69ef1101ebebf4a66143c87859763b87f8dd640a
Tilmann Becker <tilmann.becker@gmail.com>
2017-02-28 11:45:11 +0100

    refactor: map sizing
    apparently we need to invalidate the size. ui-leaflet offers an event,
    which seems simpler.

--- client/app/components/landingPage/_landingPageMap/landingPageMap.controller.js
+++ client/app/components/landingPage/_landingPageMap/landingPageMap.controller.js
@@ -1,31 +1,26 @@
 class LandingPageMapController {
-  constructor(GroupService, $timeout, $scope, leafletData) {
+  constructor(GroupService, $scope, $timeout) {
     "ngInject";
     Object.assign(this, {
       GroupService,
-      leafletData,
       markers: {},
       center: {
         lat: 50.112371,
         lng: 8.687439,
         zoom: 4
+      },
+      defaults: {
+        scrollWheelZoom: false
       }
     });
 
-    // invalidate Map is needed so that map isn't grey after it's loaded
-    const invalidateMap = () => {
-      $scope.mapController.leafletData.getMap().then((map) => {
-        map.invalidateSize();
-      });
-    };
-    $scope.mapController = this;
-    $timeout(invalidateMap, 1200);
+    $timeout(() => $scope.$broadcast("invalidateSize"), 1200);
   }
 
   $onInit() {
     this.GroupService.list().then((allGroups) => {
       angular.forEach(allGroups, (group) => {
-        if (group.latitude !== null){
+        if (group.latitude && group.longitude){
           const memberCount = group.members.length;
           this.markers[group.id] = {
             lat: group.latitude,

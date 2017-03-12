87d17e6a36c26a0d5c8872cd3726571571e44d4d
Nick Sellen <git@nicksellen.co.uk>
2017-03-12 23:26:11 +0100

    Fix storeList not updating
    Creating clone of list means further updates will not be present
    in the cloned list.

--- client/app/components/group/_storeList/storeList.controller.js
+++ client/app/components/group/_storeList/storeList.controller.js
@@ -17,7 +17,6 @@ class StoreListController {
     if (changes.groupId && angular.isDefined(changes.groupId.currentValue)) {
       this.Store.listByGroupId(changes.groupId.currentValue).then((data) => {
         this.CurrentStores.set(data);
-        this.storeList = angular.copy(this.CurrentStores.list);
       });
     }
   }

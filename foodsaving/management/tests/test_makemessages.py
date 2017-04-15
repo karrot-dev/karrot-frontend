4661a04d159e0583f16c28d087427fab31c676f6
ShababShahriar <sskhan11@live.com>
2017-04-15 23:14:59 +0600

    Add additional test to increase test coverage

--- foodsaving/management/tests/test_makemessages.py
+++ foodsaving/management/tests/test_makemessages.py
@@ -1,6 +1,11 @@
+from unittest.mock import patch
 from django.test import TestCase
 
 from ..commands.makemessages import Command as MakeMessagesCommand
+from django_jinja.management.commands.makemessages import Command as DjangoJinjaMakeMessagesCommand
+
+makemessages = MakeMessagesCommand
+django_jinja_makemessages = DjangoJinjaMakeMessagesCommand
 
 
 class CustomMakeMessagesTest(TestCase):
@@ -16,3 +21,10 @@ class CustomMakeMessagesTest(TestCase):
         options['extensions'] = ['py']
         modified_options_with_initial_extension = MakeMessagesCommand.update_options(**options)
         self.assertIn('jinja', modified_options_with_initial_extension['extensions'])
+
+    @patch(__name__ + '.django_jinja_makemessages.handle')
+    @patch(__name__ + '.makemessages.update_options', return_value={})
+    def test_handle(self, mock1, mock2):
+        MakeMessagesCommand.handle(MakeMessagesCommand())
+        assert MakeMessagesCommand.update_options.called
+        assert DjangoJinjaMakeMessagesCommand.handle.called

cd48b3c8111176f818130933e26394fc2289d653
Tilmann Becker <tilmann.becker@gmail.com>
2017-05-20 19:51:22 +0100

    test if /docs can be rendered

--- foodsaving/tests/integration/test_swagger.py
+++ foodsaving/tests/integration/test_swagger.py
@@ -10,7 +10,14 @@ class TestSwaggerAPI(APITestCase):
         cls.user = UserFactory()
         cls.url = '/docs/'
 
-    def test_swagger(self):
+    def test_swagger_coreapi(self):
         self.client.force_login(user=self.user)
         response = self.client.get(self.url)
+        self.assertEqual(response.accepted_media_type, 'application/coreapi+json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_swagger_html(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get(self.url, HTTP_ACCEPT='text/html')
+        self.assertEqual(response.accepted_media_type, 'text/html')
         self.assertEqual(response.status_code, status.HTTP_200_OK)

3c8e9da0f130a2adbb2ed87612aadfcb9ab24875
Tilmann Becker <tilmann.becker@gmail.com>
2017-06-24 13:13:53 +0200

    show translation status in UI

--- client/app/components/_topbar/_languageChooser/languageChooser.controller.js
+++ client/app/components/_topbar/_languageChooser/languageChooser.controller.js
@@ -1,3 +1,5 @@
+import frontendStatus from "../../../locales/status-frontend.json";
+
 class LanguageChooserController {
   constructor($translate) {
     "ngInject";
@@ -14,6 +16,21 @@ class LanguageChooserController {
         ru: "Русский",
         zh: "中文"
       }
+    });
+  }
+
+  $onInit() {
+    this.sortedLanguages = Object.keys(this.providedLangs).map((k) => {
+      return {
+        key: k,
+        name: this.providedLangs[k],
+        completed: parseInt(frontendStatus[k].completed.replace("%", ""))
+      };
+    });
+    this.sortedLanguages.sort((a, b) => {
+      if (a.completed !== b.completed) 
+        return b.completed - a.completed  ;
+      return a.name.localeCompare(b.name);
     });
   }
 

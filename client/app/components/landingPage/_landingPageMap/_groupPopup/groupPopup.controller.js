6dfa3b5691899593dbbbba5bd697b0ef6569c8a3
D0nPiano <spam-mails@abwesend.de>
2017-06-28 19:42:43 +0200

    redid group popup

--- client/app/components/landingPage/_landingPageMap/_groupPopup/groupPopup.controller.js
+++ client/app/components/landingPage/_landingPageMap/_groupPopup/groupPopup.controller.js
@@ -1,7 +1,14 @@
 class GroupPopupController {
-  constructor() {
+  constructor(GroupService) {
     "ngInject";
     Object.assign(this, {
+      GroupService
+    });
+  }
+
+  $onInit(){
+    this.GroupService.get(parseInt(this.id)).then((groupData) => {
+      this.groupData = groupData;
     });
   }
 }

0bb72b94909fd2883f52ace4ffd294d84ab0d588
Tilmann Becker <tilmann.becker@gmail.com>
2017-07-03 17:03:45 +0200

    show toast if not logged in

--- client/app/app.authRequired.js
+++ client/app/app.authRequired.js
@@ -6,6 +6,11 @@ let AuthRequiredConfig = ($transitionsProvider) => {
     let $state = trans.router.stateService;
     let Authentication = trans.injector().get("Authentication");
     return Authentication.update().catch(() => {
+      let $translate = trans.injector().get("$translate");
+      let $mdToast = trans.injector().get("$mdToast");
+      $translate("GLOBAL.NOT_LOGGED_IN").then((message) => {
+        $mdToast.showSimple(message);
+      });
       return $state.target("login");
     });
   });

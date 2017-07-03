b2349c0e8d95c60396bd4acfa4cd267274563c27
Tilmann Becker <tilmann.becker@gmail.com>
2017-07-03 22:58:28 +0200

    test authRequired and check for status

--- client/app/app.authRequired.js
+++ client/app/app.authRequired.js
@@ -5,13 +5,16 @@ let AuthRequiredConfig = ($transitionsProvider) => {
   } }, (trans) => {
     let $state = trans.router.stateService;
     let Authentication = trans.injector().get("Authentication");
-    return Authentication.update().catch(() => {
-      let $translate = trans.injector().get("$translate");
-      let $mdToast = trans.injector().get("$mdToast");
-      $translate("GLOBAL.NOT_LOGGED_IN").then((message) => {
-        $mdToast.showSimple(message);
-      });
-      return $state.target("login");
+    return Authentication.update(true)
+    .catch((response) => {
+      if (response.status === 401) {
+        let $translate = trans.injector().get("$translate");
+        let $mdToast = trans.injector().get("$mdToast");
+        $translate("GLOBAL.NOT_LOGGED_IN").then((message) => {
+          $mdToast.showSimple(message);
+        });
+        return $state.target("login");
+      }
     });
   });
 };

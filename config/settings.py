8f93e53edde9c3c00035b567fd0e583f022241ef
Tilmann Becker <tilmann.becker@gmail.com>
2017-07-24 19:36:01 +0200

    disable API browser
    rather use the swagger /docs endpoint
    
    Closes #280

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework.renderers.AdminRenderer',
     )
 }
 

2bc2fbd2f941aab6bb51b88e94b17c95310ba915
Tilmann Becker <tilmann.becker@gmail.com>
2017-08-04 17:02:58 +0200

    sort store list by name
    should also handle non-ascii characters

--- client/app/services/store/currentStores.service.js
+++ client/app/services/store/currentStores.service.js
@@ -17,7 +17,7 @@ export default class CurrentStores {
   }
 
   set(list) {
-    angular.copy(list, this.list);
+    angular.copy(this._getSortedByName(list), this.list);
     return this.list;
   }
 
@@ -28,12 +28,14 @@ export default class CurrentStores {
 
   pushItem(item) {
     this.list.push(item);
+    angular.copy(this._getSortedByName(this.list), this.list);
     return item;
   }
 
   replaceItem(item) {
     let i = this.list.findIndex((e) => e.id === item.id);
     this.list[i] = item;
+    angular.copy(this._getSortedByName(this.list), this.list);
     return item;
   }
 
@@ -43,6 +45,17 @@ export default class CurrentStores {
 
   get(id) {
     return this.list.find((e) => e.id === id);
+  }
+
+  _getSortedByName(list) {
+    let sortMap = list.map((el, i) => {
+      return {
+        i,
+        value: el.name ? el.name.toLowerCase() : ""   // make it forgiving, useful for tests
+      };
+    });
+    sortMap.sort((a, b) => a.value.localeCompare(b.value));
+    return sortMap.map((el) => list[el.i]);
   }
 
 }

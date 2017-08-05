4d96ab61e510bdc6af855d50b261d96746bef0fd
Tilmann Becker <tilmann.becker@gmail.com>
2017-08-05 19:24:47 +0200

    add translateFilter stub

--- client/app/components/group/_searchBar/searchBar.spec.js
+++ client/app/components/group/_searchBar/searchBar.spec.js
@@ -4,6 +4,7 @@ const { module } = angular.mock;
 
 describe("SearchBar", () => {
   beforeEach(module(SearchBarModule));
+  beforeEach(module({ translateFilter: (a) => a }));
 
   let $log;
   beforeEach(inject(($injector) => {

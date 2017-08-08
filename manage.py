b4a98b03b6a6200593b8dd7bd64ececadbf2946b
Nick Sellen <git@nicksellen.co.uk>
2017-08-08 11:09:58 +0200

    Move test settings to config/test_settings.py

--- manage.py
+++ manage.py
@@ -3,33 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-    from django.core.management import execute_from_command_line
-    from django.conf import settings
 
     if 'test' in sys.argv:
-        """Speed up test execution.
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.test_settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-        From these tips:
-          https://brobin.me/blog/2016/08/7-ways-to-speed-up-your-django-test-suite/
-
-        """
-        import logging
-
-        logging.disable(logging.CRITICAL)
-        settings.DEBUG = False
-        settings.TEMPLATE_DEBUG = False
-        settings.PASSWORD_HASHERS = [
-            'django.contrib.auth.hashers.MD5PasswordHasher',
-        ]
-        settings.MIDDLEWARE_CLASSES = [
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-        ]
-        settings.CACHES['default']['BACKEND'] = 'django.core.cache.backends.locmem.LocMemCache'
-        settings.EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
+    from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)

ef043cda8448324d5caf9fba15e0d86be711a010
Nick Sellen <git@nicksellen.co.uk>
2017-08-09 16:38:07 +0200

    Speed up test execution

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+    if 'test' in sys.argv:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.test_settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
     from django.core.management import execute_from_command_line
 

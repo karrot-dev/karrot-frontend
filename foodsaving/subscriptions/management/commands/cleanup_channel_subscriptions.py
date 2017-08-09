9ace373ec989704776f37c80958444654deea65b
Nick Sellen <git@nicksellen.co.uk>
2017-08-09 16:29:44 +0200

    Don't print "done" when command finishes

--- foodsaving/subscriptions/management/commands/cleanup_channel_subscriptions.py
+++ foodsaving/subscriptions/management/commands/cleanup_channel_subscriptions.py
@@ -9,9 +9,4 @@ class Command(BaseCommand):
     """If we have not received a message on the channel for a while, we delete our entries."""
 
     def handle(self, *args, **options):
-        def print(*args):
-            self.stdout.write(' '.join([str(_) for _ in args]))
-
         ChannelSubscription.objects.filter(lastseen_at__lt=timezone.now() - relativedelta(minutes=5)).delete()
-
-        print('done')

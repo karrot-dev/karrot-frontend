78281977abf755ebb24cca268955b5e5dfb2334f
Nick Sellen <git@nicksellen.co.uk>
2017-08-09 16:34:19 +0200

    Remove test speeding up changes
    Will put in a seperate PR

--- manage.py
+++ manage.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-
-    if 'test' in sys.argv:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.test_settings")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
     from django.core.management import execute_from_command_line
 

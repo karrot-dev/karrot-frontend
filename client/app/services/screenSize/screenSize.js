e9e51696be0343d1f15fa6f8c08b0dff77f632ec
Jasim <jasimtaqi@gmail.com>
2017-08-22 20:06:11 -0700

    refactor: Create service for determining screen size
    The service provides a function that wraps $mdMedia (an AngularJS Material service)
    and returns a boolean indicating whether a screen is wider than 960px.
    
    The service can be extended by adding similar functions to test for other breakpoints.
    
    Resolves: #465

---
+++ client/app/services/screenSize/screenSize.js
@@ -0,0 +1,9 @@
+import ScreenSize from "./screenSize.service";
+
+let screenSizeModule = angular.module("ScreenSize", [])
+
+.service("ScreenSize", ScreenSize)
+
+.name;
+
+export default screenSizeModule;

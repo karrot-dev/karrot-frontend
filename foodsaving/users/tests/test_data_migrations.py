f680e032d4a9f2fd09d1f5163650d6e01687efa2
Nick Sellen <git@nicksellen.co.uk>
2017-08-31 18:43:07 +0100

    Make that nasty raw SQL go away

--- foodsaving/users/tests/test_data_migrations.py
+++ foodsaving/users/tests/test_data_migrations.py
@@ -1,7 +1,5 @@
 import logging
 
-from django.db import connection
-
 from foodsaving.tests.utils import TestMigrations
 from foodsaving.utils.tests.fake import faker
 
@@ -15,23 +13,12 @@ class TestUnverifiedMailDataMigration(TestMigrations):
 
     def setUpBeforeMigration(self, apps):
         logging.disable(logging.CRITICAL)
-
         self.email = faker.email()
-
-        with connection.cursor() as cursor:
-            cursor.execute("""
-            INSERT INTO users_user (
-              email, password, created_at, is_active, is_staff, display_name, profile_visibility
-            ) values (
-              %s, %s, now(), 't', 'f', %s, 0
-            )
-            """, [self.email, 'yay', 'peter'])
-            cursor.execute("SELECT email FROM users_user WHERE email = %s", [self.email])
-            row = cursor.fetchone()
-            self.assertEqual(row, (self.email,))  # pass
+        User = apps.get_model('users', 'User')
+        User.objects.create(email=self.email, display_name='Peter')
 
     def test_unverified_email_set(self):
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT email, unverified_email FROM users_user WHERE email = %s", [self.email])
-            row = cursor.fetchone()
-            self.assertEqual(row, (self.email, self.email))
+        User = self.apps.get_model('users', 'User')
+        user = User.objects.filter(email=self.email).first()
+        self.assertEqual(self.email, user.email)
+        self.assertEqual(self.email, user.unverified_email)

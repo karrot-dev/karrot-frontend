51bc24aaa54c3d49ea7ab2810d41048f99a17f0e
Nick Sellen <git@nicksellen.co.uk>
2017-08-31 18:17:24 +0100

    Call UpdateModelMixin.update directly

--- foodsaving/utils/mixins.py
+++ foodsaving/utils/mixins.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+from rest_framework.mixins import UpdateModelMixin
 
 
 class PartialUpdateModelMixin(object):
@@ -6,13 +6,10 @@ class PartialUpdateModelMixin(object):
     Update a model instance.
     Only allow PATCH, no PUT
     """
+
     def perform_update(self, serializer):
         serializer.save()
 
     def partial_update(self, request, *args, **kwargs):
-        partial = True
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+        kwargs['partial'] = True
+        return UpdateModelMixin.update(self, request, *args, **kwargs)

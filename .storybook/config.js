1b0766243c631fad7e34ee6d993e3b016d57360f
Nick Sellen <git@nicksellen.co.uk>
2017-09-14 15:58:22 +0200

    Add storybook conf and scripts

---
+++ .storybook/config.js
@@ -0,0 +1,29 @@
+// Vue config (from main.js)
+
+// default to material design
+require(`../src/themes/app.mat.styl`)
+
+import Vue from 'vue'
+import Quasar from 'quasar'
+import Vuex from 'vuex'
+
+Vue.config.productionTip = false
+Vue.use(Quasar) // Install Quasar Framework
+Vue.use(Vuex) // Install Vuex
+
+require('quasar-extras/roboto-font')
+import 'quasar-extras/material-icons'
+// import 'quasar-extras/ionicons'
+// import 'quasar-extras/fontawesome'
+// import 'quasar-extras/animate'
+
+
+// Storybook config
+import { configure } from '@storybook/vue'
+
+function loadStories() {
+  require('../src/stories')
+}
+
+configure(loadStories, module)
+
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,28 @@
+const genDefaultConfig = require('@storybook/vue/dist/server/config/defaults/webpack.config.js')
+const merge = require('webpack-merge')
+
+module.exports = (baseConfig, env) => {
+  const storybookConfig = genDefaultConfig(baseConfig, env)
+  const quasarConfig = require('../build/webpack.dev.conf.js')
+  const quasarBasePlugins = require('../build/webpack.base.conf.js').plugins
+
+  // use Quasar config as default
+  let mergedConfig = merge(quasarConfig, storybookConfig)
+
+  // set Storybook entrypoint
+  mergedConfig.entry = storybookConfig.entry
+
+  // allow relative module resolution as used in Storybook
+  mergedConfig.resolve.modules = storybookConfig.resolve.modules
+
+  // only use Quasars loaders
+  mergedConfig.module.rules = quasarConfig.module.rules
+
+  // enable Storybook http server
+  mergedConfig.plugins = storybookConfig.plugins
+  // get Quasar's DefinePlugin and PostCSS settings
+  mergedConfig.plugins.unshift(quasarBasePlugins[0], quasarBasePlugins[1]) 
+
+  return mergedConfig
+}
+

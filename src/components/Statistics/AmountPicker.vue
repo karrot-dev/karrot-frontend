e2a09e061bf12bcbca8b55d8c6ab659972b477aa
D0nPiano <spam-mails@abwesend.de>
2017-09-15 13:09:06 +0200

    Fixed Amount picker

--- src/components/Statistics/AmountPicker.vue
+++ src/components/Statistics/AmountPicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <div> Amount Picker </div>
-    <img v-for="(photoSrc, idx) in photosArray" src="~assets/feedback/apple.png" :key="idx"/>
+    <img v-for="(photoSrc, idx) in photosArray" :src="appleImg" :key="idx"/>
     <div>
       <q-slider
         v-model="selectedValue"
@@ -17,12 +17,14 @@
 
 <script>
 import { QSlider } from 'quasar'
+import appleImg from '@/assets/feedback/apple.png'
 
 export default {
   data () {
     return {
       selectedValue: 3.0,
-      photosArray: [1, 2]
+      photosArray: [1, 2],
+      appleImg
     }
   },
   components: {

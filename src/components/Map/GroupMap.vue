f3deddc4caa0cd90d3b8f8969a231fa99580e0f1
D0nPiano <spam-mails@abwesend.de>
2017-09-15 17:30:39 +0200

    Added GroupMap

---
+++ src/components/Map/GroupMap.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+  <!--div v-for="(marker, idx) in markers">
+     lat-lng: {{marker.latLng}}
+     icon {{marker.icon}}
+  </div>-->
+  <v-map :zoom="zoom" :bounds="bounds" :center="center">
+    <v-tilelayer :url="url" :attribution="attribution"></v-tilelayer>
+    <v-marker v-if="options.showStores" v-for="marker in storeMarkers" :key="marker.id" :lat-lng="marker.latLng" :icon="marker.icon">
+      <v-popup :content="marker.popupcontent"></v-popup>
+    </v-marker>
+    <v-marker v-if="options.showUsers" v-for="marker in userMarkers" :key="marker.id" :lat-lng="marker.latLng" :icon="marker.icon">
+      <v-popup :content="marker.popupcontent"></v-popup>
+    </v-marker>
+  </v-map>
+  </div>
+</template>
+
+<script>
+import Vue2Leaflet from 'vue2-leaflet'
+import L from 'leaflet'
+import 'leaflet.awesome-markers/dist/leaflet.awesome-markers.js'
+
+// fix default marker icon. Should hopefully get fixed in Leaflet 1.3
+// https://github.com/Leaflet/Leaflet/issues/4968
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
+
+export default {
+  components: {
+    'v-map': Vue2Leaflet.Map,
+    'v-tilelayer': Vue2Leaflet.TileLayer,
+    'v-marker': Vue2Leaflet.Marker,
+    'v-popup': Vue2Leaflet.Popup
+  },
+  data () {
+    return {
+      zoom: 13,
+      center: L.latLng(49.9105778076202, 8.65834236145019),
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      bounds: L.latLngBounds()
+    }
+  },
+  props: {
+    users: { required: false, default: [] },
+    stores: { required: false, default: [] },
+    options: {
+      required: false,
+      default: {
+        showUsers: true,
+        showStores: true
+      }
+    }
+  },
+  computed: {
+    storeMarkers () {
+      let markers = []
+      for (let store of this.stores) {
+        markers.push({
+          latLng: L.latLng(store.latitude, store.longitude),
+          id: 'store_' + store.id,
+          icon: L.AwesomeMarkers.icon({
+            icon: 'shopping-cart',
+            markerColor: 'blue',
+            prefix: 'fa'
+          }),
+          popupcontent: '<a href="https://foodsaving.world">go somewhere</a>'
+        })
+      }
+      return markers
+    },
+    userMarkers () {
+      let markers = []
+      for (let user of this.users) {
+        markers.push({
+          latLng: L.latLng(user.latitude, user.longitude),
+          id: 'user_' + user.id,
+          icon: L.AwesomeMarkers.icon({
+            icon: 'user',
+            markerColor: 'green',
+            prefix: 'fa'
+          }),
+          popupcontent: '<a href="https://foodsaving.world">go somewhere</a>'
+        })
+      }
+      return markers
+    }
+  },
+  mounted () {
+    let bounds = new L.latLngBounds(this.storeMarkers.map((o) => o.latLng)).pad(0.2) //eslint-disable-line
+    this.bounds = bounds
+  }
+}
+</script>
+
+<style>
+@import "~leaflet/dist/leaflet.css";
+@import "~leaflet.awesome-markers/dist/leaflet.awesome-markers.css";
+
+</style>
--- src/components/Map/MapDemo.story.js
+++ src/components/Map/MapDemo.story.js
@@ -1,9 +1,28 @@
 import { storiesOf } from '@storybook/vue'
 
 import MapDemo from './MapDemo.vue'
+import GroupMap from './GroupMap.vue'
 
-storiesOf('GroupMap', module)
-  .add('Default', () => ({
+storiesOf('Map', module)
+  .add('Demo', () => ({
     components: { MapDemo },
     template: '<MapDemo style="height: 600px" />'
+  }))
+  .add('GroupMap', () => ({
+    components: { GroupMap },
+    template: '<GroupMap :stores="stores" :users="users" style="height: 600px" />',
+    data () {
+      return {
+        users: [
+          { 'id': 1, 'display_name': 'Lars', 'email': 'l@l.de', 'unverified_email': 'l@l.de', 'address': 'Darmstadt, Regierungsbezirk Darmstadt, Hessen, Deutschland', 'latitude': 49.8990022441358, 'longitude': 8.66415739059448, 'description': 'This is me!', 'mail_verified': false, 'key_expires_at': '2017-08-02T21:22:54.730980Z', 'current_group': 1, 'language': 'en' },
+          { 'id': 2, 'display_name': 'Max', 'email': 'm@m.de', 'unverified_email': 'm@m.de', 'address': 'Darmstadt, Regierungsbezirk Darmstadt, Hessen, Deutschland', 'latitude': 49.9090022441358, 'longitude': 8.66815739059448, 'description': 'This is me!', 'mail_verified': false, 'key_expires_at': '2017-08-02T21:22:54.730980Z', 'current_group': 1, 'language': 'en' }
+        ],
+        stores: [
+          { 'id': 1, 'name': 'Teststore1', 'description': 'all the good stuff', 'group': 1, 'address': 'Kranichstein, Darmstadt, Regierungsbezirk Darmstadt, Hesse, Germany', 'latitude': 49.8965397, 'longitude': 8.6847644, 'weeks_in_advance': 4, 'upcoming_notification_hours': 4 },
+          { 'id': 60, 'name': 'New Tienda', 'description': 'bla bla', 'group': 1, 'address': null, 'latitude': 49.8701892656281, 'longitude': 8.65070343017578, 'weeks_in_advance': 4, 'upcoming_notification_hours': 4 },
+          { 'id': 56, 'name': 'Supermarkt Arheilgen', 'description': 'blabla', 'group': 1, 'address': 'Frankfurter Landstraße 147 Arheilgen', 'latitude': 49.9105778076202, 'longitude': 8.65834236145019, 'weeks_in_advance': 4, 'upcoming_notification_hours': 4 },
+          { 'id': 2, 'name': 'asd', 'description': 'asd', 'group': 1, 'address': 'Luisenplatz, Darmstadt, Regierungsbezirk Darmstadt, Hessen, 64283, Deutschland', 'latitude': 49.8728175, 'longitude': 8.65062690796964, 'weeks_in_advance': 4, 'upcoming_notification_hours': 4 },
+          { 'id': 61, 'name': 'Griesheimer Markt', 'description': 'Frisches Essen dies das', 'group': 1, 'address': 'Griesheim Marktplatz', 'latitude': 49.8615586173026, 'longitude': 8.57465744018555, 'weeks_in_advance': 4, 'upcoming_notification_hours': 4 }]
+      }
+    }
   }))

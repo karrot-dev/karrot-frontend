61ed6d2d0d30ec61cd8b52bf1fa79d8ee6f7db9b
D0nPiano <spam-mails@abwesend.de>
2017-09-16 01:34:27 +0200

    Improved Login, added people to assets

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
       <div class="header">
+        <img :src="loginImage"></img>
         <h4>Login</h4>
       </div>
       <div class="content">
-        <div class="white-box shadow-1 inset-shadow">
+        <div class="white-box">
           <q-field icon="fa-envelope">
             <q-input v-model="email" float-label="Email"/>
           </q-field>
         </div>
-        <div class="white-box shadow-1 inset-shadow">
+        <div class="white-box">
           <q-field icon="fa-lock">
             <q-input v-model="password" type="password" float-label="Password"/>
           </q-field>
@@ -22,9 +23,15 @@
 
 <script>
 import { QField, QInput, QBtn } from 'quasar'
+import loginImage from '@/assets/people/cherry.png'
 
 export default {
-  components: { QField, QInput, QBtn }
+  components: { QField, QInput, QBtn },
+  data () {
+    return {
+      loginImage
+    }
+  }
 }
 </script>
 
@@ -32,6 +39,13 @@ export default {
 @import '~variables'
 .header
   padding 0.1em 1em
+  img
+    height 40px
+    vertical-align middle
+    margin-bottom: 20px
+    margin-right: 10px
+  img, h4
+    display inline-block
 .content
   border-radius 5px
   width 100%

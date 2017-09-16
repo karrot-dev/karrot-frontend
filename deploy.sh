2d1ed32d0df5a4ae33932e8f2c86df11d3a6d4de
Nick Sellen <git@nicksellen.co.uk>
2017-09-16 15:03:27 +0200

    Add .commit info to deployed artifacts

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,22 @@ if [ -z "$REF" ] || [ -z "$DIR" ]; then
   exit 1
 fi
 
+REPO_URL="https://github.com/yunity/karrot-frontend"
+COMMIT_SHA=$(git rev-parse HEAD)
+COMMIT_SHA_SHORT=$(git rev-parse --short HEAD)
+
+REF_URL="$REPO_URL/tree/$REF"
+COMMIT_URL="$REPO_URL/tree/$COMMIT_SHA"
+
 ssh-keyscan -H $HOST >> ~/.ssh/known_hosts
 
 echo "deploying frontend branch [$REF] to [$HOST] in [$DIR] dir"
 
 echo "$REF" > dist/version
 echo "$REF" > storybook-static/version
+
+echo "$COMMIT_SHA" > dist/.commit
+echo "$COMMIT_SHA" > storybook-static/.commit
 
 # send it all to the host
 rsync -avz --delete dist/ "deploy@$HOST:karrot-frontend/$DIR/"
@@ -31,14 +41,10 @@ if [ "$REF" == "$dev_deployment_branch" ]; then
     URL="https://karrot-dev.foodsaving.world"
     STORYBOOK_URL="https://karrot-storybook-dev.foodsaving.world"
 
-    REPO_URL="https://github.com/yunity/karrot-frontend"
-    REF_URL="$REPO_URL/tree/$REF"
-    COMMIT_URL="$REPO_URL/tree/$CIRCLE_SHA1"
-    SHORT_SHA=$(git rev-parse --short HEAD)
     COMMIT_MESSAGE=$(git log -1 --pretty=%s)
 
     ATTACHMENT_TEXT=":banana: <$URL|Visit the site>\n:books: <$STORYBOOK_URL|Visit the storybook>"
-    ATTACHMENT_FOOTER="Using git ref <$REF_URL|$REF>, commit <$COMMIT_URL|$SHORT_SHA> - $COMMIT_MESSAGE"
+    ATTACHMENT_FOOTER="Using git ref <$REF_URL|$REF>, commit <$COMMIT_URL|$COMMIT_SHA_SHORT> - $COMMIT_MESSAGE"
 
     payload=$(printf '{
         "channel": "#karrot-git",

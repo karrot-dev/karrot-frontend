9b7bcd0cb8230c99b145d4796747541573d57fa2
Nick Sellen <git@nicksellen.co.uk>
2017-09-16 16:13:37 +0200

    Remove commented out template

--- src/components/Sidenav/SidenavMap.vue
+++ src/components/Sidenav/SidenavMap.vue
@@ -41,25 +41,6 @@
           <span>{{$t("GROUPMAP.HIDE_USERS")}}</span>
         </q-tooltip>
       </q-btn>
-      <!--
-      <q-btn 
-        ng-click="$ctrl.CurrentGroup.map.options.showUsers = !$ctrl.CurrentGroup.map.options.showUsers;">
-        <q-tooltip v-if="$ctrl.CurrentGroup.map.options.showUsers">
-          <span translate="GROUPMAP.HIDE_USERS"></span>
-        </q-tooltip>
-        <q-tooltip v-if="!$ctrl.CurrentGroup.map.options.showUsers">
-          <span translate="GROUPMAP.SHOW_USERS"></span>
-        </q-tooltip>
-        <span class="fa-stack" v-if="!$ctrl.CurrentGroup.map.options.showUsers">
-          <i class="fa fa-user fa-stack-1x"></i>
-          <i class="fa fa-times fa-bot-right fa-stack-1x"></i>
-        </span>
-        <span class="fa-stack" v-if="$ctrl.CurrentGroup.map.options.showUsers">
-          <i class="fa fa-user fa-stack-1x"></i>
-          <i class="fa fa-check fa-bot-right fa-stack-1x"></i>
-        </span>
-      </q-btn>
-      -->
     </div>
     <div>
       <GroupMap class="map" :users="users" :stores="stores"

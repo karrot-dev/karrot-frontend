c1b5d85a809fbe7ac4bbbbdfac0be2d404b27384
Nick Sellen <git@nicksellen.co.uk>
2017-09-16 13:13:31 +0200

    Notify slack on deploy

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,30 @@ echo "$REF" > storybook-static/version
 # send it all to the host
 rsync -avz --delete dist/ "deploy@$HOST:karrot-frontend/$DIR/"
 rsync -avz --delete storybook-static/ "deploy@$HOST:karrot-frontend-storybook/$DIR/"
+
+dev_deployment_branch="quasar"
+
+if [ "$REF" == "$dev_deployment_branch" ]; then
+  if [ ! -z "$SLACK_WEBHOOK_URL" ]; then
+
+    payload=$(printf '{
+      "channel": "#karrot-git",
+        "username": "deploy",
+        "text": "Successful deploy of *karrot-dev* from %s",
+        "icon_emoji": ":sparkles:",
+        "attachments": [
+          {
+            "title": "karrot-dev.foodsaving.world",
+            "title_link": "https://karrot-dev.foodsaving.world"
+          },
+          {
+            "title": "karrot-storybook-dev.foodsaving.world",
+            "title_link": "https://karrot-storybook-dev.foodsaving.world"
+          }
+        ]
+      }' "$dev_deployment_branch")
+
+    curl -X POST --data-urlencode "payload=$payload" "$SLACK_WEBHOOK_URL"
+
+  fi
+fi

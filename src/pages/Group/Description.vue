eab11179f9df4532f496f88778d293fc002fa255
D0nPiano <spam-mails@abwesend.de>
2017-09-18 17:32:56 +0200

    Fixed Group Description

--- src/pages/Group/Description.vue
+++ src/pages/Group/Description.vue
@@ -1,57 +1,15 @@
 <template>
-  <MainLayout>
-    <div slot="sidenav-desktop">
-      <SidenavMap/>
-      <SidenavGroup/>
-      <SidenavStores :stores="stores"/>
-    </div>
-    <div>
-      <h4>Description</h4>
-      asdassdnaölkdmasösdn  
-    </div>
-  </MainLayout>
+  <GroupLayout>
+    <h4>Description</h4>
+    asdassdnaölkdmasösdn
+  </GroupLayout>
 </template>
 
 <script>
-import MainLayout from '@/components/Layout/MainLayout.vue'
-import Wall from '@/components/Wall/Wall.vue'
-import SidenavMap from '@/components/Sidenav/SidenavMap.vue'
-import SidenavGroup from '@/components/Sidenav/SidenavGroup.vue'
-import SidenavStores from '@/components/Sidenav/SidenavStores.vue'
-
-import {
-  mapGetters,
-  mapActions,
-} from 'vuex'
+import GroupLayout from '@/components/Group/Layout.vue'
 
 export default {
-  components: {
-    MainLayout, Wall, SidenavMap, SidenavGroup, SidenavStores,
-  },
-  computed: {
-    ...mapGetters({
-      emptyPickups: 'pickups/empty',
-      stores: 'stores/list',
-      messages: 'conversations/activeMessages',
-      group: 'groups/activeGroup',
-      users: 'groups/activeUsers',
-    }),
-  },
-  methods: {
-    ...mapActions({
-      selectGroup: 'groups/selectGroup',
-    }),
-  },
-  metaInfo () {
-    if (this.group) {
-      return {
-        title: this.group.name,
-      }
-    }
-    else {
-      return {}
-    }
-  },
+  components: { GroupLayout },
 }
 </script>
 

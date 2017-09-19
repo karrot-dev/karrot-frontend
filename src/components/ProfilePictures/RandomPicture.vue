5170246a0983de5957e418af13a3c3df6912e2a9
Nick Sellen <git@nicksellen.co.uk>
2017-09-20 00:38:10 +0200

    Make random picture component reactive

--- src/components/ProfilePictures/RandomPicture.vue
+++ src/components/ProfilePictures/RandomPicture.vue
@@ -20,87 +20,103 @@ export default {
     QTooltip,
   },
   mounted () {
-    function pseudoRandom (seed) {
-      let random = Math.sin(seed) * 10000
-      random -= Math.floor(random)
-      return random
-    }
-
-    let seed = pseudoRandom(this.seed)
-    let size = this.size
-
-    function getRandomRange (min, max, add = 1000) {
-      return Math.floor(pseudoRandom(seed * add) * (max - min) + min)
-    }
-    const svgns = 'http://www.w3.org/2000/svg'
-    const box = document.createElementNS(svgns, 'svg')
-
-    const rows = 3
-    const columns = 3
-    const blockSize = Math.floor(size / 2)
-    const rotate = blockSize * rows / 2
-
-    box.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink')
-    box.setAttribute('width', size)
-    box.setAttribute('height', size)
-    box.setAttribute('viewbox', '0 0 100 100')
-    box.setAttribute('class', 'box')
-
-    const g = document.createElementNS(svgns, 'g')
-    g.setAttribute(
-      'transform',
-      `translate(${-(rows * blockSize - size) / 2} ${-((rows * blockSize - size) / 2)}) ` +
-        `rotate(${90 * seed} ${rotate} ${rotate})`,
-    )
-
-    for (let i = 0; i < columns; i++) {
-      // noprotect
-      for (let j = 0; j < rows; j++) {
-        let rect = document.createElementNS(svgns, 'rect')
-        rect.setAttribute('width', blockSize)
-        rect.setAttribute('height', blockSize)
-        rect.setAttribute('fill', 'rgba(' +
-          getRandomRange(100, 255, (i + 1) * (j + 1) * 1) + ',' +
-          getRandomRange(100, 255, (i + 1) * (j + 1) * 2) + ',' +
-          getRandomRange(100, 255, (i + 1) * (j + 1) * 3) + ',1)',
-        )
-        rect.setAttribute('x', i * blockSize)
-        rect.setAttribute('y', j * blockSize)
-
-        g.appendChild(rect)
+    this.$el.appendChild(this.box)
+  },
+  watch: {
+    box (box, prevBox) {
+      this.$el.replaceChild(box, prevBox)
+    },
+  },
+  computed: {
+    initials () {
+      let parts = this.name.split(' ').map(s => s[0])
+      if (parts.length > 2) {
+        parts = [parts[0], parts[parts.length - 1]]
       }
-    }
-    box.appendChild(g)
+      return parts.join('').toUpperCase()
+    },
+    box () {
+      function pseudoRandom (seed) {
+        let random = Math.sin(seed) * 10000
+        random -= Math.floor(random)
+        return random
+      }
 
-    let overlay = document.createElementNS(svgns, 'rect')
-    overlay.setAttribute('width', size)
-    overlay.setAttribute('height', size)
-    overlay.setAttribute('fill', 'rgba(' +
-      getRandomRange(100, 255, 1) + ',' +
-      getRandomRange(100, 255, 2) + ',' +
-      getRandomRange(100, 255, 3) + ',0.5)',
-    )
-    overlay.setAttribute('x', 0)
-    overlay.setAttribute('y', 0)
-    box.appendChild(overlay)
+      let seed = pseudoRandom(this.seed)
+      let size = this.size
 
-    if (this.name) {
-      let name = this.name.substring(0, 2).toUpperCase()
+      function getRandomRange (min, max, add = 1000) {
+        return Math.floor(pseudoRandom(seed * add) * (max - min) + min)
+      }
 
-      let initials = document.createTextNode(name)
-      let textOverlay = document.createElementNS(svgns, 'text')
-      textOverlay.setAttribute('width', size)
-      textOverlay.setAttribute('height', size)
-      textOverlay.setAttribute('fill', 'rgba(0,0,0,1)')
-      textOverlay.setAttribute('font-size', blockSize)
-      textOverlay.setAttribute('text-anchor', 'middle')
-      textOverlay.setAttribute('x', blockSize)
-      textOverlay.setAttribute('y', blockSize * 1.3)
-      textOverlay.appendChild(initials)
-      box.appendChild(textOverlay)
-    }
+      const svgns = 'http://www.w3.org/2000/svg'
+      const box = document.createElementNS(svgns, 'svg')
 
-    this.$el.append(box)
+      const rows = 3
+      const columns = 3
+      const blockSize = Math.floor(size / 2)
+      const rotate = blockSize * rows / 2
+
+      box.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+      box.setAttribute('width', size)
+      box.setAttribute('height', size)
+      box.setAttribute('viewbox', '0 0 100 100')
+      box.setAttribute('class', 'box')
+
+      const g = document.createElementNS(svgns, 'g')
+      g.setAttribute(
+        'transform',
+        `translate(${-(rows * blockSize - size) / 2} ${-((rows * blockSize - size) / 2)}) ` +
+        `rotate(${90 * seed} ${rotate} ${rotate})`,
+      )
+
+      for (let i = 0; i < columns; i++) {
+        // noprotect
+        for (let j = 0; j < rows; j++) {
+          let rect = document.createElementNS(svgns, 'rect')
+          rect.setAttribute('width', blockSize)
+          rect.setAttribute('height', blockSize)
+          rect.setAttribute('fill', 'rgba(' +
+            getRandomRange(100, 255, (i + 1) * (j + 1) * 1) + ',' +
+            getRandomRange(100, 255, (i + 1) * (j + 1) * 2) + ',' +
+            getRandomRange(100, 255, (i + 1) * (j + 1) * 3) + ',1)',
+          )
+          rect.setAttribute('x', i * blockSize)
+          rect.setAttribute('y', j * blockSize)
+
+          g.appendChild(rect)
+        }
+      }
+      box.appendChild(g)
+
+      let overlay = document.createElementNS(svgns, 'rect')
+      overlay.setAttribute('width', size)
+      overlay.setAttribute('height', size)
+      overlay.setAttribute('fill', 'rgba(' +
+        getRandomRange(100, 255, 1) + ',' +
+        getRandomRange(100, 255, 2) + ',' +
+        getRandomRange(100, 255, 3) + ',0.5)',
+      )
+      overlay.setAttribute('x', 0)
+      overlay.setAttribute('y', 0)
+      box.appendChild(overlay)
+
+      if (this.initials) {
+        let initials = document.createTextNode(this.initials)
+        let textOverlay = document.createElementNS(svgns, 'text')
+        textOverlay.setAttribute('width', size)
+        textOverlay.setAttribute('height', size)
+        textOverlay.setAttribute('fill', 'rgba(0,0,0,1)')
+        textOverlay.setAttribute('font-size', blockSize)
+        textOverlay.setAttribute('text-anchor', 'middle')
+        textOverlay.setAttribute('x', blockSize)
+        textOverlay.setAttribute('y', blockSize * 1.3)
+        textOverlay.appendChild(initials)
+        box.appendChild(textOverlay)
+      }
+
+      return box
+    },
   },
 }
 </script>

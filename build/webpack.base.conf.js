529cc2fc0fd406e5c8af2992d5266385ca0e8229
Nick Sellen <git@nicksellen.co.uk>
2017-09-20 16:49:34 +0200

    Use hashed webpack filenames

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: config[env.prod ? 'build' : 'dev'].publicPath,
-    filename: 'js/[name].js',
+    filename: 'js/[name].[hash].js',
     chunkFilename: 'js/[id].[chunkhash].js'
   },
   resolve: {

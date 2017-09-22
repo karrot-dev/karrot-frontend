dbe7c8058e7f0fbd78f884488c3319baf01c283a
Tilmann Becker <tilmann.becker@gmail.com>
2017-09-22 21:28:22 +0200

    add user signup actions

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -5,6 +5,11 @@ export const types = {
 
   SELECT_USER: 'Select User',
 
+  REQUEST_USER_SIGNUP: 'Request User Signup',
+  RECEIVE_USER_SIGNUP: 'Receive User Signup',
+  RECEIVE_USER_SIGNUP_ERROR: 'Receiver User Signup Error',
+  CLEAN_SIGNUP: 'Clean Signup',
+
   REQUEST_USERS: 'Request Users',
   RECEIVE_USERS: 'Receive Users',
   RECEIVE_USERS_ERROR: 'Receive Users Error',
@@ -15,6 +20,10 @@ export const state = {
   isFetching: false,
   error: null,
   activeUserId: null,
+  signup: {
+    isWaiting: false,
+    error: null,
+  },
 }
 
 export const getters = {
@@ -24,12 +33,31 @@ export const getters = {
   },
   withLocation: state => state.entries.filter(e => e.longitude && e.latitude),
   activeUser: state => state.activeUserId && indexById(state.entries)[state.activeUserId],
+  signupStatus: state => state.signup,
 }
 
 export const actions = {
-  async selectUser ({ commit, state, dispatch, getters, rootState }, { userId }) {
+  async selectUser ({ commit }, { userId }) {
     console.log('selecting user!', userId)
     commit(types.SELECT_USER, { userId })
+  },
+
+  async signup ({ commit, dispatch }, userData) {
+    commit(types.REQUEST_USER_SIGNUP)
+    try {
+      await users.create(userData)
+      commit(types.RECEIVE_USER_SIGNUP)
+    }
+    catch (error) {
+      commit(types.RECEIVE_USER_SIGNUP_ERROR, { error })
+      return
+    }
+
+    dispatch('auth/login', { email: userData.email, password: userData.password }, { root: true })
+  },
+
+  cleanSignup ({ commit }) {
+    commit(types.CLEAN_SIGNUP)
   },
 
   async fetchList ({ commit }) {
@@ -47,6 +75,34 @@ export const mutations = {
   [types.SELECT_USER] (state, { userId }) {
     state.activeUserId = userId
   },
+
+  // sign user up
+  [types.REQUEST_USER_SIGNUP] (state) {
+    state.signup = {
+      isWaiting: true,
+      error: null,
+    }
+  },
+  [types.RECEIVE_USER_SIGNUP] (state) {
+    state.signup = {
+      isWaiting: false,
+      error: null,
+    }
+  },
+  [types.RECEIVE_USER_SIGNUP_ERROR] (state, { error }) {
+    state.signup = {
+      isWaiting: false,
+      error: error,
+    }
+  },
+  [types.CLEAN_SIGNUP] (state) {
+    state.signup = {
+      isWaiting: false,
+      error: null,
+    }
+  },
+
+  // get users
   [types.REQUEST_USERS] (state) {
     state.isFetching = true
   },

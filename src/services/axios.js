5b947faac71bf5a2ff283ba15e7a91913a921d63
Tilmann Becker <tilmann.becker@gmail.com>
2017-09-22 21:27:27 +0200

    show toast on server and connection error

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,19 +1,36 @@
 import Axios from 'axios'
+import { Toast } from 'quasar'
 
 /*
 * Axios configured for Django REST API
 */
 
-import { camelizeKeys } from '@/services/utils'
+import { camelizeKeys, underscorizeKeys } from '@/services/utils'
 
 const axios = Axios.create({
   xsrfCookieName: 'csrftoken',
   xsrfHeaderName: 'X-CSRFTOKEN',
 })
 
+axios.interceptors.request.use(request => {
+  request.data = underscorizeKeys(request.data)
+  return request
+}, (error) => {
+  Toast.create.warning('Could not connect to the server')
+  return Promise.reject(error)
+})
+
 axios.interceptors.response.use(response => {
   response.data = camelizeKeys(response.data)
   return response
+}, (error) => {
+  if (error.response.status >= 500) {
+    Toast.create.warning('Server error')
+  }
+  else {
+    error.data = camelizeKeys(error.data)
+  }
+  return Promise.reject(error)
 })
 
 export default axios

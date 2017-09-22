56a074c72ab737aeffb5c0572d5f3902542714db
Tilmann Becker <tilmann.becker@gmail.com>
2017-09-22 21:26:17 +0200

    refine login and signup storybook

--- src/components/Login/Login.story.js
+++ src/components/Login/Login.story.js
@@ -1,23 +1,55 @@
 import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
 
 import Login from './Login.vue'
 import Signup from './Signup.vue'
 import i18n from '@/i18n'
 
+const methods = {
+  login: action('login'),
+  signup: action('signup'),
+}
+
 storiesOf('Login & Signup', module)
   .add('Login', () => ({
     components: { Login },
-    template: `<div id="q-app" style="padding: 2em"><Login @loginDo="loginDo" :error="null"/></div>`,
+    template: `<Login @submit="login" :error="null"/>`,
     i18n,
-    methods: {
-      loginDo (email, password) {
-        this.$emit('loginDo', email, password)
-      },
-    },
+    methods,
   }))
 
   .add('Signup', () => ({
     components: { Signup },
-    template: '<div id="q-app" style="padding: 2em"><Signup/></div>',
+    template: '<Signup @submit="signup" :status="status"/>',
+    data () {
+      return {
+        status: { error: null, isWaiting: false },
+      }
+    },
+    methods,
+    i18n,
+  }))
+
+  .add('Signup spinning', () => ({
+    components: { Signup },
+    template: '<Signup @submit="signup" :status="status"/>',
+    data () {
+      return {
+        status: { error: null, isWaiting: true },
+      }
+    },
+    methods,
+    i18n,
+  }))
+
+  .add('Signup error', () => ({
+    components: { Signup },
+    template: '<Signup @submit="signup" :status="status"/>',
+    data () {
+      return {
+        status: { error: 'some error', isWaiting: false },
+      }
+    },
+    methods,
     i18n,
   }))

8dcddb35ca3dd46078b08be4c807843929af5e6c
D0nPiano <spam-mails@abwesend.de>
2017-09-23 23:52:50 +0200

    Solved overflow problem of pickupListItem profiles

--- src/components/ProfilePictures/ProfilesInlineUI.vue
+++ src/components/ProfilePictures/ProfilesInlineUI.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="row justify-start no-wrap">
+  <div class="row justify-start">
+    <ProfilePicture
+      v-for="user in users"
+      v-if="user.id !== currentUser.id"
+      :key="'0' + user.id"
+      :user="user"
+      :size="size"
+      class="profilePic" />
+
     <CurrentUser
       v-if="isUserMember"
       :size="size"
@@ -8,23 +16,21 @@
       class="profilePic"
       v-on:leave="$emit('leave')"/>
 
-    <ProfilePicture
-      v-for="user in users"
-      v-if="user.id !== currentUser.id"
-      :key="'0' + user.id"
-      :user="user"
-      :size="size"
-      class="profilePic" />-
-
     <UserSlot
       v-for="n in emptySlots"
       :key="n"
       :size="size"
-      :showJoin="!isUserMember"
+      :showJoin="!isUserMember && n == 1"
       :popup="joinPopup"
       :currentUser="currentUser"
       class="profilePic"
       v-on:join="$emit('join')"/>
+
+    <div v-if="noNotShownEmptySlots > 0" class="emptySlots" v-bind:style="{ width: size + 'px', height: size + 'px' }">
+       <div></div>
+       <span v-if="noNotShownEmptySlots <= 99">+ {{ noNotShownEmptySlots }}</span>
+       <span v-if="noNotShownEmptySlots > 99">...</span>
+    </div>
   </div>
 </template>
 
@@ -46,16 +52,33 @@ export default {
     size: {
       default: 36,
     },
+    maxEmptyNumToShow: {
+      default: 1,
+    },
+    minSlotsToShow: {
+      default: 6,
+    },
   },
   components: {
     ProfilePicture, UserSlot, CurrentUser,
   },
   computed: {
-    emptySlots () {
+    emptyPlaces () {
       if (this.users) {
-        return Math.max(this.slots - this.users.length, 0)
+        return this.slots - this.users.length
       }
       return 0
+    },
+    emptySlots () {
+      if (this.users) {
+        const minToShow = Math.min(this.maxEmptyNumToShow, this.emptyPlaces)
+        const maxToShow = Math.max(minToShow, this.minSlotsToShow - this.users.length - 1)
+        return Math.min(this.emptyPlaces, maxToShow)
+      }
+      return 0
+    },
+    noNotShownEmptySlots () {
+      return this.emptyPlaces - this.emptySlots
     },
     isUserMember () {
       const currentUser = this.users.filter((el) => el.id === this.currentUser.id)
@@ -67,7 +90,15 @@ export default {
 
 <style scoped lang="stylus">
 .emptySlots
-  margin-right .5em
+    display inline-block
+    border 2px dashed lightgrey
+    color grey
+    border-radius $borderRadius
+    text-align center
+    div
+      display inline-block
+      height 100%
+      vertical-align middle
 .profilePic
   margin-right .3em
 </style>
--- src/components/ProfilePictures/UserSlot.vue
+++ src/components/ProfilePictures/UserSlot.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="user-slot-wrapper" v-bind:style="{ width: size + 'px', height: size + 'px' }">
+  <div class="user-slot-wrapper" v-bind:class="{ greyedOut: !showJoin, active: showJoin }" v-bind:style="{ width: size + 'px', height: size + 'px' }">
     <div v-bind:class="{ hoverHide: showJoin }">
     </div>
     <div v-if="showJoin" v-bind:class="{ hoverShow: showJoin }">
@@ -37,20 +37,26 @@ export default {
 @import '~variables'
 
   .user-slot-wrapper
-    cursor pointer
-    display inline-block
     border 2px dashed
     border-radius $borderRadius
-    text-align center
+    margin-right 3.8px
+    margin-bottom 3.8px
     span
       vertical-align middle
       font-weight 600
+  .user-slot-wrapper.active
+    cursor pointer
+    display inline-block
+    text-align center
     .hoverShow
       display none
-  .user-slot-wrapper:hover
+  .user-slot-wrapper.active:hover
     border: 0
     .hoverHide
       display none
     .hoverShow
       display inline-block
+  .greyedOut
+    border-color lightgrey
+    cursor ini
 </style>

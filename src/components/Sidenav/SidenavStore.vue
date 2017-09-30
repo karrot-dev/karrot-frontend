fad50a65914ec222bf21308f80965db39d8a191c
Tilmann Becker <tilmann.becker@gmail.com>
2017-09-30 16:09:16 +0200

    create sidenavstore connector

--- src/components/Sidenav/SidenavStore.vue
+++ src/components/Sidenav/SidenavStore.vue
@@ -1,37 +1,10 @@
-<template>
-  <SidenavBox>
-    <div slot="name"><i class="fa fa-fw fa-home"></i>Store Options</div>
-    <div slot="tools">
-      <q-btn flat>
-        <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-        <q-popover fit ref="popover">
-          <GroupOptions/>
-        </q-popover>
-      </q-btn>
-    </div>
-    <div>
-      <q-list highlight no-border>
-        <q-item :to="{name: 'storePickups', params: { storeId }}">{{ $t("GROUP.PICKUPS")}}</q-item>
-        <q-item :to="{name: 'storeHistory', params: { storeId }}">{{ $t("GROUP.HISTORY")}}</q-item>
-      </q-list>
-    </div>
-  </SidenavBox>
-</template>
-
 <script>
-import { QBtn, QList, QItem, QPopover } from 'quasar'
-import SidenavBox from './SidenavBox.vue'
-import GroupOptions from './GroupOptions.vue'
+import { connect } from 'vuex-connect'
+import SidenavStoreUI from './SidenavStoreUI.vue'
 
-export default {
-  props: {
-    storeId: { required: true },
+export default connect({
+  gettersToProps: {
+    storeId: 'stores/activeStoreId',
   },
-  components: {
-    SidenavBox, GroupOptions, QBtn, QList, QItem, QPopover,
-  },
-}
+})('SidenavStoreUI', SidenavStoreUI)
 </script>
-
-<style scoped lang="stylus">
-</style>
---
+++ src/components/Sidenav/SidenavStoreUI.vue
@@ -0,0 +1,37 @@
+<template>
+  <SidenavBox>
+    <div slot="name"><i class="fa fa-fw fa-home"></i>Store Options</div>
+    <div slot="tools">
+      <q-btn flat>
+        <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+        <q-popover fit ref="popover">
+          <GroupOptions/>
+        </q-popover>
+      </q-btn>
+    </div>
+    <div>
+      <q-list highlight no-border>
+        <q-item :to="{name: 'storePickups', params: { storeId }}">{{ $t("GROUP.PICKUPS")}}</q-item>
+        <q-item :to="{name: 'storeHistory', params: { storeId }}">{{ $t("GROUP.HISTORY")}}</q-item>
+      </q-list>
+    </div>
+  </SidenavBox>
+</template>
+
+<script>
+import { QBtn, QList, QItem, QPopover } from 'quasar'
+import SidenavBox from './SidenavBox.vue'
+import GroupOptions from './GroupOptions.vue'
+
+export default {
+  props: {
+    storeId: { required: true },
+  },
+  components: {
+    SidenavBox, GroupOptions, QBtn, QList, QItem, QPopover,
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+</style>

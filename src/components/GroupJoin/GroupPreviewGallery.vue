c415ac8f21660d70267794a372be4a6f013800c3
Tilmann Becker <tilmann.becker@gmail.com>
2017-09-30 12:49:49 +0200

    fix comparison

--- src/components/GroupJoin/GroupPreviewGallery.vue
+++ src/components/GroupJoin/GroupPreviewGallery.vue
@@ -13,7 +13,7 @@
         v-for="group in myGroups"
         :key="group.id"
         class="inline-block col-xs-12 col-sm-6 col-md-4 items-stretch">
-        <GroupPreviewCard :class="{highlight: group.id == currentGroupId}" :group="group" :isMember="true" @preview="$emit('preview', { groupId: group.id })" @visit="$emit('visit', { groupId: group.id })" />
+        <GroupPreviewCard :class="{highlight: group.id === currentGroupId}" :group="group" :isMember="true" @preview="$emit('preview', { groupId: group.id })" @visit="$emit('visit', { groupId: group.id })" />
       </div>
     </div>
     <h4 class="text-primary" v-if="otherGroups.length>0">

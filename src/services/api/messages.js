380b07bff48a4c9932b2cceee6bb30e5ee1d63d0
Tilmann Becker <tilmann.becker@gmail.com>
2017-10-01 21:24:32 +0200

    no need to parse initial prev cursor

--- src/services/api/messages.js
+++ src/services/api/messages.js
@@ -11,7 +11,6 @@ export default {
     return {
       ...response,
       next: parseCursor(response.next),
-      prev: parseCursor(response.prev),
       results: convertDate(response.results),
     }
   },

462118b2d832816842405e0fc9a97f2309ece6d0
Tilmann Becker <tilmann.becker@gmail.com>
2017-10-02 16:24:27 +0200

    add simple history detail modal

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -15,7 +15,7 @@
         />
       </template>
       <template slot='col-actions' scope='cell'>
-        <q-btn color="primary" @click="$emit('info', cell.row.id)" small>Info</q-btn>
+        <q-btn color="primary" @click="showDetails(cell.row)" small>Info</q-btn>
       </template>
     </q-data-table>
     <q-btn color="primary" @click="$emit('more')" :disabled="!canLoadMore" loader :value="status.isWaiting">
@@ -27,12 +27,19 @@
       </span>
     </q-btn>
     <pre>{{ status.error }}</pre>
+
+    <q-modal ref="detailModal" :content-css="{padding: '50px', minWidth: '50vw'}">
+      <span class="caption text-italic">still in development</span>
+      <h4>Details</h4>
+      <pre>{{ modalData }}</pre>
+      <q-btn color="primary" @click="$refs.detailModal.close()">Close</q-btn>
+    </q-modal>
   </div>
 </template>
 
 <script>
 import ProfilePicture from '@/components/ProfilePictures/ProfilePicture.vue'
-import { QBtn, QDataTable } from 'quasar'
+import { QBtn, QDataTable, QModal } from 'quasar'
 
 export default {
   data () {
@@ -88,6 +95,7 @@ export default {
           options: [5, 10, 15, 30, 50, 500],
         },
       },
+      modalData: null,
     }
   },
   props: {
@@ -95,7 +103,13 @@ export default {
     status: { required: true },
     canLoadMore: { required: true },
   },
-  components: { QBtn, QDataTable, ProfilePicture },
+  methods: {
+    showDetails (entry) {
+      this.modalData = entry
+      this.$refs.detailModal.open()
+    },
+  },
+  components: { QBtn, QDataTable, ProfilePicture, QModal },
 }
 </script>
 

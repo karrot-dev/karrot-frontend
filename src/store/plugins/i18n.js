32569f51c86ad70b913a9d28caa43dbf9f09d63f
Tilmann Becker <tilmann.becker@gmail.com>
2017-10-02 10:39:10 +0200

    add example

--- src/store/plugins/i18n.js
+++ src/store/plugins/i18n.js
@@ -2,6 +2,7 @@ import i18n from '@/i18n'
 import { isObject, camelize } from '@/services/utils'
 
 // Hotfix to use existing files
+// e.g. converts translations containing {{store_name}} into {storeName}
 // TODO: convert files and sync them with transifex
 function angularToVueI18n (val) {
   if (isObject(val)) {

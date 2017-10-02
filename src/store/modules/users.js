3a47c81dbfbe364cb4fa833fd05ed243ec3deadb
Nick Sellen <git@nicksellen.co.uk>
2017-10-02 12:46:47 +0200

    Don't depend on store

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,5 +1,13 @@
 import users from '@/services/api/users'
-import { indexById } from '@/store/helpers'
+
+// moved from helpers to avoid loading store
+// TODO: put it somewhere general it will not pull in store!
+export function indexById (iterable) {
+  return iterable.reduce((acc, cur, i) => {
+    acc[cur.id] = cur
+    return acc
+  }, {})
+}
 
 export const types = {
 

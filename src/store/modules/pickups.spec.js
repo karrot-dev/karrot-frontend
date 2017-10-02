b72f7561768441da55574f150a58808b05ef70a5
Nick Sellen <git@nicksellen.co.uk>
2017-10-02 11:10:03 +0200

    Add more pickup tests

--- src/store/modules/pickups.spec.js
+++ src/store/modules/pickups.spec.js
@@ -1,4 +1,8 @@
+const mockGet = jest.fn()
+const mockJoin = jest.fn()
 jest.mock('@/services/api/pickups', () => ({
+  get: mockGet,
+  join: mockJoin,
 }))
 
 import { createStore } from '>/helpers'
@@ -8,20 +12,18 @@ describe('pickups', () => {
 
   let storeMocks
   let vstore
+  let type
 
   let userId = 10
-  let store = {
-    id: 20,
-  }
 
   let pickup1
   let pickup2
   let pickup3
 
   beforeEach(() => {
-    pickup1 = { id: 1, store: store.id, collectorIds: [] }
-    pickup2 = { id: 2, store: store.id, collectorIds: [userId] }
-    pickup3 = { id: 3, store: store.id, collectorIds: [userId] }
+    pickup1 = { id: 1, store: 10, date: new Date(), collectorIds: [] }
+    pickup2 = { id: 2, store: 11, date: new Date(), collectorIds: [userId] }
+    pickup3 = { id: 3, store: 12, date: new Date(), collectorIds: [userId] }
   })
 
   describe('logged in', () => {
@@ -29,6 +31,7 @@ describe('pickups', () => {
       storeMocks = {
         auth: {
           getters: {
+            isLoggedIn: () => true,
             userId: () => userId,
           },
         },
@@ -47,21 +50,73 @@ describe('pickups', () => {
           },
         },
       }
+      let pickups = require('./pickups')
+      type = name => `pickups/${pickups.types[name]}`
       vstore = createStore({
-        pickups: require('./pickups'),
+        pickups,
         ...storeMocks,
       })
     })
 
     beforeEach(() => {
-      vstore.commit('pickups/Receive List', { pickups: [pickup1, pickup2, pickup3] })
+      vstore.commit(type('RECEIVE_LIST'), { pickups: [pickup1, pickup2, pickup3] })
+    })
+
+    it('can enrich', async () => {
+      expect(vstore.getters['pickups/enrich'](pickup2)).toEqual({
+        ...pickup2,
+        store: { id: pickup2.store },
+        isUserMember: true,
+        isWaiting: false,
+        collectors: [{ id: userId, name: `Some Name${userId}` }],
+      })
     })
 
     it('can fetch list', async () => {
-      // Currently only checking very basic way...
-      // TODO: test the enriched version
-      expect(vstore.getters['pickups/all'].map(pickup => pickup.id))
-        .toEqual([pickup1, pickup2, pickup3].map(pickup => pickup.id))
+      expect(vstore.getters['pickups/all'].map(getId)).toEqual([pickup1, pickup2, pickup3].map(getId))
+    })
+
+    it('can get empty pickups', async () => {
+      expect(vstore.getters['pickups/empty'].map(getId)).toEqual([pickup1].map(getId))
+    })
+
+    it('can get my pickups', async () => {
+      expect(vstore.getters['pickups/mine'].map(getId)).toEqual([pickup2, pickup3].map(getId))
+    })
+
+    it('can get filtered', async () => {
+      vstore.commit(type('SET_STORE_ID_FILTER'), { storeId: pickup2.store })
+      expect(vstore.getters['pickups/filtered'].map(getId)).toEqual([pickup2].map(getId))
+    })
+
+    it('can fetch a pickup', async () => {
+      let date = new Date()
+      let pickupId = 99
+      let storeId = 101
+      mockGet.mockImplementationOnce(id => ({ id, date, store: storeId, collectorIds: [] }))
+      await vstore.dispatch('pickups/fetch', pickupId)
+      expect(vstore.getters['pickups/get'](pickupId)).toEqual({
+        id: pickupId,
+        collectorIds: [],
+        collectors: [],
+        date,
+        isUserMember: false,
+        isWaiting: false,
+        store: {
+          id: storeId,
+        },
+      })
+    })
+
+    it('can join a pickup', async () => {
+      expect(vstore.getters['pickups/mine'].map(getId)).not.toContain(pickup1.id)
+      await vstore.dispatch('pickups/join', pickup1.id)
+      expect(vstore.getters['pickups/mine'].map(getId)).toContain(pickup1.id)
+      expect(mockJoin).toBeCalledWith(pickup1.id)
     })
   })
 })
+
+function getId ({ id }) {
+  return id
+}

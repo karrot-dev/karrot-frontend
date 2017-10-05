896790b519c1066fbeb62a0b275416618ddabbfa
Nick Sellen <git@nicksellen.co.uk>
2017-10-06 00:50:10 +0200

    Simplify GroupMap

--- src/components/Map/GroupMap.vue
+++ src/components/Map/GroupMap.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-    <!--div v-for="(marker, idx) in markers">
-      lat-lng: {{marker.latLng}}
-      icon {{marker.icon}}
-    </div>-->
     <v-map :zoom="zoom" :bounds="bounds" :center="center">
       <v-tile-layer :url="url" :attribution="attribution"></v-tile-layer>
-      <v-marker v-for="marker in storeMarkers" :key="marker.id" :lat-lng="marker.latLng" :icon="marker.icon">
-        <v-popup :content="marker.popupcontent"></v-popup>
-      </v-marker>
-      <v-marker v-for="marker in userMarkers" :key="marker.id" :lat-lng="marker.latLng" :icon="marker.icon">
+      <v-marker v-for="marker in markers" :key="marker.id" :lat-lng="marker.latLng" :icon="marker.icon">
         <v-popup :content="marker.popupcontent"></v-popup>
       </v-marker>
     </v-map>
@@ -55,43 +48,41 @@ export default {
     showStores: { required: false, default: true },
   },
   computed: {
-    storeMarkers () {
-      if (!this.showStores) return []
-      return this.stores.filter(hasLocation).map(store => {
-        return {
-          latLng: L.latLng(store.latitude, store.longitude),
-          id: 'store_' + store.id,
-          icon: L.AwesomeMarkers.icon({
-            icon: 'shopping-cart',
-            markerColor: 'blue',
-            prefix: 'fa',
-          }),
-          popupcontent: `<a href="/#/group/${store.group}/store/${store.id}">${store.name}</a>`,
-        }
-      })
-    },
-    userMarkers () {
-      if (!this.showUsers) return []
-      return this.users.filter(hasLocation).map(user => {
-        return {
-          latLng: L.latLng(user.latitude, user.longitude),
-          id: 'user_' + user.id,
-          icon: L.AwesomeMarkers.icon({
-            icon: 'user',
-            markerColor: 'green',
-            prefix: 'fa',
-          }),
-          popupcontent: `<a href="/#/user/${user.id}">${user.displayName}</a>`,
-        }
-      })
+    markers () {
+      let items = []
+      if (this.showStores) {
+        items.push(...this.stores.filter(hasLocation).map(store => {
+          return {
+            latLng: L.latLng(store.latitude, store.longitude),
+            id: 'store_' + store.id,
+            icon: L.AwesomeMarkers.icon({
+              icon: 'shopping-cart',
+              markerColor: 'blue',
+              prefix: 'fa',
+            }),
+            popupcontent: `<a href="/#/group/${store.group}/store/${store.id}">${store.name}</a>`,
+          }
+        }))
+      }
+      if (this.showUsers) {
+        items.push(...this.users.filter(hasLocation).map(user => {
+          return {
+            latLng: L.latLng(user.latitude, user.longitude),
+            id: 'user_' + user.id,
+            icon: L.AwesomeMarkers.icon({
+              icon: 'user',
+              markerColor: 'green',
+              prefix: 'fa',
+            }),
+            popupcontent: `<a href="/#/user/${user.id}">${user.displayName}</a>`,
+          }
+        }))
+      }
+      return items
     },
     bounds () {
-      let markers = [...this.storeMarkers]
-      if (markers.length === 0) {
-        markers = [...this.storeMarkers, ...this.userMarkers]
-      }
-      if (markers.length > 0) {
-        return L.latLngBounds(markers.map(m => m.latLng)).pad(0.2)
+      if (this.markers.length > 0) {
+        return L.latLngBounds(this.markers.map(m => m.latLng)).pad(0.2)
       }
       else {
         return L.latLngBounds()

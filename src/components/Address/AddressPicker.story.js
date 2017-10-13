b1cd3280739695c855961b8880182808bb6bab07
Nick Sellen <git@nicksellen.co.uk>
2017-10-13 19:59:40 +0200

    Fixup address picker storybook

--- src/components/Address/AddressPicker.story.js
+++ src/components/Address/AddressPicker.story.js
@@ -5,5 +5,27 @@ import AddressPicker from './AddressPicker.vue'
 storiesOf('AddressPicker', module)
   .add('Default', () => ({
     components: { AddressPicker },
-    template: '<AddressPicker />',
+    template: '<AddressPicker v-model="item" />',
+    data () {
+      return {
+        item: {
+          address: '',
+          latitude: '',
+          longitude: '',
+        },
+      }
+    },
+  }))
+  .add('With map', () => ({
+    components: { AddressPicker },
+    template: '<AddressPicker v-model="item" :map="true" />',
+    data () {
+      return {
+        item: {
+          address: '',
+          latitude: '',
+          longitude: '',
+        },
+      }
+    },
   }))

5b8fb14dd2ae02f9254115b818419fbc119914cb
Tilmann Becker <tilmann.becker@gmail.com>
2017-10-26 19:26:43 +0200

    wrap pickupedit in form

--- src/components/Pickups/PickupEdit.vue
+++ src/components/Pickups/PickupEdit.vue
@@ -1,54 +1,55 @@
 <template>
   <div class="edit" :class="{ changed: hasChanged }">
-    <q-field
-      icon="access time"
-      :label="$t('CREATEPICKUP.TIME')"
-      :helper="$t('CREATEPICKUP.TIME_HELPER')"
-      :error="!!requestError('time')"
-      :error-label="requestError('time')"
-      >
-      <q-datetime type="time"
-                  v-model="pickupEdit.date"
-                  :format24h="is24h"
-                  :display-value="$d(pickupEdit.date, 'timeShort')"/>
-    </q-field>
+    <form @submit="save">
+      <q-field
+        icon="access time"
+        :label="$t('CREATEPICKUP.TIME')"
+        :helper="$t('CREATEPICKUP.TIME_HELPER')"
+        :error="!!requestError('time')"
+        :error-label="requestError('time')"
+        >
+        <q-datetime type="time"
+                    v-model="pickupEdit.date"
+                    :format24h="is24h"
+                    :display-value="$d(pickupEdit.date, 'timeShort')"/>
+      </q-field>
 
-    <q-field
-      icon="today"
-      :label="$t('CREATEPICKUP.DATE')"
-      :helper="$t('CREATEPICKUP.DATE_HELPER')"
-      :error="!!requestError('date')"
-      :error-label="requestError('date')"
-      >
-      <q-datetime type="date" v-model="pickupEdit.date" :display-value="$d(pickupEdit.date, 'dateShort')"/>
-    </q-field>
+      <q-field
+        icon="today"
+        :label="$t('CREATEPICKUP.DATE')"
+        :helper="$t('CREATEPICKUP.DATE_HELPER')"
+        :error="!!requestError('date')"
+        :error-label="requestError('date')"
+        >
+        <q-datetime type="date" v-model="pickupEdit.date" :display-value="$d(pickupEdit.date, 'dateShort')"/>
+      </q-field>
 
-    <q-field
-      icon="group"
-      :label="$t('CREATEPICKUP.MAX_COLLECTORS')"
-      :helper="$t('CREATEPICKUP.MAX_COLLECTORS_HELPER')"
-      :error="!!requestError('maxCollectors')"
-      :error-label="requestError('maxCollectors')"
-      >
-      <q-slider v-model="pickupEdit.maxCollectors" :min="1" :max="10" label label-always />
-    </q-field>
+      <q-field
+        icon="group"
+        :label="$t('CREATEPICKUP.MAX_COLLECTORS')"
+        :helper="$t('CREATEPICKUP.MAX_COLLECTORS_HELPER')"
+        :error="!!requestError('maxCollectors')"
+        :error-label="requestError('maxCollectors')"
+        >
+        <q-slider v-model="pickupEdit.maxCollectors" :min="1" :max="10" label label-always />
+      </q-field>
 
-    <q-field
-      icon="info"
-      :label="$t('CREATEPICKUP.COMMENT')":helper="$t('CREATEPICKUP.COMMENT_HELPER')"
-      :error="!!requestError('description')"
-      :error-label="requestError('description')"
-      >
-      <q-input v-model="pickupEdit.description" type="textarea" :min-rows="1" :max-height="100" />
-    </q-field>
+      <q-field
+        icon="info"
+        :label="$t('CREATEPICKUP.COMMENT')":helper="$t('CREATEPICKUP.COMMENT_HELPER')"
+        :error="!!requestError('description')"
+        :error-label="requestError('description')"
+        >
+        <q-input v-model="pickupEdit.description" type="textarea" :min-rows="1" :max-height="100" />
+      </q-field>
 
-    <div class="text-negative">{{ requestError('nonFieldErrors') }}</div>
+      <div class="text-negative">{{ requestError('nonFieldErrors') }}</div>
 
-    <q-btn color="primary" @click="save" :disable="!isNew && !hasChanged">{{ $t(isNew ? 'BUTTON.CREATE' : 'BUTTON.SAVE_CHANGES') }}</q-btn>
-    <q-btn @click="reset" v-if="!isNew" :disable="!hasChanged">{{ $t('BUTTON.RESET') }}</q-btn>
-    <q-btn @click="$emit('cancel')" v-if="isNew">{{ $t('BUTTON.CANCEL') }}</q-btn>
-    <q-btn color="red" @click="destroy" v-if="!isNew && !pickup.series">{{ $t('BUTTON.DELETE') }}</q-btn>
-
+      <q-btn type="submit" color="primary" :disable="!isNew && !hasChanged">{{ $t(isNew ? 'BUTTON.CREATE' : 'BUTTON.SAVE_CHANGES') }}</q-btn>
+      <q-btn type="button" @click="reset" v-if="!isNew" :disable="!hasChanged">{{ $t('BUTTON.RESET') }}</q-btn>
+      <q-btn type="button" @click="$emit('cancel')" v-if="isNew">{{ $t('BUTTON.CANCEL') }}</q-btn>
+      <q-btn type="button" color="red" @click="destroy" v-if="!isNew && !pickup.series">{{ $t('BUTTON.DELETE') }}</q-btn>
+    </form>
   </div>
 </template>
 
--- src/components/Pickups/PickupSeriesEdit.vue
+++ src/components/Pickups/PickupSeriesEdit.vue
@@ -1,56 +1,56 @@
 <template>
   <div class="edit" :class="{ changed: hasChanged }">
+    <form @submit="save">
+      <q-field
+        icon="access time"
+        :label="$t('CREATEPICKUP.TIME')"
+        :helper="$t('CREATEPICKUP.TIME_HELPER')"
+        :error="!!requestError('startDate')"
+        :error-label="requestError('startDate')"
+        >
+        <q-datetime type="time"
+                    v-model="seriesEdit.startDate"
+                    :format24h="is24h"
+                    :display-value="$d(seriesEdit.startDate, 'timeShort')"/>
+      </q-field>
 
-    <q-field
-      icon="access time"
-      :label="$t('CREATEPICKUP.TIME')"
-      :helper="$t('CREATEPICKUP.TIME_HELPER')"
-      :error="!!requestError('startDate')"
-      :error-label="requestError('startDate')"
-      >
-      <q-datetime type="time"
-                  v-model="seriesEdit.startDate"
-                  :format24h="is24h"
-                  :display-value="$d(seriesEdit.startDate, 'timeShort')"/>
-    </q-field>
+      <q-field
+        icon="today"
+        label="Weekdays"
+        helper="On which weekdays should the pick-up take place?"
+        :error="!!requestError('rule')"
+        :error-label="requestError('rule')"
+        >
+        <q-select multiple toggle v-model="seriesEdit.rule.byDay" :options="dayOptions"/>
+      </q-field>
 
-    <q-field
-      icon="today"
-      label="Weekdays"
-      helper="On which weekdays should the pick-up take place?"
-      :error="!!requestError('rule')"
-      :error-label="requestError('rule')"
-      >
-      <q-select multiple toggle v-model="seriesEdit.rule.byDay" :options="dayOptions"/>
-    </q-field>
+      <q-field
+        icon="group"
+        :label="$t('CREATEPICKUP.MAX_COLLECTORS')"
+        :helper="$t('CREATEPICKUP.MAX_COLLECTORS_HELPER')"
+        :error="!!requestError('maxCollectors')"
+        :error-label="requestError('maxCollectors')"
+        >
+        <q-slider v-model="seriesEdit.maxCollectors" :min="1" :max="10" label label-always />
+      </q-field>
 
-    <q-field
-      icon="group"
-      :label="$t('CREATEPICKUP.MAX_COLLECTORS')"
-      :helper="$t('CREATEPICKUP.MAX_COLLECTORS_HELPER')"
-      :error="!!requestError('maxCollectors')"
-      :error-label="requestError('maxCollectors')"
-      >
-      <q-slider v-model="seriesEdit.maxCollectors" :min="1" :max="10" label label-always />
-    </q-field>
+      <q-field
+        icon="info"
+        :label="$t('CREATEPICKUP.COMMENT')"
+        :helper="$t('CREATEPICKUP.COMMENT_HELPER')"
+        :error="!!requestError('description')"
+        :error-label="requestError('description')"
+        >
+        <q-input v-model="seriesEdit.description" type="textarea" :min-rows="1" :max-height="100" />
+      </q-field>
 
-    <q-field
-      icon="info"
-      :label="$t('CREATEPICKUP.COMMENT')"
-      :helper="$t('CREATEPICKUP.COMMENT_HELPER')"
-      :error="!!requestError('description')"
-      :error-label="requestError('description')"
-      >
-      <q-input v-model="seriesEdit.description" type="textarea" :min-rows="1" :max-height="100" />
-    </q-field>
+      <div class="text-negative">{{ requestError('nonFieldErrors') }}</div>
 
-    <div class="text-negative">{{ requestError('nonFieldErrors') }}</div>
-
-    <q-btn color="primary" @click="save" :disable="!isNew && !hasChanged">{{ $t(isNew ? 'BUTTON.CREATE' : 'BUTTON.SAVE_CHANGES') }}</q-btn>
-    <q-btn @click="reset" v-if="!isNew" :disable="!hasChanged">{{ $t('BUTTON.RESET') }}</q-btn>
-    <q-btn @click="$emit('cancel')" v-if="isNew">{{ $t('BUTTON.CANCEL') }}</q-btn>
-    <q-btn color="red" @click="destroy" v-if="!isNew">{{ $t('BUTTON.DELETE') }}</q-btn>
-
+      <q-btn type="submit" color="primary" :disable="!isNew && !hasChanged">{{ $t(isNew ? 'BUTTON.CREATE' : 'BUTTON.SAVE_CHANGES') }}</q-btn>
+      <q-btn type="button" @click="reset" v-if="!isNew" :disable="!hasChanged">{{ $t('BUTTON.RESET') }}</q-btn>
+      <q-btn type="button" @click="$emit('cancel')" v-if="isNew">{{ $t('BUTTON.CANCEL') }}</q-btn>
+      <q-btn type="button" color="red" @click="destroy" v-if="!isNew">{{ $t('BUTTON.DELETE') }}</q-btn>
+    </form>
   </div>
 </template>
 

f6ab2fdbbf57b00f2d5b2a1b619dff6aefaafe76
Nick Sellen <git@nicksellen.co.uk>
2017-10-27 22:19:31 +0200

    Load drf session auth first

--- config/settings.py
+++ config/settings.py
@@ -65,8 +65,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 

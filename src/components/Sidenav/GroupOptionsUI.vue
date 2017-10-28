944505d6fceaa3287b517832b30247678051e357
Tilmann Becker <tilmann.becker@gmail.com>
2017-10-28 11:28:07 +0200

    close popover after click
    need to use the native click event on router-links

--- src/components/Sidenav/GroupOptionsUI.vue
+++ src/components/Sidenav/GroupOptionsUI.vue
@@ -1,15 +1,15 @@
 <template>
   <q-popover fit ref="popover">
     <q-list item-separator link>
-      <q-item :to="{name: 'groupEdit', params: {groupId: activeGroupId}}">
+      <q-item :to="{name: 'groupEdit', params: {groupId: activeGroupId}}" @click.native="$refs.popover.close()">
         <q-icon size="1em" name="fa-pencil fa-fw on-left" />
         {{$t("GROUP.EDIT")}}
       </q-item>
-      <q-item :to="{name: 'groupInfo', params: {groupInfoId: activeGroupId}}" @click="$refs.popover.close()">
+      <q-item :to="{name: 'groupInfo', params: {groupInfoId: activeGroupId}}" @click.native="$refs.popover.close()">
         <q-icon size="1em" name="fa-info-circle fa-fw on-left" />
         {{$t("GROUPINFO.META")}}
       </q-item>
-      <q-item :to="{name: 'groupInvitations', params: {groupId: activeGroupId}}" @click="$refs.popover.close()">
+      <q-item :to="{name: 'groupInvitations', params: {groupId: activeGroupId}}" @click.native="$refs.popover.close()">
         <q-icon size="1em" name="fa-user-plus fa-fw on-left" />
         {{$t("GROUP.INVITE_TITLE")}}
       </q-item>

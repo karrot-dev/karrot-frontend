f5c4ea7415cc1b5e901f987490e0d237c5698143
Nick Sellen <git@nicksellen.co.uk>
2017-10-28 22:54:47 +0200

    Fix group serializer test

--- foodsaving/groups/tests/test_serializers.py
+++ foodsaving/groups/tests/test_serializers.py
@@ -12,7 +12,7 @@ class TestGroupSerializer(TestCase):
 
     def test_detail(self):
         serializer = GroupDetailSerializer(self.group)
-        self.assertEqual(len(serializer.data.keys()), 12)
+        self.assertEqual(len(serializer.data.keys()), 13)
         self.assertEqual(serializer.data['id'], self.group.id)
         self.assertEqual(serializer.data['name'], self.group.name)
         self.assertEqual(serializer.data['description'], self.group.description)

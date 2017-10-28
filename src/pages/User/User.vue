e43acd65a422e081511641c6ece65ec350f7d2cd
Andreas Langecker <a.langecker@posteo.de>
2017-10-28 11:39:11 +0200

    seperated user profile

--- src/pages/User/User.vue
+++ src/pages/User/User.vue
@@ -1,54 +1,6 @@
 <template>
   <div v-if="user">
-    <div class="row margin-sides no-wrap">
-      <div class="">
-        <q-transition duration="510" name="turn-in" appear>
-          <div class="photoCard shadow-2">
-            <ProfilePicture :user="user" :size="profilePictureSize"/>
-          </div>
-        </q-transition>
-      </div>
-      <div style="overflow: hidden">
-        <h1 class="accent-font">{{user.displayName}}</h1>
-        <p class="subtitle">
-          <span v-for="(group, indx) in groups" :key="group.id">
-            <router-link :to="{name: 'group', params: { groupId: group.id }}">
-              {{ group.name }}
-            </router-link>
-            <span v-if="groups.length !== indx + 1">,</span>
-          </span>
-        </p>
-      </div>
-    </div>
-    <q-card class="generic-padding">
-      <UserMapPreview v-if="user.latitude && user.longitude" :user="user"class="map"/>
-      <div class="info">
-        <div class="info-item">
-          <strong><i class="fa fa-fw fa-envelope-o on-left"></i> </strong>
-          {{ user.email }}
-          <span v-if="user.email !== user.unverifiedEmail">
-            <i class="fa fa-arrow-right"/>
-            <router-link :to="{name: 'settings'}">{{ user.unverifiedEmail }}</router-link>
-          </span>
-        </div>
-        <div class="info-item" v-if="user.address">
-          <strong class="info-item"><i class="fa fa-fw fa-map-marker on-left"></i> </strong>
-          {{ user.address }}
-        </div>
-      </div>
-      <q-card-separator v-if="user.description != ''"/><br/>
-      <Markdown
-        v-if="user.description != ''"
-        :source="user.description" />
-      <!-- <q-card-separator />
-      <q-card-actions>
-        <q-btn flat disabled>
-          <i class="fa fa-comment on-left"/>
-          Message
-      </q-btn>
-      </q-card-actions>
-      <div style="clear: both"/> -->
-    </q-card>
+    <UserProfile :user="user" :groups="groups" />
     <q-card>
       <q-card-title>History</q-card-title>
       <History :user="user" />
@@ -57,70 +9,22 @@
 </template>
 
 <script>
-import {
-  mapGetters,
-} from 'vuex'
-
-import Markdown from '@/components/Markdown.vue'
-import ProfilePicture from '@/components/ProfilePictures/ProfilePicture'
-import UserMapPreview from '@/components/Map/UserMapPreview'
+import { mapGetters } from 'vuex'
+import UserProfile from '@/components/User/Profile'
 import History from '@/components/History/HistoryList.vue'
-
-import { QCard, QCardTitle, QTransition, QCardActions, QBtn, QCardSeparator } from 'quasar'
+import { QCard, QCardTitle } from 'quasar'
 
 export default {
-  components: { History, Markdown, UserMapPreview, QCard, QCardTitle, QTransition, QCardActions, QBtn, QCardSeparator, ProfilePicture },
+  components: { History, UserProfile, QCard, QCardTitle },
   computed: {
     ...mapGetters({
       user: 'users/activeUser',
       groups: 'groups/activeUserGroups',
     }),
-    profilePictureSize () {
-      if (this.$q.platform.is.mobile) {
-        return 60
-      }
-      return 90
-    },
   },
 }
 </script>
 
 <style scoped lang="stylus">
-h1, p.subtitle
-  padding-left 30px
-  margin 0
-p.subtitle
-  margin .5em .5em .5em 0
-  text-overflow ellipsis
-  overflow hidden
-  width 100%
-.margin-sides
-  margin 2em 2em 1em 2em
-.photoCard
-  margin-top .3em
-  padding .5em
-  background-color white
-  transform rotate(-3deg)
-.map
-  height 200px
-  width 200px
-  float right
-
-body.mobile .map
-  height 150px
-  width 150px
-.info
-  margin-bottom 1em
-  .info-item
-    margin-bottom 8px
-
-.turn-in-enter
-  transform rotate(-15deg)
-
-.turn-in-leave-active, .turn-in-enter-active
-  transition: all .5s ease
-
-.turn-in-enter-to
-  transform rotate(-3deg)
 
 </style>

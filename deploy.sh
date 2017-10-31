5a859d5d4b25829426dc82bd504dd1ad24fdf5ec
Nick Sellen <git@nicksellen.co.uk>
2017-10-31 18:28:37 +0100

    Deploy production branch using systemd
    with @tiltec

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,5 @@ fi
 
 echo "deploying branch [$BRANCH] to [$HOST]"
 
-if [ "$BRANCH" = "production" ]; then
-  scp deploy/deploy-uwsgi.sh deploy@$HOST:deploy-wsgi.sh
-  ssh deploy@$HOST ./deploy-wsgi.sh "$BRANCH"
-else
-  scp deploy/deploy-systemd.sh deploy@$HOST:deploy-systemd.sh
-  ssh deploy@$HOST ./deploy-systemd.sh "$BRANCH"
-fi
+scp deploy/deploy-systemd.sh deploy@$HOST:deploy-systemd.sh
+ssh deploy@$HOST ./deploy-systemd.sh "$BRANCH"

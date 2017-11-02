986950f99906c4e6e1930a0c093a01da19af2635
Tilmann Becker <tilmann.becker@gmail.com>
2017-11-02 14:00:10 +0100

    introduce helper for request states

--- src/store/helpers.js
+++ src/store/helpers.js
@@ -3,6 +3,8 @@
  * If a helper depends on the vuex store, put it in storeHelpers.js
  */
 
+import Vue from 'vue'
+
 /**
  * Returns an object that maps entries of iterables by their `id` field
  *
@@ -24,5 +26,104 @@ export function onlyHandleAPIError (error, handleFn) {
   else {
     handleFn({ error })
     throw error
+  }
+}
+
+export function defineRequestModule ({ namespace = 'meta' } = {}) {
+  const types = {
+    REQUEST: 'Request',
+    RECEIVE_SUCCESS: 'Receive Success',
+    RECEIVE_ERROR: 'Receive Error',
+    CLEAR: 'Clear',
+  }
+
+  const state = {
+    entries: {},
+  }
+
+  const getters = {
+    get: state => id => state.entries[id] || ({
+      isWaiting: false,
+      error: null,
+      success: false,
+    }),
+    error: (state, getters) => id => field => {
+      const error = getters.get(id).error
+      return error && error[field] && error[field][0]
+    },
+  }
+
+  const actions = {
+    clear: ({ commit }) => commit('clear'),
+  }
+
+  const mutations = {
+    [types.REQUEST] (state, { id }) {
+      Vue.set(state.entries, id, {
+        isWaiting: true,
+        error: null,
+        success: false,
+      })
+    },
+    [types.RECEIVE_SUCCESS] (state, { id }) {
+      Vue.set(state.entries, id, {
+        isWaiting: false,
+        error: null,
+        success: true,
+      })
+    },
+    [types.RECEIVE_ERROR] (state, { id, error }) {
+      Vue.set(state.entries, id, {
+        isWaiting: false,
+        error,
+        success: false,
+      })
+    },
+    [types.CLEAR] (state, { id }) {
+      Vue.set(state.entries, id, {
+        isWaiting: false,
+        error: null,
+        success: false,
+      })
+    },
+  }
+
+  function namespaced (val) {
+    return namespace + '/' + val
+  }
+
+  function createRequestAction ({ metaId, request }) {
+    // we need to use the namespaced commits here, as the commit happens in parent
+    // I didn't find a good workaround, maybe it would be better to dispatch an action from parent to child?
+    return async function (store, arg) {
+      const { commit } = store
+      const id = metaId(arg)
+      commit(namespaced(types.REQUEST), { id })
+      try {
+        await request(store, arg)
+        commit(namespaced(types.RECEIVE_SUCCESS), { id })
+      }
+      catch (error) {
+        console.log('receive error with id', id, error)
+        const { response: { status = -1, data } = {} } = error
+        if (status >= 400 && status < 500) {
+          commit(namespaced(types.RECEIVE_ERROR), { id, error: data })
+        }
+        else {
+          throw error
+        }
+      }
+    }
+  }
+
+  return {
+    [namespace]: {
+      namespaced: true,
+      state,
+      getters,
+      actions,
+      mutations,
+      createRequestAction,
+    },
   }
 }

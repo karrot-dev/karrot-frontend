80a935cdbbc5869ef2a002513c1bf9d4270d40de
Tilmann Becker <tilmann.becker@gmail.com>
2017-11-04 21:47:59 +0100

    hide tab underline if on unrelated route

--- src/components/Group/Navbar.vue
+++ src/components/Group/Navbar.vue
@@ -1,25 +1,22 @@
 <template>
   <div slot="navigation" class="shadow-14 inset-shadow row">
-    <q-tabs color="tertiary" hide="icon" class="col">
+    <q-tabs color="tertiary" hide="icon" class="col" ref="groupnav">
       <q-route-tab
         :to="{name: 'group'}"
         exact
-        slot="title"
-        name="wall">
+        slot="title">
         {{ $t('GROUP.WALL') }}
       </q-route-tab>
       <q-route-tab
         :to="{name: 'groupDescription'}"
         exact
-        slot="title"
-        name="description">
+        slot="title">
         {{ $t('GROUP.DESCRIPTION') }}
       </q-route-tab>
       <q-route-tab
         :to="{name: 'groupHistory'}"
         exact
-        slot="title"
-        name="history">
+        slot="title">
         {{ $t('GROUP.HISTORY') }}
       </q-route-tab>
     </q-tabs>
@@ -36,6 +33,13 @@ import GroupOptions from '@/components/Sidenav/GroupOptions'
 
 export default {
   components: { QTabs, QRouteTab, QBtn, QIcon, GroupOptions },
+  watch: {
+    $route (route) {
+      if (route.name !== 'group' && route.name !== 'groupDescription' && route.name !== 'groupHistory') {
+        this.$refs.groupnav.selectTab(null)
+      }
+    },
+  },
 }
 </script>
 

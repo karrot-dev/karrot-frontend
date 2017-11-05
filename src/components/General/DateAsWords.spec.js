d05beaf5a7a95d0583166893d7bbd7e0f052660a
rene_imac <HarryMillerMail@googlemail.com>
2017-11-05 13:56:35 +0100

    #311 use jest.fn() instead of .genMockFunction()

--- src/components/General/DateAsWords.spec.js
+++ src/components/General/DateAsWords.spec.js
@@ -4,7 +4,7 @@ import { mountWithDefaults } from '>/helpers'
 describe('DateAsWords', () => {
   it('renders 1 day ago', () => {
     const now = new Date('2017-08-12T12:00:00Z')
-    Date.now = jest.genMockFunction().mockReturnValue(now)
+    Date.now = jest.fn().mockReturnValue(now)
 
     let date = new Date('2017-08-11T10:00:00Z')
     let wrapper = mountWithDefaults(DateAsWords,

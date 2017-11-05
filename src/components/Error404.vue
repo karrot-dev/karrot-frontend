37aec8f1557f20c1f86d3162d4c6e1f619fa6acc
Tilmann Becker <tilmann.becker@gmail.com>
2017-11-05 09:52:30 +0100

    translate 404 page

--- src/components/Error404.vue
+++ src/components/Error404.vue
@@ -6,7 +6,7 @@
     <div>
       <div class="error-card shadow-4 bg-white column items-center justify-center no-wrap">
         <q-icon name="error_outline" color="grey-5" />
-        <p class="caption text-center">Oops. Nothing here...</p>
+        <p class="caption text-center">{{ $t('NOT_FOUND.EXPLANATION') }}</p>
         <p class="text-center group">
           <q-btn
             v-if="canGoBack"
@@ -15,7 +15,7 @@
             @click="goBack"
             icon="keyboard_arrow_left"
           >
-            Go back
+            {{ $t('BUTTON.BACK') }}
           </q-btn>
           <q-btn
             color="primary"
@@ -23,7 +23,7 @@
             @click="$router.replace('/')"
             icon-right="home"
           >
-            Go home
+            {{ $t('NOT_FOUND.HOME') }}
           </q-btn>
         </p>
       </div>

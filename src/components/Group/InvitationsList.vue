ab3a3625dc23b06be77ab76187f5cf21ed940fe8
Tilmann Becker <tilmann.becker@gmail.com>
2017-11-14 21:27:13 +0100

    adapt related files

---
+++ src/components/Group/InvitationsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h5 class="text-primary generic-padding" v-if="invitations.length > 0">
+      {{ $t('GROUP.INVITED_LIST') }}
+    </h5>
+    <ul>
+      <li v-for="invite in invitations" :key="invite.id">{{ invite.email }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    invitations: { required: true },
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
--- src/pages/Group/Invitations.vue
+++ src/pages/Group/Invitations.vue
@@ -5,15 +5,13 @@ import InvitationsUI from '@/components/Group/InvitationsUI'
 export default connect({
   gettersToProps: {
     invitations: 'invitations/list',
-    listStatus: 'invitations/listStatus',
     sendStatus: 'invitations/sendStatus',
-    requestError: 'invitations/sendError',
   },
   actionsToEvents: {
     submit: 'invitations/send',
   },
   lifecycle: {
-    mounted: ({ dispatch }) => dispatch('invitations/fetchForActiveGroup'),
+    mounted: ({ dispatch }) => dispatch('invitations/fetch'),
   },
 })('GroupInvitations', InvitationsUI)
 </script>

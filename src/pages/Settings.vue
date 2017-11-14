ebac1534679d4a528f48d0298a6d3b257ac20424
Tilmann Becker <tilmann.becker@gmail.com>
2017-11-14 17:56:53 +0100

    Remove language select from settings
    #669

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="generic-padding grey-border">
     <q-list no-border>
-      <q-list-header>{{ $t('SETTINGS.LANGUAGE') }}</q-list-header>
-      <q-item style="padding-top: 0">
-        <q-item-side>
-          <i class="fa fa-fw fa-language"/>
-        </q-item-side>
-        <q-item-main>
-          <q-select @input="setLocale" :value="locale" :options="localeOptions" />
-        </q-item-main>
-      </q-item>
-
       <q-list-header>{{ $t('USERDATA.PROFILE_TITLE') }}</q-list-header>
 
       <q-item>
@@ -34,7 +24,7 @@
 </template>
 
 <script>
-import { QList, QItem, QItemSide, QListHeader, QItemSeparator, QItemMain, QSelect, QCollapsible } from 'quasar'
+import { QList, QItem, QListHeader } from 'quasar'
 import ProfileEdit from '@/components/Settings/ProfileEdit'
 import ChangePassword from '@/components/Settings/ChangePassword'
 import ChangeEmail from '@/components/Settings/ChangeEmail'
@@ -43,17 +33,14 @@ import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Settings',
-  components: { QList, QItem, QItemSide, QListHeader, QItemSeparator, QItemMain, QSelect, QCollapsible, ProfileEdit, ChangePassword, ChangeEmail },
+  components: { QList, QItem, QListHeader, ProfileEdit, ChangePassword, ChangeEmail },
   computed: {
     ...mapGetters({
       user: 'auth/user',
-      locale: 'i18n/locale',
-      localeOptions: 'i18n/localeOptions',
     }),
   },
   methods: {
     ...mapActions({
-      setLocale: 'i18n/setLocale',
       saveUser: 'auth/update',
       changePassword: 'auth/changePassword',
       changeEmail: 'auth/changeEmail',

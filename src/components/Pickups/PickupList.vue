531c0a11bb1c1ffc978077deaa2d7cccc881a28f
djahnie <djahnie@disroot.org>
2017-11-16 18:07:59 +0100

    move manage-button out of pickupList

--- src/components/Pickups/PickupList.vue
+++ src/components/Pickups/PickupList.vue
@@ -1,24 +1,14 @@
 <template>
   <div>
-    <div class="topbar">
-      <router-link v-if="options.showCreate" :to="{ name: 'storePickupsManage', params: { storeId: store.id } }">
-        <q-btn style="background-color: white">
-          <i class="fa fa-clock-o on-left"/>
-          {{$t('STOREDETAIL.MANAGE')}}
-        </q-btn>
-      </router-link>
-    </div>
-    <div>
-      <PickupItem
-        v-for="pickup in pickups"
-        :key="pickup.id"
-        :pickup="pickup"
-        @join="$emit('join', arguments[0])"
-        @leave="$emit('leave', arguments[0])"
-        >
-        {{ $d(pickup.date, 'dateLongWithDayName') }}
-      </PickupItem>
-    </div>
+    <PickupItem
+      v-for="pickup in pickups"
+      :key="pickup.id"
+      :pickup="pickup"
+      @join="$emit('join', arguments[0])"
+      @leave="$emit('leave', arguments[0])"
+      >
+      {{ $d(pickup.date, 'dateLongWithDayName') }}
+    </PickupItem>
   </div>
 </template>
 
@@ -28,17 +18,7 @@ import PickupItem from './PickupItem'
 
 export default {
   props: {
-    options: {
-      required: false,
-      default () {
-        return {
-          showStore: false, // either time or store
-          showCreate: true,
-        }
-      },
-    },
     pickups: { required: true },
-    store: { required: true },
   },
   components: {
     QCardTitle, QCard, QCardMain, QCardSeparator, QCardActions, QBtn, QIcon, PickupItem,
@@ -47,9 +27,4 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-.topbar
-  padding 8px
-  q-btn
-    display inline-block
-    padding .3em
 </style>

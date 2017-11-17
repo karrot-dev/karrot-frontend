a5205f300ff22c2458f7c9921f60268613df5042
Nick Sellen <git@nicksellen.co.uk>
2017-11-17 02:01:17 +0000

    Switch back to html template

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -30,7 +30,7 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'src/index.ejs',
+      template: 'src/index.html',
       inject: true
     }),
     new FriendlyErrorsPlugin({

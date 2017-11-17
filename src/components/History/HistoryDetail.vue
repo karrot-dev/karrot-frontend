39d10ff7337f63f9ef36b6e29f60bc728a843b38
D0nPiano <spam-mails@abwesend.de>
2017-11-17 11:34:55 +0100

    HistoryDetail: Added breadcrumb, Fixed icon with, changed design

--- src/components/History/HistoryDetail.vue
+++ src/components/History/HistoryDetail.vue
@@ -1,23 +1,17 @@
 <template>
   <q-card>
-    <q-card-title>
-      {{ $t('HISTORY.DETAILS') }}
-    </q-card-title>
     <table class="q-table" v-if="entry">
       <colgroup>
         <col span="1" style="width: 1%;">
         <col span="1">
       </colgroup>
       <tbody>
-        <tr class="bg-primary text-white">
-          <td colspan="2"><q-icon name="fa-info"></q-icon></td>
-        </tr>
         <tr>
-          <td><q-icon name="fa-clock-o"></q-icon></td>
+          <td><q-icon name="fa-fw fa-clock-o"></q-icon></td>
           <td>{{ $d(new Date(entry.date), 'long') }}, <DateAsWords :date="entry.date" /></td>
         </tr>
         <tr>
-          <td><q-icon name="fa-user"></q-icon></td>
+          <td><q-icon name="fa-fw fa-user"></q-icon></td>
           <td>
             <ProfilePicture
               v-for="user in entry.users"
@@ -27,27 +21,27 @@
           </td>
         </tr>
         <tr>
-          <td><q-icon name="fa-commenting-o"></q-icon></td>
+          <td><q-icon name="fa-fw fa-commenting-o"></q-icon></td>
           <td>{{ entry.message }}</td>
         </tr>
         <tr v-if="entry.group && entry.group.name">
-          <td><q-icon name="fa-home"></q-icon></td>
+          <td><q-icon name="fa-fw fa-home"></q-icon></td>
           <td>{{ entry.group.name }}</td>
         </tr>
         <tr v-if="entry.store && entry.store.name">
-          <td><q-icon name="fa-shopping-cart"></q-icon></td>
+          <td><q-icon name="fa-fw fa-shopping-cart"></q-icon></td>
           <td>{{ entry.store.name }}</td>
         </tr>
         <template v-if="entry.payload">
           <tr class="bg-primary text-white">
-            <td colspan="2"><q-icon name="fa-file-text-o"></q-icon></td>
+            <td colspan="2"><q-icon name="fa-fw fa-file-text-o"></q-icon></td>
           </tr>
           <tr v-for="(value, key) in entry.payload" :key="key">
             <td>{{ key }}</td>
             <td>{{ value }}</td>
           </tr>
         </template>
-        <tr class="bg-primary text-white">
+        <tr class="bg-neutral text-white">
           <td colspan="2">
             <q-btn @click="toggleRaw()" color="secondary">Raw data</q-btn>
           </td>

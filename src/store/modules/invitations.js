6fcd43113f060a10db7d998f8522885831e98414
Tilmann Becker <tilmann.becker@gmail.com>
2017-12-08 22:02:32 +0100

    refactor invitations module

--- src/store/modules/invitations.js
+++ src/store/modules/invitations.js
@@ -3,14 +3,6 @@ import invitations from '@/services/api/invitations'
 import router from '@/router'
 import { indexById, createMetaModule, withMeta, metaStatuses } from '@/store/helpers'
 
-export const modules = { meta: createMetaModule() }
-
-export const types = {
-  RECEIVE: 'Receive',
-  APPEND: 'Append',
-  CLEAR: 'Clear',
-}
-
 function initialState () {
   return {
     entries: {},
@@ -18,73 +10,74 @@ function initialState () {
   }
 }
 
-export const state = initialState()
+export default {
+  namespaced: true,
+  modules: { meta: createMetaModule() },
+  state: initialState(),
+  getters: {
+    list: state => state.idList.map(i => state.entries[i]),
+    ...metaStatuses(['fetch', 'send', 'accept']),
+  },
+  actions: {
+    ...withMeta({
+      /**
+       * Fetch sent invitations for current group
+       */
+      async fetch ({ commit, dispatch, rootGetters }) {
+        const groupId = rootGetters['group/currentGroupId']
+        commit('set', await invitations.listByGroupId(groupId))
+      },
 
-export const getters = {
-  list: state => state.idList.map(i => state.entries[i]),
-  ...metaStatuses(['fetch', 'send', 'accept']),
-}
+      /**
+       * Send invitation to e-mail
+       */
+      async send ({ commit, rootGetters }, email) {
+        const invited = await invitations.create({
+          email,
+          group: rootGetters['currentGroup/id'],
+        })
+        commit('append', invited)
+      },
 
-export const actions = {
-  ...withMeta({
+      /**
+       * Accept invitation with token
+       */
+      async accept ({ commit, dispatch }, token) {
+        try {
+          await invitations.accept(token)
+          // Current group has changed, refresh user data
+          await dispatch('auth/check', { root: true })
+          dispatch('alerts/create', { type: 'inviteAcceptSuccess' }, { root: true })
+          router.push('/')
+        }
+        catch (error) {
+          dispatch('alerts/create', { type: 'inviteAcceptError' }, { root: true })
+          router.push({ name: 'groupsGallery' })
+          throw error
+        }
+      },
+    }),
+
     /**
-     * Fetch sent invitations for current group
+     * Reset all state
      */
-    async fetch ({ commit, dispatch, rootGetters }) {
-      const groupId = rootGetters['group/currentGroupId']
-      commit(types.RECEIVE, { list: await invitations.listByGroupId(groupId) })
+    clear ({ commit }) {
+      commit('clear')
     },
 
-    /**
-     * Send invitation to e-mail
-     */
-    async send ({ commit, rootGetters }, email) {
-      const invited = await invitations.create({
-        email,
-        group: rootGetters['currentGroup/id'],
-      })
-      commit(types.APPEND, { invited })
+  },
+  mutations: {
+    set (state, list) {
+      state.entries = indexById(list)
+      state.idList = list.map(e => e.id)
     },
-
-    /**
-     * Accept invitation with token
-     */
-    async accept ({ commit, dispatch }, token) {
-      try {
-        await invitations.accept(token)
-        // Current group has changed, refresh user data
-        await dispatch('auth/check', { root: true })
-        dispatch('alerts/create', { type: 'inviteAcceptSuccess' }, { root: true })
-        router.push('/')
-      }
-      catch (error) {
-        dispatch('alerts/create', { type: 'inviteAcceptError' }, { root: true })
-        router.push({ name: 'groupsGallery' })
-        throw error
-      }
+    append (state, invited) {
+      Vue.set(state.entries, invited.id, invited)
+      state.idList.push(invited.id)
     },
-  }),
-
-  /**
-   * Reset all state
-   */
-  clear ({ commit }) {
-    commit(types.CLEAR)
-  },
-}
-
-export const mutations = {
-  [types.RECEIVE] (state, { list }) {
-    state.entries = indexById(list)
-    state.idList = list.map(e => e.id)
-  },
-  [types.APPEND] (state, { invited }) {
-    Vue.set(state.entries, invited.id, invited)
-    state.idList.push(invited.id)
-  },
-
-  [types.CLEAR] (state) {
-    Object.entries(initialState())
-      .forEach(([prop, value]) => Vue.set(state, prop, value))
+    clear (state) {
+      Object.entries(initialState())
+        .forEach(([prop, value]) => Vue.set(state, prop, value))
+    },
   },
 }

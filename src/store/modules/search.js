865b70f92bf6fd7c0be6d899fe32159ec4e84cae
Tilmann Becker <tilmann.becker@gmail.com>
2017-12-08 22:36:03 +0100

    refactor search module

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -1,9 +1,3 @@
-export const types = {
-  SHOW: 'Show',
-  HIDE: 'Hide',
-  SET_TERMS: 'Set Terms',
-}
-
 function initialState () {
   return {
     open: false,
@@ -11,69 +5,71 @@ function initialState () {
   }
 }
 
-export const state = initialState()
+export default {
+  namespaced: true,
+  state: initialState(),
+  getters: {
+    open: state => state.open,
+    terms: state => state.terms || '',
+    results: (state, getters, rootState, rootGetters) => {
+      const stores = rootGetters['stores/all'],
+        groups = rootGetters['groups/all'],
+        users = rootGetters['users/all']
 
-export const getters = {
-  open: state => state.open,
-  terms: state => state.terms || '',
-  results: (state, getters, rootState, rootGetters) => {
-    const stores = rootGetters['stores/all'],
-      groups = rootGetters['groups/all'],
-      users = rootGetters['users/all']
+      const matches = (name) => name.toLowerCase().includes(getters.terms.toLowerCase())
 
-    const matches = (name) => name.toLowerCase().includes(getters.terms.toLowerCase())
+      const storeRes = stores.filter((store) => matches(store.name))
+      const groupRes = groups.filter((group) => matches(group.name))
+      const userRes = users.filter((user) => matches(user.displayName))
 
-    const storeRes = stores.filter((store) => matches(store.name))
-    const groupRes = groups.filter((group) => matches(group.name))
-    const userRes = users.filter((user) => matches(user.displayName))
+      return [
+        ...storeRes.map(e => ({
+          value: {name: 'store', params: {groupId: e.group, storeId: e.id}},
+          label: e.name,
+          icon: 'fa-shopping-cart',
+        })),
+        ...groupRes.map(e => ({
+          value: {name: 'group', params: {groupId: e.id}},
+          label: e.name,
+          icon: 'fa-home',
+        })),
+        ...userRes.map(e => ({
+          value: {name: 'user', params: {userId: e.id}},
+          label: e.displayName,
+          icon: 'fa-user',
+        })),
+      ]
+    },
 
-    return [
-      ...storeRes.map(e => ({
-        value: {name: 'store', params: {groupId: e.group, storeId: e.id}},
-        label: e.name,
-        icon: 'fa-shopping-cart',
-      })),
-      ...groupRes.map(e => ({
-        value: {name: 'group', params: {groupId: e.id}},
-        label: e.name,
-        icon: 'fa-home',
-      })),
-      ...userRes.map(e => ({
-        value: {name: 'user', params: {userId: e.id}},
-        label: e.displayName,
-        icon: 'fa-user',
-      })),
-    ]
   },
-}
+  actions: {
+    show ({ commit }) {
+      commit('show')
+    },
 
-export const actions = {
-  show ({ commit }) {
-    commit(types.SHOW)
-  },
+    hide ({ commit }) {
+      commit('hide')
+    },
+    hideIfEmpty ({ commit, getters }) {
+      if (!getters.terms) {
+        commit('hide')
+      }
+    },
 
-  hide ({ commit }) {
-    commit(types.HIDE)
-  },
-  hideIfEmpty ({ commit, getters }) {
-    if (!getters.terms) {
-      commit(types.HIDE)
-    }
-  },
+    setTerms ({ commit }, terms) {
+      commit('setTerms', terms)
+    },
 
-  setTerms ({ commit }, terms) {
-    commit(types.SET_TERMS, { terms })
   },
-}
-
-export const mutations = {
-  [types.SHOW] (state) {
-    state.open = true
-  },
-  [types.HIDE] (state) {
-    state.open = false
-  },
-  [types.SET_TERMS] (state, { terms }) {
-    state.terms = terms
+  mutations: {
+    show (state) {
+      state.open = true
+    },
+    hide (state) {
+      state.open = false
+    },
+    setTerms (state, terms) {
+      state.terms = terms
+    },
   },
 }

e2384c183f93ef1d4c6c895755478ab21f4b127b
Arnaud Meersschaut <arnaud.meersschaut@student.howest.be>
2018-01-08 17:32:44 +0100

    proper code for cancel button

--- src/components/Store/StoreEdit.vue
+++ src/components/Store/StoreEdit.vue
@@ -108,7 +108,7 @@
             </q-btn>
             <q-btn
               type="button"
-              @click="$router.push('/group/' + $route.params.groupId)"
+              @click="cancel()"
               v-if="isNew"
             >
               {{ $t('BUTTON.CANCEL') }}
@@ -215,6 +215,9 @@ export default {
           },
         ],
       })
+    },
+    cancel () {
+      this.$router.go(-1)
     },
   },
   validations: {

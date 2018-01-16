e9e45be1ede436b25d0d3738ee6aaa6de9a69df5
Tilmann Becker <tilmann.becker@gmail.com>
2018-01-16 18:00:07 +0100

    revert to minimal change

--- src/components/Store/StoreEdit.vue
+++ src/components/Store/StoreEdit.vue
@@ -108,7 +108,7 @@
             </q-btn>
             <q-btn
               type="button"
-              @click="cancel()"
+              @click="$emit('cancel')"
               v-if="isNew"
             >
               {{ $t('BUTTON.CANCEL') }}
@@ -215,9 +215,6 @@ export default {
           },
         ],
       })
-    },
-    cancel () {
-      this.$router.go(-1)
     },
   },
   validations: {

7f8822bc90bb5af8397282398311837895ba6a60
Nick Sellen <git@nicksellen.co.uk>
2018-01-31 18:47:50 +0000

    Keep domain in image urls (#858)
    * Keep domain in image urls
    
    * Serve profile photos via proxy in development
    
    * Add DEV global to jest config
    
    * Handle case where there is no profile photo
    
    * Add basic ChangePhoto component tests

---
+++ src/components/Settings/ChangePhoto.spec.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+
+import ChangePhoto from './ChangePhoto'
+import { usersMock } from '>/mockdata'
+import cloneDeep from 'clone-deep'
+
+import { mountWithDefaults, polyfillRequestAnimationFrame, statusMocks } from '>/helpers'
+
+polyfillRequestAnimationFrame()
+
+describe('ChangePhoto', () => {
+  let wrapper
+  let user
+
+  beforeEach(() => {
+    user = cloneDeep(usersMock[0])
+    wrapper = mountWithDefaults(ChangePhoto, { propsData: { value: user, status: statusMocks.default() } })
+  })
+
+  it('renders', () => {
+    expect(wrapper.element.className).toBe('edit-box')
+  })
+
+  it('renders placeholder image', () => {
+    expect(wrapper.find('img').element.src).toBe('statics/ic_person_black_24px.svg')
+  })
+
+  it('renders image from localhost in development/test', async () => {
+    Vue.set(wrapper.vm.value.photoUrls, 'fullSize', '/foo.jpg')
+    await Vue.nextTick()
+    expect(wrapper.find('img').element.src).toBe('http://localhost:8080/foo.jpg')
+  })
+
+  it('emits a save event', async () => {
+    wrapper.vm.save()
+    expect(wrapper.emitted().save[0][0]).toEqual({ photo: null })
+  })
+})

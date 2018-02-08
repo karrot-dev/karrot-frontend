d04d750d6f91f72c7664ab79b539895943b40f33
christopher michel <christopher.michel@uconn.edu>
2018-02-08 16:27:22 -0500

    Third commit Back To Top Button

--- config/index.js
+++ config/index.js
@@ -53,7 +53,7 @@ module.exports = {
     // auto open browser or not
     openBrowser: true,
     publicPath: '/',
-    port: 3000,
+    port: 8080,
 
     // If for example you are using Quasar Play
     // to generate a QR code then on each dev (re)compilation

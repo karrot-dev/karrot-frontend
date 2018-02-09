0935a3d5dfff8a3a67700abf8823c66082a80f42
searchingfor <chris.michel33@gmail.com>
2018-02-09 05:31:14 -0500

    Back To Top Button (#867)
    Closes #867 
    
    * First commit for the Back To Top Button
    
    * Second commit for the Back To Top Button
    
    * Third commit Back To Top Button

---
+++ src/components/BackToTopBtn.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-btn
+    v-back-to-top
+    round
+    class="fixed-bottom-right bg-primary btt-btn"
+  >
+    <q-icon
+      name="keyboard_arrow_up"
+      color = "white"
+    />
+  </q-btn>
+</template>
+
+<script>
+import {
+  Ripple,
+  QBtn,
+  QIcon,
+  BackToTop,
+} from 'quasar'
+export default {
+  components: {
+    QBtn,
+    QIcon,
+  },
+  directives: {
+    BackToTop,
+    Ripple,
+  },
+  name: 'BackToTopBtn',
+}
+</script>
+
+<style scoped lang = "stylus">
+  .btt-btn
+    margin: 0 15px 80px 0
+</style>

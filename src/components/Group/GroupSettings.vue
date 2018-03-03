575fd9337b5c58d91dc74db4c86ece6cf7a5dcc4
Nick Sellen <git@nicksellen.co.uk>
2018-03-03 21:51:14 +0100

    Add group settings page for notification options (#933)
    * Add group settings page for notification options
    
    * remove emittingcheckbox, remove console.log

---
+++ src/components/Group/GroupSettings.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-card
+    class="no-mobile-margin no-shadow grey-border"
+  >
+    <RandomArt
+      :seed="group.id"
+      type="circles">
+      <div class="art-overlay"/>
+    </RandomArt>
+    <q-list link>
+      <q-list-header>{{ $t('GROUP.NOTIFICATIONS') }}</q-list-header>
+      <q-item
+        tag="label"
+        v-for="type in availableNotificationTypes"
+        :key="type"
+      >
+        <q-item-side>
+          <q-checkbox
+            :value="notificationIsEnabled(type)"
+            @input="change(type, arguments[0])"
+          />
+        </q-item-side>
+        <q-item-main>
+          <q-item-tile label>{{ $t('GROUP.NOTIFICATION_TYPES.' + type + '.NAME') }}</q-item-tile>
+          <q-item-tile sublabel>{{ $t('GROUP.NOTIFICATION_TYPES.' + type + '.DESCRIPTION') }}</q-item-tile>
+        </q-item-main>
+      </q-item>
+    </q-list>
+  </q-card>
+</template>
+
+<script>
+import { QCard, QCheckbox, QList, QListHeader, QItem, QItemSide, QItemMain, QItemTile } from 'quasar'
+
+import RandomArt from '@/components/General/RandomArt'
+
+export default {
+  name: 'GroupSettings',
+  components: { RandomArt, QCard, QCheckbox, QList, QListHeader, QItem, QItemSide, QItemMain, QItemTile },
+  props: {
+    group: {
+      type: Object,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      availableNotificationTypes: ['weekly_summary'],
+    }
+  },
+  methods: {
+    change (notificationType, enabled) {
+      this.$emit('changeNotificationType', { notificationType, enabled })
+    },
+    notificationIsEnabled (type) {
+      return this.group.notificationTypes.indexOf(type) !== -1
+    },
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+</style>

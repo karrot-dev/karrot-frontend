460163cb07dbe0511425bc9350cb839c3199ff17
Neha Kandpal <iec2015048@iiita.ac.in>
2018-03-08 01:07:59 +0530

    Edit button removed from other user profile

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -55,6 +55,7 @@
         round
         class="inlinee on-right"
         color="secondary"
+        v-if="user.id==currentUserId"
         @click="$router.push('/settings')"
       />
       <div class="info">
@@ -91,6 +92,7 @@
 
 <script>
 
+import { mapGetters } from 'vuex'
 import Markdown from '@/components/Markdown'
 import ProfilePicture from '@/components/ProfilePictures/ProfilePicture'
 import UserMapPreview from '@/components/Map/UserMapPreview'
@@ -110,6 +112,9 @@ export default {
       }
       return 180
     },
+    ...mapGetters({
+      currentUserId: 'auth/userId',
+    }),
   },
 }
 </script>

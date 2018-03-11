223900dd2875bddabe8c3c3c136ae88c4b435b79
Tilmann Becker <tilmann.becker@gmail.com>
2018-03-11 16:35:25 +0100

    save cache after e2e test

--- src/store/plugins/dependentState.js
+++ src/store/plugins/dependentState.js
@@ -5,7 +5,9 @@ export default store => {
   store.watch(memberIdsGetter, updateUserList)
 
   // If our groups change, then available stores may change, so refresh them
-  const updateStoreList = () => store.dispatch('stores/fetch', null, { root: true })
+  const updateStoreList = () => store.dispatch('stores/fetch', null, { root: true }).catch(error => {
+    if (!error.message.startsWith('ALREADY_PENDING')) return Promise.reject(error)
+  })
   const groupIdsGetter = () => store.getters['groups/mine'].map(group => group.id).sort().join(',')
   store.watch(groupIdsGetter, updateStoreList)
 }

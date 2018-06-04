6ca838455f4ac67f801d0f632bf77f700a2a68cb
Tilmann Becker <tilmann.becker@gmail.com>
2018-06-04 23:23:07 +0200

    Use bcp47 instead of ISO format
    Transifex uses ISO, Django uses bcp47

--- update_i18n.py
+++ update_i18n.py
@@ -2,10 +2,15 @@
 import json
 import txclib.project as project
 
-STATUS_FILE = './src/locales/status-frontend.json'
-PRJ = project.Project()
-PRJ.pull()
-STATS = PRJ._get_stats_for_resource()
-with open(STATUS_FILE, 'w') as f:
-    json.dump(STATS, f, sort_keys=True, indent=2)
-    print("Updated", STATUS_FILE)
+status_file = './src/locales/status-frontend.json'
+project = project.Project()
+project.pull()
+stats = project._get_stats_for_resource()
+with open(status_file, 'w') as f:
+    # switch to HTTP Accept-Language format
+    for key in stats.keys():
+        if '_' in key:
+          new_key = key.replace('_', '-').lower()
+          stats[new_key] = stats.pop(key)
+    json.dump(stats, f, sort_keys=True, indent=2)
+    print("Updated", status_file)

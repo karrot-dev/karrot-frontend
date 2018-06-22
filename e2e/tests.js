35c9c86fd95ccdcb13de4ea1cbf4686e6b1218d8
Tilmann Becker <tilmann.becker@gmail.com>
2018-06-22 19:44:58 +0200

    fix e2e tests

--- e2e/tests.js
+++ e2e/tests.js
@@ -2,7 +2,7 @@ import { Selector, Role } from 'testcafe'
 const testTime = Math.floor(new Date() / 1000)
 
 const testUser = Role('http://localhost:8080/#/login', async t => {
-  const mailField = Selector('div').withText('Mail').child('input')
+  const mailField = Selector('div').withText('E-mail').child('input')
   const passwordField = Selector('div').withText('Password').child('input')
   await t
     .selectText(mailField)
@@ -21,14 +21,14 @@ test('create user', async t => {
   await t
     .expect(await t.eval(() => document.title)).eql('Sign Up! · Karrot')
     .typeText(Selector('div').withText('Name').child('input'), 'E2E Testuser')
-    .typeText(Selector('div').withText('Mail').child('input'), `user${testTime}@example.com`)
+    .typeText(Selector('div').withText('E-mail').child('input'), `user${testTime}@example.com`)
     .typeText(Selector('div').withText('Password').child('input'), 'user')
     .pressKey('enter')
     .expect(Selector('.fa-user').parent(0).innerText).contains('E2E TESTUSER')
 })
 
 test('login', async t => {
-  const mailField = Selector('div').withText('Mail').child('input')
+  const mailField = Selector('div').withText('E-mail').child('input')
   const passwordField = Selector('div').withText('Password').child('input')
   await t
     .click(Selector('button').withText('LOGIN'))
@@ -59,7 +59,7 @@ test('select a group, signs in and gets added to the group', async t => {
   await t
     .expect(await t.eval(() => document.title)).eql('Sign Up! · Karrot')
     .typeText(Selector('div').withText('Name').child('input'), 'Testuser B')
-    .typeText(Selector('div').withText('Mail').child('input'), `userB${testTime}@example.com`)
+    .typeText(Selector('div').withText('E-mail').child('input'), `userB${testTime}@example.com`)
     .typeText(Selector('div').withText('Password').child('input'), 'user')
     .pressKey('enter')
     .expect(Selector('.lastElement').innerText).contains(testTime)

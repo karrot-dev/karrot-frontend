7abe7e4b7e4b8061efff6326fcbd6f45691ecffb
Nick Sellen <git@nicksellen.co.uk>
2018-06-29 17:05:01 +0200

    Revert newline change

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
  */
 import LoadingProgress from '@/components/Layout/LoadingProgress'
 import BackToTopBtn from '@/components/BackToTopBtn'
-
 export default {
   components: {
     LoadingProgress,

9c77f0e9baba9fd48cb1d852de315b016b70a106
Nick Sellen <git@nicksellen.co.uk>
2018-06-30 01:19:22 +0200

    Reduce the number of tiny js files

--- src/main.js
+++ src/main.js
@@ -32,28 +32,27 @@ import 'quasar-extras/material-icons'
 import 'quasar-extras/fontawesome'
 import 'quasar-extras/animate'
 import 'typeface-cabin-sketch'
+import App from '@/App'
+import '@/presenceReporter'
 
-(async () => {
+const { DetectMobileKeyboardPlugin } = require('@/services/detectMobileKeyboard')
+Vue.use(DetectMobileKeyboardPlugin)
+
+;(async () => {
   sync(store, router)
 
-  const detectMobileKeyboard = await import('@/services/detectMobileKeyboard')
-  Vue.use(detectMobileKeyboard.DetectMobileKeyboardPlugin)
-
-  const [App] = await Promise.all([
-    import('./App'),
-    import('@/presenceReporter'),
+  await Promise.all([
     polyfill.init(),
     store.dispatch('auth/check'),
     store.dispatch('groups/fetch'),
   ])
 
-  const app = App.default
   /* eslint-disable no-new */
   new Vue({
     el: '#q-app',
     router,
     store,
     i18n,
-    render: h => h(app),
+    render: h => h(App),
   })
 })()

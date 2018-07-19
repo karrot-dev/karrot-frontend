4a4863f248c5cf0afcf3fbfe2370280a21c04476
Tilmann Becker <tilmann.becker@gmail.com>
2018-07-19 22:14:00 +0200

    fix tests

--- src/store/modules/refresh.spec.js
+++ src/store/modules/refresh.spec.js
@@ -5,11 +5,14 @@ const mockStoresList = jest.fn()
 const mockPickupsList = jest.fn()
 const mockGroupsInfoList = jest.fn()
 const mockAuthUserGet = jest.fn()
+const mockMessagesGet = jest.fn()
+const mockMessagesListThread = jest.fn()
 jest.mock('@/services/api/users', () => ({ list: mockUsersList }))
 jest.mock('@/services/api/stores', () => ({ list: mockStoresList }))
 jest.mock('@/services/api/pickups', () => ({ list: mockPickupsList }))
 jest.mock('@/services/api/groupsInfo', () => ({ list: mockGroupsInfoList }))
 jest.mock('@/services/api/authUser', () => ({ get: mockAuthUserGet }))
+jest.mock('@/services/api/messages', () => ({ get: mockMessagesGet, listThread: mockMessagesListThread }))
 
 import Vue from 'vue'
 import { configureQuasar } from '>/helpers'
@@ -26,6 +29,8 @@ describe('storeHelpers', () => {
     mockPickupsList.mockReturnValueOnce({ results: [] })
     mockGroupsInfoList.mockReturnValueOnce([])
     mockAuthUserGet.mockReturnValue({})
+    mockMessagesGet.mockReturnValue({})
+    mockMessagesListThread.mockReturnValue({ results: [] })
 
     store.dispatch('refresh/refresh')
 
@@ -34,5 +39,7 @@ describe('storeHelpers', () => {
     expect(mockPickupsList).toHaveBeenCalled()
     expect(mockGroupsInfoList).toHaveBeenCalled()
     expect(mockAuthUserGet).toHaveBeenCalled()
+    expect(mockMessagesGet).toHaveBeenCalled()
+    expect(mockMessagesListThread).toHaveBeenCalled()
   })
 })

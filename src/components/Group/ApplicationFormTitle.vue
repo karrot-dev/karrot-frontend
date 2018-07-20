1ec30334c58c8f2fac5d48819233e1b95556ce97
Taïs Tadam <real.j.tais@gmail.com>
2018-07-20 17:09:38 +0200

    add ApplicationFormTitle

---
+++ src/components/Group/ApplicationFormTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img :src="loginImage">
+    <h4>
+      {{ $t('APPLICATION_FORM.TITLE', { groupName: group.name }) }}
+    </h4>
+  </div>
+</template>
+
+<script>
+import loginImage from 'assets/people/cherry.png'
+import { mapGetters } from 'vuex'
+
+export default {
+  components: { },
+  data () {
+    return {
+      loginImage,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      group: 'groups/activePreview'
+    })
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>

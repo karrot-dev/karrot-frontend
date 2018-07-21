6561ee7b47e96ac72f3d0ff8804e124f519c3536
djahnie <djahnie@disroot.org>
2018-07-21 18:24:48 +0200

    add more group-application service methods

--- src/services/api/groupApplications.js
+++ src/services/api/groupApplications.js
@@ -4,3 +4,28 @@ export default {
   async create (data) {
     return (await axios.post('/api/group-applications/', data)).data
   },
+  
+  async get (data) {
+    return (await axios.get('/api/group-applications/{id}/', data)).data
+  },
+
+  async list () {
+    return (await axios.get('/api/group-applications/', data)).data
+  },
+
+  async listByGroupId (data) {
+    return (await axios.get('/api/group-applications/', data)).data
+  },
+
+  async accept (data) {
+    return (await axios.post('/api/group-applications/{id}/accept', data)).data
+  },
+
+  async decline (data) {
+    return (await axios.post('/api/group-applications/{id}/decline', data)).data
+  },
+
+  async withdraw (data) {
+    return (await axios.post('/api/group-applications/{id}/withdraw', data)).data
+  }
+}

3ca2e9738b732125a5199e4aabb441f588723a7b
djahnie <djahnie@disroot.org>
2018-07-27 16:09:29 +0200

    create ApplicationItem component

---
+++ src/components/Applications/ApplicationItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card class="no-shadow grey-border">
+    <q-card-main class="generic-padding">
+      <div class="row no-wrap">
+
+      </div>
+    </q-card-main>
+  </q-card>
+</template>
+
+<script>
+import { QCard, QCardMain } from 'quasar'
+import ProfilePicture from '@/components/ProfilePictures/ProfilePicture'
+
+export default {
+  components: {
+    QCard, QCardMain, ProfilePicture,
+  },
+  props: {
+
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+</style>

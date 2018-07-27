3cc5bfd12abb982a82515bcd8f7e3b56f7e18126
Taïs Tadam <real.j.tais@gmail.com>
2018-07-27 16:11:17 +0200

    Create applicationList basic template

--- src/components/Applications/ApplicationList.vue
+++ src/components/Applications/ApplicationList.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    {{ 'Hello!' }}
-  </div>
+  <q-infinite-scroll>
+    <ApplicationItem
+      v-for="applicationitem in applications"
+      :key="applicationitem.id"
+      :feedback="feedbackitem"
+    >
+    </ApplicationItem>
+  </q-infinite-scroll>
 </template>
 
 <script>
+import ApplicationItem from './ApplicationItem'
+import QInfiniteScroll from 'quasar'
 
 export default {
   components: {
+    QInfiniteScroll, ApplicationItem
   },
   props: {
+    applications: { required: true, type: Array }
   },
   computed: {
   },

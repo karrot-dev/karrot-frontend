3c72d5c063a5b43653f98814204233b362e0981b
djahnie <djahnie@disroot.org>
2018-07-29 00:43:03 +0200

    start fiddling with QItems in ApplicationItem

--- src/components/Applications/ApplicationItem.vue
+++ src/components/Applications/ApplicationItem.vue
@@ -1,30 +1,51 @@
 <template>
-  <q-card class="no-shadow grey-border">
+  <q-list class="no-shadow grey-border">
     <div class="row no-wrap">
       <ProfilePicture
         :key="application.user"
         :user="application.applicant"
         :size="80"
+        style="margin: 0.1rem 0.5rem"
       />
       <div class="content">
-        <q-card-main class="generic-padding">
-          <div class="row no-wrap">
-            {{ 'This is application #' + application.id + ' from user ' + userName }}
-          </div>
-        </q-card-main>
+        <q-item
+          multiline
+          class="generic-padding"
+        >
+          <q-item-tile :label="userName">
+            {{ userName + ' applied to join our group!' }}
+          </q-item-tile>
+          <q-item-tile :label="userName">
+            {{ 'It has id #' + application.id }}
+          </q-item-tile>
+          <q-item-side float-right>
+            <q-btn
+              round
+              color="positive"
+              icon="fas fa-check"
+              class="float-right generic-margin"
+            />
+            <q-btn
+              round
+              color="negative"
+              icon="fas fa-times"
+              class="float-right generic-margin"
+            />
+          </q-item-side>
+        </q-item>
       </div>
     </div>
-  </q-card>
+  </q-list>
 </template>
 
 <script>
-import { QCard, QCardMain } from 'quasar'
+import { QList, QItem, QBtn, QItemSide, QItemTile } from 'quasar'
 // import DateAsWords from '@/components/General/DateAsWords' --> needs created-at prop in application object
 import ProfilePicture from '@/components/ProfilePictures/ProfilePicture'
 
 export default {
   components: {
-    QCard, QCardMain, ProfilePicture,
+    QList, QItem, QBtn, QItemSide, QItemTile, ProfilePicture,
   },
   props: {
     application: {

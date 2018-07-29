d186c2502155011cc4c559447348cfe890f71e45
djahnie <djahnie@disroot.org>
2018-07-29 20:42:18 +0200

    comment out unused service methods

--- src/services/api/groupApplications.js
+++ src/services/api/groupApplications.js
@@ -5,9 +5,9 @@ export default {
     return (await axios.post('/api/group-applications/', data)).data
   },
 
-  async get (data) {
-    return (await axios.get('/api/group-applications/{id}/', data)).data
-  },
+  // async get (data) {
+  //   return (await axios.get('/api/group-applications/{id}/', data)).data
+  // },
 
   async list (filter) {
     return (await axios.get('/api/group-applications/', { params: filter })).data
@@ -18,9 +18,9 @@ export default {
     return (await axios.post(`/api/group-applications/${applicationId}/accept/`)).data
   },
 
-  async decline (data) {
-    return (await axios.post('/api/group-applications/{id}/decline', data)).data
-  },
+  // async decline (data) {
+  //   return (await axios.post('/api/group-applications/{id}/decline', data)).data
+  // },
 
   async withdraw (applicationId) {
     return (await axios.post(`/api/group-applications/${applicationId}/withdraw/`)).data

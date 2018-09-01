f77c13aba07aeb14d2ceb1da4d915b512f039c02
Tilmann Becker <tilmann.becker@gmail.com>
2018-09-01 22:48:39 +0200

    refactor trustbutton

--- src/components/User/TrustButton.vue
+++ src/components/User/TrustButton.vue
@@ -1,71 +1,84 @@
 <template>
   <q-btn
     round
-    color="primary"
+    :size="small ? 'sm' : null"
+    :color="isEditor ? 'primary' : 'white'"
+    @click="showing = true"
     class="karrot-button"
-    @click="showModal = true"
-    :disable="user.isCurrentUser && trustedBy.length === 0"
+    :class="!isEditor && 'small'"
+    :title="tooltip"
   >
+    <CircleProgress
+      v-if="!isEditor"
+      class="knob"
+      :value="trustProgress"
+    />
     <q-chip
       floating
+      :style="small && 'top: 5px; right: -12px'"
       :color="trusted || user.isCurrentUser ? 'positive' : 'negative'"
     >
       {{ trustedBy.length }}
     </q-chip>
-    <q-modal
-      v-model="showModal"
+    <q-dialog
+      v-model="showing"
       minimized
+      :title="headline"
+      :message="info"
     >
-      <div class="generic-padding">
-        <p>{{ headline }}</p>
-        <div>
-          <ProfilePicture
-            v-for="u in trustedBy"
-            :key="u.id"
-            :user="u"
-            :size="20"
-          />
-        </div>
-        <div class="row justify-end actionButtons">
-          <q-btn
-            @click="showModal = false"
-            rounded
-            v-t="'BUTTON.CLOSE'"
-          />
-          <q-btn
-            v-if="!trusted && !user.isCurrentUser"
-            rounded
-            color="secondary"
-            @click="showCreateTrustDialog"
-          >
-            <span class="q-mr-xs">+</span>
-            <img
-              src="./twemoji-karrot.png"
-              width="20px"
-            >
-          </q-btn>
-        </div>
+      <div slot="body">
+        <ProfilePicture
+          v-for="u in trustedBy"
+          :key="u.id"
+          :user="u"
+          :size="35"
+          class="q-mr-sm"
+        />
       </div>
-    </q-modal>
+      <template
+        slot="buttons"
+        slot-scope="props"
+      >
+        <q-btn
+          flat
+          :label="$t('BUTTON.CLOSE')"
+          @click="props.cancel"
+        />
+        <q-btn
+          v-if="!trusted && !user.isCurrentUser"
+          color="secondary"
+          :loading="membership.trustUserStatus.pending"
+          @click="showCreateTrustDialog"
+        >
+          <span class="q-mr-xs">+</span>
+          <img
+            src="./twemoji-karrot.png"
+            width="20px"
+          >
+        </q-btn>
+      </template>
+    </q-dialog>
   </q-btn>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import ProfilePicture from '@/components/ProfilePictures/ProfilePicture'
+import CircleProgress from '@/components/General/CircleProgress'
 
 import {
   QBtn,
-  QModal,
   QChip,
+  QDialog,
   Dialog,
 } from 'quasar'
 
 export default {
   components: {
     ProfilePicture,
+    CircleProgress,
     QBtn,
-    QModal,
+    QDialog,
     QChip,
   },
   props: {
@@ -81,10 +94,14 @@ export default {
       default: () => ({}),
       type: Object,
     },
+    small: {
+      default: false,
+      type: Boolean,
+    },
   },
   data () {
     return {
-      showModal: false,
+      showing: false,
     }
   },
   computed: {
@@ -97,25 +114,59 @@ export default {
     trusted () {
       return this.membership.trusted
     },
+    threshold () {
+      return this.membership.trustThresholdForNewcomer
+    },
+    trustProgress () {
+      return this.membership.trustProgress
+    },
+    isEditor () {
+      return this.membership.isEditor
+    },
     headline () {
       const otherTrust = this.trustedBy.filter(u => !u.isCurrentUser)
       const youTrust = this.trustedBy.find(u => u.isCurrentUser)
-      return this.trustedBy.length > 0 ? this.$tc(
-        youTrust ? (otherTrust.length > 0 ? 'USERDATA.PEOPLE_TRUST_USER' : 'USERDATA.YOU_TRUST_USER') : 'USERDATA.OTHER_PEOPLE_TRUST_USER',
-        otherTrust.length,
-        {
-          count: otherTrust.length,
+
+      const firstTrustMessage = () => this.$t('USERDATA.FIRST_TRUST', {
+        groupName: this.group.name,
+        userName: this.user.displayName,
+      })
+
+      const trustMessagePath = () => {
+        if (!youTrust || this.user.isCurrentUser) return 'USERDATA.OTHER_PEOPLE_TRUST_USER'
+        if (otherTrust.length === 0) return 'USERDATA.YOU_TRUST_USER'
+        return 'USERDATA.PEOPLE_TRUST_USER'
+      }
+
+      const trustMessage = () => this.$tc(trustMessagePath(), otherTrust.length, {
+        count: otherTrust.length,
+        groupName: this.group.name,
+        userName: this.user.isCurrentUser ? this.$t('CONVERSATION.REACTIONS.YOU') : this.user.displayName,
+        otherUser: otherTrust.length > 0 && otherTrust[0].displayName,
+      })
+
+      return (this.trustedBy.length > 0 || this.user.isCurrentUser) ? trustMessage() : firstTrustMessage()
+    },
+    info () {
+      if (!this.isEditor && this.user.isCurrentUser) {
+        return this.$t('USERDATA.NEWCOMER_INFO', {
           groupName: this.group.name,
-          userName: this.user.isCurrentUser ? this.$t('CONVERSATION.REACTIONS.YOU') : this.user.displayName,
-          otherUser: otherTrust.length > 0 && otherTrust[0].displayName,
-        },
-      ) : this.$t(
-        'USERDATA.FIRST_TRUST',
-        {
-          groupName: this.group.name,
+          threshold: this.threshold,
+        })
+      }
+      if (!this.isEditor) {
+        return this.$t('USERDATA.TRUST_PROGRESS_TEXT_LONG', {
           userName: this.user.displayName,
-        },
-      )
+          trustNeeded: this.trustNeeded,
+        })
+      }
+    },
+    trustNeeded () {
+      return this.threshold - this.trustedBy.length
+    },
+    tooltip () {
+      if (this.isEditor) return this.headline
+      return this.$t('USERDATA.NEWCOMER') + ' - ' + this.$t('USERDATA.TRUST_PROGRESS_TEXT', { trustNeeded: this.trustNeeded })
     },
   },
   methods: {
@@ -126,7 +177,7 @@ export default {
         cancel: this.$t('BUTTON.CANCEL'),
         ok: this.$t('BUTTON.OF_COURSE'),
       })
-        .then(() => this.$emit('createTrust', { userId: this.user.id, groupId: this.group.id }))
+        .then(() => this.$emit('createTrust', this.user.id))
         .catch(() => {})
     },
   },
@@ -141,7 +192,11 @@ export default {
   background-position center
   min-height 100%
   min-width 100%
+.karrot-button.small >>> .q-btn-inner
+  background-size 50%
 
-.actionButtons .q-btn
-  margin 20px 0 0 5px
+.knob
+  position absolute
+  top 0
+  left 0
 </style>

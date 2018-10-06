80cbc0d5f43184a54fa47a5f3fd06c3d6022cc64
Tilmann Becker <tilmann.becker@gmail.com>
2018-10-06 09:24:27 +0200

    allow TX_TOKEN in updateLocalesHelper

--- updateLocalesHelper/updateLocales.py
+++ updateLocalesHelper/updateLocales.py
@@ -11,14 +11,28 @@ script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 status_file = os.path.join(script_path, '../src/locales/translationStatus.json')
 
-txrc_file = utils.get_transifex_file()
-txrc = config.OrderedRawConfigParser()
-txrc.read((txrc_file,))
-token = txrc.get('https://www.transifex.com', 'token')
+def get_token():
+    token = os.environ.get('TX_TOKEN')
+    if token:
+        return token
+
+    txrc_file = utils.get_transifex_file()
+    txrc = config.OrderedRawConfigParser()
+    txrc.read((txrc_file,))
+    return txrc.get('https://www.transifex.com', 'token')
+
+token = get_token()
 
 r = requests.get('https://api.transifex.com/organizations/yunity-1/projects/karrot/',
   auth=('api', token)
 )
+
+if r.status_code != 200:
+    print('Failed to get translation status!')
+    print('Response is:')
+    print(r.json())
+    sys.exit(1)
+
 stats = r.json()['stats']
 percentage_dict = {lang: round(v['translated']['percentage']*100) for lang, v in stats.items()}
 percentage_dict['en'] = 100

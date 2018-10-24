karrot-dev/karrot-frontend

commit e61452f70c3d6adf490e5b1d8d7ce13b886e8b68

#968 bug redirect on user settings save (#1032)
* #968 Disable redirect on user settings save

Dispatch an action without pushing a route.

* #968 - refactor save method; added messages

Dispatch backgroundSave with await
Added Notify message from using Quasar
Enclosed the messages in try/catch block to handle potential request errors.

* #968 - refactor - replace Notify with Toasts

Replaced Notidy Quasar component with Toasts abstraction.

* Added translations for new messages.

Added translations for messages: settings saved and an error occured.

* User settings notifications are translatable

Added reference to the i18n package to make notifications translatable.

src/store/modules/auth.js
@@ -1,3 +1,4 @@
+import i18n from '@/i18n'
 import auth from '@/services/api/auth'
 import authUser from '@/services/api/authUser'
 import router from '@/router'
@@ -110,8 +111,21 @@ export default {
 
     ...withMeta({
       async save ({ dispatch }, data) {
-        const savedUser = await dispatch('backgroundSave', data)
-        router.push({ name: 'user', params: { userId: savedUser.id } })
+        try {
+          await dispatch('backgroundSave', data)
+          dispatch('toasts/show', {
+            message: i18n.t('NOTIFICATIONS.CHANGES_SAVED'),
+            timeout: 2000,
+            icon: 'thumb_up',
+          }, { root: true })
+        }
+        catch (error) {
+          dispatch('toasts/show', {
+            message: i18n.t('NOTIFICATIONS.CHANGES_ERROR'),
+            timeout: 2000,
+            icon: 'warning',
+          }, { root: true })
+        }
       },
     }, {
       // ignore ID to have simple saveStatus

commit 114e8e114aac829ef84cd22ff487ec8396ce528c

check displayName length and show validation errors in profileEdit
This partially undoes the changes introduced in https://github.com/yunity/karrot-frontend/pull/1032 because validation error details in ProfileEdit would never be shown to the user.

src/components/Login/Signup.vue
@@ -17,8 +17,8 @@
         <div class="white-box">
           <q-field
             icon="fas fa-user"
-            :error="hasError('displayName')"
-            :error-label="firstError('displayName')"
+            :error="hasDisplayNameError"
+            :error-label="displayNameError"
           >
             <q-input
               :autofocus="true"
@@ -27,6 +27,7 @@
               autocorrect="off"
               autocapitalize="off"
               spellcheck="false"
+              @blur="$v.user.displayName.$touch"
             />
           </q-field>
         </div>
@@ -105,10 +106,12 @@
 import { QCard, QCardTitle, QCardMain, QIcon, QField, QInput, QBtn, QSpinner, QCheckbox } from 'quasar'
 import loginImage from 'assets/people/cherry.png'
 import statusMixin from '@/mixins/statusMixin'
+import { required, minLength, maxLength } from 'vuelidate/lib/validators'
+import { validationMixin } from 'vuelidate'
 
 export default {
   components: { QCard, QCardTitle, QCardMain, QIcon, QField, QInput, QBtn, QSpinner, QCheckbox },
-  mixins: [statusMixin],
+  mixins: [validationMixin, statusMixin],
   props: {
     prefillEmail: {
       required: true,
@@ -141,17 +144,45 @@ export default {
   },
   methods: {
     submit () {
-      if (!this.isPending) {
-        this.$emit('submit', {
-          userData: this.user,
-          joinPlayground: this.joinPlayground,
-        })
-      }
+      this.$v.user.$touch()
+      if (!this.canSave || this.isPending) return
+      this.$emit('submit', {
+        userData: this.user,
+        joinPlayground: this.joinPlayground,
+      })
+      this.$v.user.$reset()
     },
   },
   computed: {
     canJoinPlayground () {
       return this.hasPlayground && !this.hasGroupToJoin
+    },
+    hasDisplayNameError () {
+      return !!this.displayNameError
+    },
+    displayNameError () {
+      if (this.$v.user.displayName.$error) {
+        const m = this.$v.user.displayName
+        if (!m.required) return this.$t('VALIDATION.REQUIRED')
+        if (!m.minLength) return this.$t('VALIDATION.MINLENGTH', { min: 2 })
+        if (!m.maxLength) return this.$t('VALIDATION.MAXLENGTH', { max: 81 })
+      }
+      return this.firstError('displayName')
+    },
+    canSave () {
+      if (this.$v.user.$error) {
+        return false
+      }
+      return true
+    },
+  },
+  validations: {
+    user: {
+      displayName: {
+        required,
+        minLength: minLength(3),
+        maxLength: maxLength(80),
+      },
     },
   },
 }

src/components/Settings/ProfileEdit.vue
@@ -3,14 +3,17 @@
     class="edit-box"
     :class="{ changed: hasChanged }"
   >
-    <form @submit.prevent="save">
+    <form @submit.prevent="maybeSave">
       <q-field
         icon="fas fa-star"
         :label="$t('USERDETAIL.DISPLAY_NAME')"
-        :error="hasError('displayName')"
-        :error-label="firstError('displayName')"
+        :error="hasDisplayNameError"
+        :error-label="displayNameError"
       >
-        <q-input v-model="edit.displayName"/>
+        <q-input
+          v-model="edit.displayName"
+          @blur="$v.edit.displayName.$touch"
+        />
       </q-field>
 
       <q-field
@@ -24,7 +27,7 @@
             v-model="edit.description"
             type="textarea"
             rows="1"
-            @keyup.ctrl.enter="save"
+            @keyup.ctrl.enter="maybeSave"
           />
         </MarkdownInput>
       </q-field>
@@ -89,13 +92,24 @@ import AddressPicker from '@/components/Address/AddressPicker'
 import MarkdownInput from '@/components/MarkdownInput'
 import editMixin from '@/mixins/editMixin'
 import statusMixin from '@/mixins/statusMixin'
+import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength } from 'vuelidate/lib/validators'
 
 export default {
   components: {
     QDatetime, QField, QSlider, QInput, QBtn, QSelect, AddressPicker, MarkdownInput,
   },
-  mixins: [statusMixin, editMixin],
+  mixins: [statusMixin, editMixin, validationMixin],
   computed: {
+    canSave () {
+      if (this.$v.edit.$error) {
+        return false
+      }
+      if (!this.isNew && !this.hasChanged) {
+        return false
+      }
+      return true
+    },
     hasAddressError () {
       return !!this.addressError
     },
@@ -103,6 +117,35 @@ export default {
       for (let field of ['address', 'latitude', 'longitude']) {
         if (this.hasError(field)) return this.firstError(field)
       }
+    },
+    hasDisplayNameError () {
+      return !!this.displayNameError
+    },
+    displayNameError () {
+      if (this.$v.edit.displayName.$error) {
+        const m = this.$v.edit.displayName
+        if (!m.required) return this.$t('VALIDATION.REQUIRED')
+        if (!m.minLength) return this.$t('VALIDATION.MINLENGTH', { min: 2 })
+        if (!m.maxLength) return this.$t('VALIDATION.MAXLENGTH', { max: 81 })
+      }
+      return this.firstError('displayName')
+    },
+  },
+  methods: {
+    maybeSave () {
+      this.$v.edit.$touch()
+      if (!this.canSave) return
+      this.$v.edit.$reset()
+      this.save()
+    },
+  },
+  validations: {
+    edit: {
+      displayName: {
+        required,
+        minLength: minLength(3),
+        maxLength: maxLength(80),
+      },
     },
   },
 }

src/store/modules/auth.js
@@ -125,25 +125,14 @@ export default {
 
     ...withMeta({
       async save ({ dispatch }, data) {
-        try {
-          await dispatch('backgroundSave', data)
-          dispatch('toasts/show', {
-            message: 'NOTIFICATIONS.CHANGES_SAVED',
-            config: {
-              timeout: 2000,
-              icon: 'thumb_up',
-            },
-          }, { root: true })
-        }
-        catch (error) {
-          dispatch('toasts/show', {
-            message: 'NOTIFICATIONS.CHANGES_ERROR',
-            config: {
-              timeout: 2000,
-              icon: 'warning',
-            },
-          }, { root: true })
-        }
+        await dispatch('backgroundSave', data)
+        dispatch('toasts/show', {
+          message: 'NOTIFICATIONS.CHANGES_SAVED',
+          config: {
+            timeout: 2000,
+            icon: 'thumb_up',
+          },
+        }, { root: true })
       },
     }, {
       // ignore ID to have simple saveStatus
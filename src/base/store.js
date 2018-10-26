145309980389002d829b0889826aa046544aea7b
Tilmann Becker <tilmann.becker@gmail.com>
2018-10-26 15:35:57 +0200

    sort store imports

--- src/base/store.js
+++ src/base/store.js
@@ -1,48 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import feedback from '@/feedback/datastore'
-import utils from '@/utils/datastore'
-import messages from '@/messages/datastore'
-import topbar from '@/topbar/datastore'
-import base from '@/base/datastore'
+// alphabetical
 import agreements from '@/agreements/datastore'
-import authuser from '@/authuser/datastore'
-import subscriptions from '@/subscriptions/datastore'
 import alerts from '@/alerts/datastore'
-import notifications from '@/notifications/datastore'
-import communityFeed from '@/communityFeed/datastore'
-import group from '@/group/datastore'
 import applications from '@/applications/datastore'
+import authuser from '@/authuser/datastore'
+import base from '@/base/datastore'
+import communityFeed from '@/communityFeed/datastore'
+import feedback from '@/feedback/datastore'
+import group from '@/group/datastore'
 import groupInfo from '@/groupInfo/datastore'
 import history from '@/history/datastore'
 import invitations from '@/invitations/datastore'
+import messages from '@/messages/datastore'
+import notifications from '@/notifications/datastore'
 import pickups from '@/pickups/datastore'
 import sidenav from '@/sidenav/datastore'
 import stores from '@/stores/datastore'
+import subscriptions from '@/subscriptions/datastore'
+import topbar from '@/topbar/datastore'
 import users from '@/users/datastore'
+import utils from '@/utils/datastore'
 
+// alphabetical
 const appModules = [
-  feedback,
-  utils,
-  messages,
-  topbar,
-  base,
   agreements,
-  authuser,
-  subscriptions,
   alerts,
-  notifications,
-  communityFeed,
-  group,
   applications,
+  authuser,
+  base,
+  communityFeed,
+  feedback,
+  group,
   groupInfo,
   history,
   invitations,
+  messages,
+  notifications,
   pickups,
   sidenav,
   stores,
+  subscriptions,
+  topbar,
   users,
+  utils,
 ]
 
 Vue.use(Vuex)
@@ -50,7 +52,7 @@ Vue.use(Vuex)
 const collectedModules = {}
 const collectedPlugins = []
 
-appModules.forEach(({ modules = {}, plugins = [] }) => {
+appModules.forEach(({modules = {}, plugins = []}) => {
   Object.assign(collectedModules, modules)
   collectedPlugins.push(...plugins)
 })

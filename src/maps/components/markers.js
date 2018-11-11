65c752a0ba5fefe8353760940e0a0b71ac6a5349
Tilmann Becker <tilmann.becker@gmail.com>
2018-11-11 23:36:18 +0100

    fix marker urls for app and use router.resolve

--- src/maps/components/markers.js
+++ src/maps/components/markers.js
@@ -1,8 +1,13 @@
 import { escape } from '@/utils/utils'
+import router from '@/base/router'
 
 // TODO: popupcontent should also accept an HTML element
 // https://leafletjs.com/reference-1.3.4.html#popup-setcontent
 // So we could use vue components inside popups
+
+function groupUrl (groupId) {
+  return router.resolve({ name: 'group', params: { groupId } }).href
+}
 
 export function groupMarker (group) {
   return {
@@ -10,8 +15,12 @@ export function groupMarker (group) {
     id: 'group_' + group.id,
     fontIcon: group.isMember ? 'fas fa-home' : 'fas fa-info-circle',
     color: group.isMember ? 'positive' : 'blue',
-    popupcontent: `<big><a href="/#/group/${group.id}/">${escape(group.name)}</a></big>`,
+    popupcontent: `<big><a href="${groupUrl(group.id)}">${escape(group.name)}</a></big>`,
   }
+}
+
+function userUrl (userId) {
+  return router.resolve({ name: 'user', params: { userId } }).href
 }
 
 export function userMarker (user) {
@@ -20,8 +29,12 @@ export function userMarker (user) {
     id: 'user_' + user.id,
     fontIcon: 'fas fa-user',
     color: 'positive',
-    popupcontent: `<a href="/#/user/${user.id}">${escape(user.displayName)}</a>`,
+    popupcontent: `<a href="${userUrl(user.id)}">${escape(user.displayName)}</a>`,
   }
+}
+
+function storeUrl (groupId, storeId) {
+  return router.resolve({ name: 'store', params: { groupId, storeId } }).href
 }
 
 export function storeMarker (store) {
@@ -30,6 +43,6 @@ export function storeMarker (store) {
     id: 'store_' + store.id,
     fontIcon: 'fas fa-shopping-cart',
     color: store.ui.color,
-    popupcontent: `<a href="/#/group/${store.group.id}/store/${store.id}">${escape(store.name)}</a>`,
+    popupcontent: `<a href="${storeUrl(store.group.id, store.id)}">${escape(store.name)}</a>`,
   }
 }

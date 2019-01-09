0f4128a5de1ada4d6676d0bedc7f83d89197173b
djahnie <djahnie@disroot.org>
2019-01-09 14:30:21 +0100

    intermediate file sharing

--- src/conflictResolution/datastore/conflictResolution.js
+++ src/conflictResolution/datastore/conflictResolution.js
@@ -1,10 +1,29 @@
-import Vue from 'vue'
-import conflictResolution from '@/conflictResolution/api/conflictResolution'
 import router from '@/base/router'
-import { withMeta, createMetaModule, metaStatuses } from '@/utils/datastore/helpers'
 
 function initialState () {
   return {
-    entries: {},
+    entries: {
+      1: {
+        'id': 1,
+        'submitDate': '',
+        'group': 13,
+        'ppp': 4,
+        'initiator': 1,
+        'initialStatement': 'He is so unreliable and I cannot stand him!',
+      },
+    },
   }
+}
+export default {
+  state: initialState(),
+  getters: {
+    get: (state, getters, rootState, rootGetters) => conflictId => {
+      return getters.enrich(state.entries[conflictId])
+    },
+  },
+  actions: {
+    startSetup ({ dispatch }) {
+      router.push({ name: 'conflictResolutionSetup', params: { groupId: currentGroup.id } })
+    },
+  },
 }

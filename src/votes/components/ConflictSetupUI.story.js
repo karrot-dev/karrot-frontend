872569c369f2818f595a7a07272fd853f7343bad
djahnie <djahnie@disroot.org>
2019-01-14 15:36:25 +0100

    revive storybook

---
+++ src/votes/components/ConflictSetupUI.story.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/vue'
+import { storybookDefaults as defaults } from '>/helpers'
+import { groupsMock } from '>/mockdata'
+
+import ConflictSetupUI from './ConflictSetupUI'
+
+storiesOf('ConflictSetup', module)
+  .add('create', () => defaults({
+    render: h => h(ConflictSetupUI, {
+      props: {
+        group: groupsMock[0],
+      },
+    }),
+  }))

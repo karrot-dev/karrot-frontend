9edc60307d2317934095d65912f7644b508c65a6
Tilmann Becker <tilmann.becker@gmail.com>
2019-03-09 17:58:40 +0100

    remove sentry-cordova again

--- src/base/sentry.js
+++ src/base/sentry.js
@@ -1,8 +1,18 @@
+import Vue from 'vue'
 import * as Sentry from '@sentry/browser'
-import config from '@/base/sentryConfig'
 
 if (__ENV.SENTRY_CONFIG) {
-  Sentry.init(config({
+  Sentry.init({
     dsn: __ENV.SENTRY_CONFIG,
-  }))
+    integrations: [new Sentry.Integrations.Vue({ Vue })],
+    release: __ENV.GIT_SHA1,
+    beforeSend: event => {
+      const { values } = event.exception
+      const firstValue = values && values.length > 0 && values[0].value
+      if (firstValue && firstValue.includes('ResizeObserver loop limit exceeded')) {
+        return null
+      }
+      return event
+    },
+  })
 }

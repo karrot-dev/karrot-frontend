25df7cd17ec2ef661bdc223aa2c5a9dd374d8aca
Tilmann Becker <tilmann.becker@gmail.com>
2019-09-17 11:41:13 +0200

    add roadmap parts of the vision text into roadmap.md

--- scripts/generate_roadmap.js
+++ scripts/generate_roadmap.js
@@ -31,9 +31,21 @@ run({
 })
 
 const HEADER = `
-# karrot Roadmap
+# Karrot Roadmap
 
-Only major features and improvements will be listed here.
+## General Direction
+
+First of all, we want to support the existing users on karrot.world, which are mostly foodsaving groups. For this, we want to improve governance and statistics.
+
+To use Karrot for other purposes than saving food, we want to consolidate a user base that is part of two or more groups. Development should focus on a useful platform for them to coordinate with their groups/teams and to create synergies of cooperation between groups and people in those groups. An example of that, which is already happening in Gothenburg, is the foodsaving group (Solikyl) and the Bike Kitchen group. There is potential for much more in this city, which could prototype this kind of use case.
+
+To make Karrot useful to a wider public, we want to add sharing features and allow users to communicate directly with established groups, which would become hubs for saving and sharing. The possibility to create new groups for sharing stuff or coordinating activities would also be a much more straightforward feature.
+
+Karrot is a free and open-source software and its vision should inspire more people to contribute and co-create it. Therefore we need to work also on the following:
+
+- On governance and organisation: finish writing the constitution and defining what kind of association should be created. This would give clear guidelines for the role of contributors and ensures a more transparent and democratic development process.
+- [Discuss and define the need and role of funding](https://community.foodsaving.world/t/funding-concept-for-karrot/136). Apply for grants when time is right and there is an opportunity.
+- Start doing some PR and recruiting: by improving the website with a clearer description of what Karrot does and its vision, reach out in our networks to show it off and recruit new contributors.
 `
 
 async function run ({ owner, repo }) {

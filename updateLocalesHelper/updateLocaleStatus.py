2ef3f8b5bb67bf9367557701eccf87baeb5c482c
Raymon Skjørten Hansen <raymon.s.hansen@gmail.com>
2019-10-09 14:48:23 +0200

    Refactor
    Refactor into smaller functions for easier debugging. Flake 8 compliant.
    Haven't tested, but should be functionally equivalent to previous version.
    * Moved static strings into global variables in case they change
    * Smaller functions
    * Fixed shadowing of 'config' variable

--- updateLocalesHelper/updateLocaleStatus.py
+++ updateLocalesHelper/updateLocaleStatus.py
@@ -1,59 +1,92 @@
 #! /bin/env python
-import json
+from configparser import ConfigParser
+from http import HTTPStatus
+from json import dump
+from os import path, environ
+from requests import get as GET
+import sys
+
 from txclib import utils
 from txclib import config
-import requests
-import os
-import sys
-import configparser
 
-script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 
-status_file = os.path.join(script_path, '../src/locales/translationStatus.json')
-config_file = os.path.join(script_path, '../.tx/config')
+STATUS_FILE_PATH = '../src/locales/translationStatus.json'
+CONFIG_FILE_PATH = '../.tx/config'
+SCRIPT_PATH = path.dirname(path.realpath(sys.argv[0]))
+TRANSLATION_URL = 'https://api.transifex.com/organizations/yunity-1/projects/karrot/'
+DEFAULT_TOKEN_URL = 'https://www.transifex.com'
+
+STATUS_FILE = path.join(SCRIPT_PATH, STATUS_FILE_PATH)
+CONFIG_FILE = path.join(SCRIPT_PATH, CONFIG_FILE_PATH)
+
 
 def get_token():
-    token = os.environ.get('TX_TOKEN')
+    token = environ.get('TX_TOKEN')
     if token:
         return token
 
     txrc_file = utils.get_transifex_file()
     txrc = config.OrderedRawConfigParser()
     txrc.read((txrc_file,))
-    return txrc.get('https://www.transifex.com', 'token')
+    return txrc.get(DEFAULT_TOKEN_URL, 'token')
 
-token = get_token()
 
-r = requests.get('https://api.transifex.com/organizations/yunity-1/projects/karrot/',
-  auth=('api', token)
-)
+def get_translation_status(token):
+    response = GET(TRANSLATION_URL, auth=('api', token))
+    if response.status_code != HTTPStatus.OK:
+        print('Failed to get translation status!\n')
+        print(response.json())
+        sys.exit(1)
+    return response.json()['stats']
 
-if r.status_code != 200:
-    print('Failed to get translation status!')
-    print('Response is:')
-    print(r.json())
-    sys.exit(1)
 
-stats = r.json()['stats']
-percentage_dict = {lang: round(v['translated']['percentage']*100) for lang, v in stats.items()}
-percentage_dict['en'] = 100
+def map_status_to_percentage(translation_status):
+    percentage_dict = {lang: round(v['translated']['percentage']*100) for lang, v in translation_status.items()}
+    percentage_dict['en'] = 100
+    return percentage_dict
 
-config = configparser.ConfigParser()
-config.read(config_file)
-lang_map_raw = config.get('main', 'lang_map')
-lang_map = {}
-for arg in lang_map_raw.replace(' ', '').split(','):
-    k, v = arg.split(":")
-    lang_map.update({k: v})
 
-# translate to HTTP Accept-Language format
-def translate_key(key):
+def get_raw_language_map_from_config_file(config_file):
+    configuration = ConfigParser()
+    configuration.read(config_file)
+    lang_map_raw = configuration.get('main', 'lang_map').replace(' ', '').split(',')
+    return lang_map_raw
+
+
+def clean_lang_map_raw(lang_map_raw):
+    lang_map = dict()
+    for arg in lang_map_raw:
+        k, v = arg.split(":")
+        lang_map[k] = v
+    return lang_map
+
+
+def translate_key(key, lang_map):
+    """Translate to HTTP Accept-Language format"""
     if key in lang_map:
         key = lang_map[key]
     return key.replace('_', '-').lower()
 
-translated_dict = { translate_key(k): v for (k, v) in percentage_dict.items() }
 
-with open(status_file, 'w') as f:
-    json.dump(translated_dict, f, sort_keys=True, indent=2)
-    print("Updated", status_file)
+def get_translated_dict(lang_map, percentage_dict):
+    for k, v in percentage_dict.items():
+        translate_key(k, lang_map)
+    return percentage_dict
+
+
+def write_dict_to_file(dictionary, filename):
+    with open(STATUS_FILE, 'w') as f:
+        dump(dictionary, f, sort_keys=True, indent=2)
+        print("Updated", STATUS_FILE)
+
+
+token = get_token()
+translation_status = get_translation_status(token)
+percentage_dict = map_status_to_percentage(translation_status)
+
+lang_map_raw = get_raw_language_map_from_config_file(CONFIG_FILE)
+lang_map = clean_lang_map_raw(lang_map_raw)
+
+translated_dict = get_translated_dict(lang_map, percentage_dict)
+
+write_dict_to_file(translated_dict, STATUS_FILE)

ef5660bd181c32d58bd209445704c10616915f6e
Nick Sellen <git@nicksellen.co.uk>
2019-11-16 11:45:56 +0100

    Remove status from offer form
    Status change is now done via explicit actions/buttons

--- src/offers/components/OfferForm.vue
+++ src/offers/components/OfferForm.vue
@@ -31,52 +31,6 @@
           @keyup.ctrl.enter="maybeSave"
         />
 
-        <QSelect
-          v-model="edit.status"
-          :options="statusOptions"
-          map-options
-          emit-value
-          :label="$t('OFFEREDIT.STATUS')"
-          :error="hasError('status')"
-          :error-message="firstError('status')"
-        >
-          <template v-slot:before>
-            <QIcon name="fas fa-handshake" />
-          </template>
-          <template v-slot:option="scope">
-            <QItem
-              :key="scope.index"
-              dense
-              v-bind="scope.itemProps"
-              v-on="scope.itemEvents"
-            >
-              <QItemSection side>
-                <QIcon
-                  :name="scope.opt.icon"
-                  :color="scope.opt.color"
-                  size="1.1em"
-                />
-              </QItemSection>
-              <QItemSection>
-                <QItemLabel>{{ scope.opt.label }}</QItemLabel>
-              </QItemSection>
-            </QItem>
-          </template>
-          <template v-slot:selected-item="scope">
-            <div class="row">
-              <QIcon
-                :name="scope.opt.icon"
-                :color="scope.opt.color"
-                size="1.1em"
-                class="on-left q-ml-xs"
-              />
-              <div>
-                {{ scope.opt.label }}
-              </div>
-            </div>
-          </template>
-        </QSelect>
-
         <MultiCroppa v-model="edit.images" />
 
         <div class="row justify-end q-gutter-sm q-mt-sm">
@@ -108,7 +62,7 @@ import { validationMixin } from 'vuelidate'
 import { required, minLength, maxLength } from 'vuelidate/lib/validators'
 import editMixin from '@/utils/mixins/editMixin'
 import statusMixin from '@/utils/mixins/statusMixin'
-import { QBtn, QCard, QIcon, QInput, QSelect, QItem, QItemLabel, QItemSection } from 'quasar'
+import { QBtn, QCard, QIcon, QInput } from 'quasar'
 import MarkdownInput from '@/utils/components/MarkdownInput'
 import MultiCroppa from '@/offers/components/MultiCroppa'
 
@@ -120,10 +74,6 @@ export default {
     QCard,
     QInput,
     QIcon,
-    QSelect,
-    QItem,
-    QItemLabel,
-    QItemSection,
   },
   mixins: [validationMixin, editMixin, statusMixin],
   props: {

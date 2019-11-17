156c011c5ba26a6155747d29a575c9151dc7a763
Nick Sellen <git@nicksellen.co.uk>
2019-11-17 12:24:02 +0100

    Use address autocomplete (#1878)
    * Use address autocomplete
    
    As it used to be.
    Also defaults place edit map to be group centre if available.
    
    * Remove uneeded extra wrapped div
    
    * Update setup for storyshots testing
    
    "sync: false" seems to be the new way it goes and will be the default
    in the future, see https://github.com/vuejs/vue-test-utils/issues/1137
    
    The transition group stubs seem to be there by default now too, I think..
    Anyway it works where it did no tbefore :)
    
    Also see https://github.com/vuejs/vue-test-utils/issues/1163
    
    * Add @types/jest - for editor support
    
    See https://blog.jetbrains.com/webstorm/2018/10/testing-with-jest-in-webstorm/
    
    * Increase test wait
    
    Not really sure why this changed, but don't want to think about
    it for now...
    
    * Add text to translation file
    
    * Use the spelling of the enemy
    
    * Simplify template logic

--- src/places/components/PlaceEdit.vue
+++ src/places/components/PlaceEdit.vue
@@ -86,6 +86,7 @@
             :label="$t('STOREEDIT.ADDRESS')"
             :error="hasAddressError"
             :error-message="addressError"
+            :default-map-center="defaultMapCenter"
           />
 
           <div>
@@ -269,6 +270,11 @@ export default {
     },
     markerColor () {
       if (this.edit) return optionsFor(this.edit).color
+      return null
+    },
+    defaultMapCenter () {
+      const { latitude: lat, longitude: lng } = this.edit.group || {}
+      if (lat && lng) return { lat, lng }
       return null
     },
   },

a14b230811fba1a9bab5d65cf2780ed74a2e7db0
Nick Sellen <git@nicksellen.co.uk>
2019-11-18 17:11:06 +0100

    Add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: 'stylelint-config-recess-order',
+  rules: {
+    'block-no-empty': null,
+    'color-no-invalid-hex': true,
+    'comment-empty-line-before': ['always', {
+      ignore: ['stylelint-commands', 'after-comment'],
+    }],
+    'declaration-colon-space-after': 'always',
+    indentation: [2, {
+      except: ['value'],
+    }],
+    'max-empty-lines': 2,
+    'rule-empty-line-before': ['always', {
+      except: ['first-nested'],
+      ignore: ['after-comment'],
+    }],
+    'unit-whitelist': [
+      'px',
+      'em',
+      'rem',
+      '%',
+      's',
+      'deg',
+      'vw',
+      'vh',
+      'vmax',
+    ],
+  },
+}

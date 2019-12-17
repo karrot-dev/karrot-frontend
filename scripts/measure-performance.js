299a1a7f72159e18e9bf50838b87c8fbdc849384
Nick Sellen <git@nicksellen.co.uk>
2019-12-17 20:53:04 +0100

    Add rough measure-peformance script

---
+++ scripts/measure-performance.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+const cookie = require('cookie');
+
+puppeteer.launch({ headless: false }).then(async browser => {
+
+  // log in directly via API
+  const data = await axios.post('http://localhost:8080/api/auth/', {
+    email: 'foo@foo.com',
+    password: 'foofoo'
+  })
+
+  // suck out the cookies (pretty crappy/flakey implementation)
+  const cookies = data.headers['set-cookie'].map(cookie.parse).map(d => {
+    return {
+      name: d.csrftoken ? 'csrftoken' : 'sessionid',
+      value: d.csrftoken || d.sessionid,
+      domain: 'localhost:8080',
+      path: d.Path,
+      sameSite: d.SameSite,
+      expires: new Date(d.expires).getTime() / 1000
+    }
+  });
+
+  console.log('cookies', cookies);
+
+  const page = await browser.newPage();
+
+  page.setCookie(...cookies);
+
+  await page.exposeFunction('onCustomEvent', ({ type, detail }) => {
+      console.log(detail);
+      browser.close();
+  });
+  
+  await page.evaluateOnNewDocument(() => {
+      window.addEventListener('measured', ({ type, detail }) => {
+          window.onCustomEvent({ type, detail });
+      });
+  });
+
+  await page.goto('http://localhost:8080/');
+});

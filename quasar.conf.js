bde9f5b7365002d2edab3f3272b5164c55edb317
Tilmann Becker <tilmann.becker@gmail.com>
2020-09-01 17:34:25 +0200

    add icongenie

--- quasar.conf.js
+++ quasar.conf.js
@@ -234,7 +234,7 @@ module.exports = configure(function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#4a3520',
         icons: [
           {
             src: 'icons/icon-128x128.png',

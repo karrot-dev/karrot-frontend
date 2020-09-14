0700821d897bbe2eb92794e1dc92a444e2a6ce1a
Nick Sellen <git@nicksellen.co.uk>
2020-09-14 12:07:01 +0200

    Fix brain error camelize -> underscoreize
    Meant the statistics was not being filtered for date range

--- src/statistics/api/statistics.js
+++ src/statistics/api/statistics.js
@@ -1,11 +1,11 @@
 import axios from '@/base/api/axios'
-import { camelizeKeys } from '@/utils/utils'
+import { underscorizeKeys } from '@/utils/utils'
 
 export default {
   async activityHistory ({ group, user, dateAfter, dateBefore }) {
     return (await axios.get(
       '/api/stats/activity-history/',
-      { params: camelizeKeys({ group, user, dateAfter, dateBefore }) },
+      { params: underscorizeKeys({ group, user, dateAfter, dateBefore }) },
     )).data
   },
 }

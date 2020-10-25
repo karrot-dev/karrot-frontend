e3e56d620cf1720f5bcf259b35353d20beef6e9c
Nick Sellen <git@nicksellen.co.uk>
2020-10-25 19:02:37 +0100

    Minor formatting change

--- src/activities/api/activityTypes.js
+++ src/activities/api/activityTypes.js
@@ -1,7 +1,6 @@
 import axios from '@/base/api/axios'
 
 export default {
-
   async list (filter) {
     const params = filter || {}
     return (await axios.get('/api/activity-types/', { params })).data

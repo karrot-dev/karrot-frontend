fafbdc73de4a42dd70e838ba3180d24b15cac12c
Nick Sellen <git@nicksellen.co.uk>
2021-01-12 13:06:30 +0000

    Remove commented out code

--- src/group/pages/EditActivityTypes.vue
+++ src/group/pages/EditActivityTypes.vue
@@ -5,15 +5,11 @@ import EditActivityTypesUI from '@/group/components/EditActivityTypesUI'
 export default connect({
   gettersToProps: {
     activityTypes: 'activityTypes/byCurrentGroup',
-    // status: 'activityTypes/saveStatus',
     activityTypeCreateStatus: 'activityTypes/createStatus',
   },
   actionsToEvents: {
     create: 'activityTypes/create',
     save: 'activityTypes/save',
   },
-  // methodsToEvents: {
-  //   reset: ({ dispatch }, id) => dispatch('groups/meta/clear', ['save', id]),
-  // },
 })('EditActivityTypes', EditActivityTypesUI)
 </script>

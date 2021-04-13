3c0b4e69454771f8f9d30b64e1d948a7aa3bdf1d
Nick Sellen <git@nicksellen.co.uk>
2021-04-13 12:41:45 +0100

    Change nearby groups calculation minimum distance

--- src/groupInfo/components/GroupGalleryMap.vue
+++ src/groupInfo/components/GroupGalleryMap.vue
@@ -54,9 +54,9 @@ export default {
       const groups = this.groupsWithCoordinates.filter(this.hasDistance)
       if (groups.length === 0) return []
       // Any group with max 3x distance of the closest group
-      // Minimum "closest distance" is 30km, so it won't filter for unhelpful
+      // Minimum "closest distance" is 10km, so it won't filter for unhelpful
       // distances like <3km if you happen to be 1km from a group...
-      const closestDistance = Math.max(groups[0].distance, 30)
+      const closestDistance = Math.max(groups[0].distance, 10)
       const maxDistance = closestDistance * 3
       return groups.filter(group => group.distance < maxDistance)
     },

c2add33b3644c016e711294a49f3ff0c2e7ae895
pogopaule <pogopaule@posteo.de>
2021-05-18 20:41:12 +0200

    update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -236,7 +236,7 @@ module.exports = configure(function (ctx) {
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
       iconSet: 'material-icons', // Quasar icon set
-      lang: 'en-us', // Quasar language pack
+      lang: 'en-US', // Quasar language pack
       config: {},
 
       // Possible values for "importStrategy":

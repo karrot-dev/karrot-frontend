karrot-dev/karrot-frontend

commit d1d71f6ee9f4b0ff1bf019812e63deffe3a0747b

replace usages of group.members (#2383)
* rewrite usages of group.members

* use new events

* maybe reload user info when receiving a conversation message

fixes a bug when the applicant wouldn't see the authors' name when the conversation message would be delivered via websocket

* fix/extend tests

* remove blank line

* update changelog

src/groupInfo/datastore/groups.js
@@ -20,15 +20,12 @@ export default {
     },
     enrich: (state, getters, rootState, rootGetters) => group => {
       if (!group) return
-      const userId = rootGetters['auth/userId']
-      const isMember = userId && group.members ? group.members.includes(userId) : false
       const isCurrentGroup = group.id === rootGetters['currentGroup/id']
       const isPlayground = group.status === 'playground'
       const isInactive = group.status === 'inactive'
       const myApplicationPending = rootGetters['applications/getMineInGroup'] && rootGetters['applications/getMineInGroup'](group.id)
       return {
         ...group,
-        isMember,
         isCurrentGroup,
         isPlayground,
         isInactive,
@@ -42,15 +39,6 @@ export default {
     },
     mineWithApplications: (state, getters) => getters.all.filter(myGroupsWithApplications).sort(applicationsFirstThenSortByName),
     mine: (state, getters) => getters.all.filter(myGroups).sort(applicationsFirstThenSortByName),
-    // A de-duplicated list of member ids of all groups the user is part of
-    myMemberIds: (state, getters) => {
-      return Object.keys(getters.mineWithApplications.reduce((obj, group) => {
-        for (const member of group.members) {
-          obj[member] = true
-        }
-        return obj
-      }, {})).map(parseInt).sort()
-    },
     other: (state, getters) => getters.all.filter(e => !myGroupsWithApplications(e)).sort(sortByDistanceOrMemberCount),
     activePreview: (state, getters) => getters.get(state.activePreviewId),
     saveStatus: (state, getters, rootState, rootGetters) => {
@@ -144,7 +132,7 @@ function applicationsFirstThenSortByName (a, b) {
 // ... then ones without distance below, ordered by member count
 function sortByDistanceOrMemberCount (a, b) {
   if (a.distance === null && b.distance === null) {
-    return b.members.length - a.members.length
+    return b.memberCount - a.memberCount
   }
   else if (a.distance === null) {
     return 1

src/base/datastore/dependentState.js
@@ -1,13 +1,5 @@
 export default datastore => {
-  // TODO split up and move into users/datastore and places/datastore
-
-  // If any change to our group members then available users may change, so refresh them
-  const updateUserList = () => {
-    if (!datastore.getters['auth/isLoggedIn']) return
-    datastore.dispatch('users/fetch', null, { root: true })
-  }
-  const memberIdsGetter = () => datastore.getters['groups/myMemberIds'].join(',')
-  datastore.watch(memberIdsGetter, updateUserList)
+  // TODO move into places/datastore
 
   // If our groups change, then available places may change, so refresh them
   const updatePlaceList = () => {

src/base/pages/Landing.vue
@@ -243,7 +243,7 @@ export default {
         // take the best 6 entries
         .slice(0, 6)
         // sort those results by how many people they have
-        .sort((a, b) => b.members.length - a.members.length)
+        .sort((a, b) => b.memberCount - a.memberCount)
     },
   },
   created () {

src/boot/socket.js
@@ -171,6 +171,7 @@ export default async function ({ store: datastore }) {
         if (message.thread) {
           datastore.dispatch('latestMessages/updateThreadsAndRelated', { threads: [message] })
         }
+        datastore.dispatch('conversations/fetchRelatedUserInfo', [message])
       }
     }
     else if (topic === 'conversations:conversation') {
@@ -196,6 +197,12 @@ export default async function ({ store: datastore }) {
     }
     else if (topic === 'groups:group_preview') {
       datastore.commit('groups/update', [camelizeKeys(payload)])
+    }
+    else if (topic === 'groups:user_joined') {
+      datastore.dispatch('users/fetch', null, { root: true })
+    }
+    else if (topic === 'groups:user_left') {
+      datastore.dispatch('users/fetch', null, { root: true })
     }
     else if (topic === 'invitations:invitation') {
       datastore.commit('invitations/update', [convertInvitation(camelizeKeys(payload))])

src/groupInfo/components/GroupGallery.story.js
@@ -3,7 +3,13 @@ import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 
 import GroupGallery from './GroupGalleryUI'
-import { groupsMock } from '>/mockdata'
+import { makeGroupInfo } from '>/enrichedFactories'
+
+const groups = [
+  makeGroupInfo({ memberCount: 20 }),
+  makeGroupInfo({ memberCount: 40 }),
+  makeGroupInfo({ memberCount: 60 }),
+]
 
 const defaultOn = {
   preview: action('view group preview'),
@@ -14,7 +20,7 @@ storiesOf('GroupGallery', module)
   .add('signup view', () => defaults({
     render: h => h(GroupGallery, {
       props: {
-        otherGroups: groupsMock,
+        otherGroups: groups,
       },
       on: defaultOn,
     }),
@@ -22,8 +28,11 @@ storiesOf('GroupGallery', module)
   .add('switch and explore', () => defaults({
     render: h => h(GroupGallery, {
       props: {
-        myGroups: groupsMock.slice(0, 3).map(g => ({ ...g, id: g.id * 10, isMember: true })),
-        otherGroups: groupsMock,
+        myGroups: [
+          makeGroupInfo({ isMember: true }),
+          makeGroupInfo({ isMember: true }),
+        ],
+        otherGroups: groups,
         isLoggedIn: true,
       },
       on: defaultOn,

src/groupInfo/components/GroupGalleryCard.story.js
@@ -4,7 +4,7 @@ import { action } from '@storybook/addon-actions'
 
 import GroupGalleryCard from './GroupGalleryCard'
 
-import { groupsMock } from '>/mockdata'
+import { makeGroupInfo } from '>/enrichedFactories'
 
 const methods = {
   preview: action('view group preview'),
@@ -15,10 +15,7 @@ storiesOf('GroupGalleryCard', module)
   .add('isMember = true', () => defaults({
     render: h => h(GroupGalleryCard, {
       props: {
-        group: {
-          ...groupsMock[0],
-          isMember: true,
-        },
+        group: makeGroupInfo({ isMember: true }),
       },
       on: methods,
     }),
@@ -26,10 +23,7 @@ storiesOf('GroupGalleryCard', module)
   .add('isMember = false', () => defaults({
     render: h => h(GroupGalleryCard, {
       props: {
-        group: {
-          ...groupsMock[0],
-          isMember: false,
-        },
+        group: makeGroupInfo(),
       },
       on: methods,
     }),
@@ -37,11 +31,7 @@ storiesOf('GroupGalleryCard', module)
   .add('isMember = false, application pending', () => defaults({
     render: h => h(GroupGalleryCard, {
       props: {
-        group: {
-          ...groupsMock[0],
-          isMember: false,
-          myApplicationPending: true,
-        },
+        group: makeGroupInfo({ myApplicationPending: true }),
       },
       on: methods,
     }),
@@ -49,11 +39,7 @@ storiesOf('GroupGalleryCard', module)
   .add('without public description', () => defaults({
     render: h => h(GroupGalleryCard, {
       props: {
-        group: {
-          ...groupsMock[0],
-          publicDescription: '',
-          isMember: false,
-        },
+        group: makeGroupInfo({ publicDescription: '' }),
       },
       on: methods,
     }),

src/groupInfo/components/GroupGalleryCard.vue
@@ -52,7 +52,7 @@
               />
             </span>
             <span class="text-subtitle2">
-              {{ group.members.length }} {{ $tc('JOINGROUP.NUM_MEMBERS', group.members.length) }}
+              {{ group.memberCount }} {{ $tc('JOINGROUP.NUM_MEMBERS', group.memberCount) }}
             </span>
           </div>
         </div>

commit 1176cbb9173cf72483e6e28318efa3f14d9465ad

reload groups-info on login/logout
This fixes another critical error I introduced with https://github.com/yunity/karrot-frontend/pull/2383.
By having group.isMember dependent on user auth state, we need to reload groups info after login and logout.

src/groupInfo/datastore/groups.js
@@ -150,3 +150,9 @@ function myGroupsWithApplications (group) {
 function myGroups (group) {
   return group.isMember
 }
+
+export function plugin (datastore) {
+  datastore.watch((state, getters) => getters['auth/isLoggedIn'], isLoggedIn => {
+    datastore.dispatch('groups/fetch', null, { root: true })
+  })
+}

src/groupInfo/datastore/index.js
@@ -1,7 +1,10 @@
-import groups from '@/groupInfo/datastore/groups'
+import groups, { plugin } from '@/groupInfo/datastore/groups'
 
 export default {
   modules: {
     groups,
   },
+  plugins: [
+    plugin,
+  ],
 }
0a29a4ad06e1621c6709aa442294dee92ced2699
Tilmann Becker <tilmann.becker@gmail.com>
2021-07-18 13:36:18 +0200

    add one more comment

--- src/feedback/components/FeedbackList.vue
+++ src/feedback/components/FeedbackList.vue
@@ -101,5 +101,6 @@ export default {
 
 .highlight
   border-color $secondary
+  /* Override !important of no-shadow class */
   box-shadow alpha($secondary, 0.3) 0px 0px 0px 3px !important
 </style>

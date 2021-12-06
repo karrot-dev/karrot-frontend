47343b79decadfefc8c3158525f301816e93476a
Nick Sellen <git@nicksellen.co.uk>
2021-12-06 12:53:45 +0000

    Update babel-eslint package
    Now uses @babel/eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   root: true,
 
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },

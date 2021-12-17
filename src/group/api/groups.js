87f67b33206b477fd20a45dfdc578f2bce963895
Kori Reine <kkr.reine@gmail.com>
2021-12-17 17:43:29 -0600

    edit group api to implement name change

--- src/group/api/groups.js
+++ src/group/api/groups.js
@@ -83,6 +83,8 @@ export function convert (val) {
     return val.map(convert)
   }
   else {
+    val.information = val.description
+    delete val.description
     Object.values(val.memberships).forEach(convertMembership)
     return {
       ...val,

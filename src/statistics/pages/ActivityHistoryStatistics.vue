9f12f4928a97582709510742798b1f0f5ed60c74
Nick Sellen <git@nicksellen.co.uk>
2021-12-23 16:14:47 +0000

    Add more statistics filter options (#2467)
    * Fix aggregate options
    
    Was possible to filter for individual users over 12 months due to
    bug in code.
    
    * Add years filter for current and last 2 years
    
    * Add changelog entry
    
    * Fix linting error

--- src/statistics/pages/ActivityHistoryStatistics.vue
+++ src/statistics/pages/ActivityHistoryStatistics.vue
@@ -29,7 +29,28 @@
           filled
           :options="periodFilterOptions"
           class="q-mr-sm"
-        />
+          style="min-width: 180px;"
+        >
+          <template #option="{ index, itemProps, itemEvents, opt: { label: itemLabel, sectionLabel } }">
+            <template v-if="sectionLabel">
+              <QSeparator />
+              <QItemLabel header>
+                {{ sectionLabel }}
+              </QItemLabel>
+            </template>
+            <QItem
+              :key="index"
+              v-bind="itemProps"
+              v-on="itemEvents"
+            >
+              <QItemSection>
+                <QItemLabel>
+                  {{ itemLabel }}
+                </QItemLabel>
+              </QItemSection>
+            </QItem>
+          </template>
+        </QSelect>
         <QSelect
           v-model="leftOptionsSelected"
           :label="$t('STATISTICS.COLUMN_ACTIVITY_LEFT')"
@@ -68,13 +89,14 @@
 </template>
 
 <script>
-import { QSelect, QTable, QToggle, QItem, QItemSection, QItemLabel } from 'quasar'
+import { QSelect, QTable, QToggle, QItem, QItemSection, QItemLabel, QSeparator } from 'quasar'
 import subDays from 'date-fns/subDays'
 import subMonths from 'date-fns/subMonths'
 
 import api from '@/statistics/api/statistics'
 import { mapGetters } from 'vuex'
 import { indexById } from '@/utils/datastore/helpers'
+import { endOfYear, getYear, startOfYear, subYears } from 'date-fns'
 
 export default {
   components: {
@@ -84,6 +106,7 @@ export default {
     QItem,
     QItemSection,
     QItemLabel,
+    QSeparator,
   },
   data () {
     return {
@@ -178,35 +201,105 @@ export default {
       })
     },
     periodFilterOptions () {
+      const now = new Date()
       return [
-        // If you add new options, be sure to handle them in dateQuery too
         {
           label: this.$t('STATISTICS.FILTER_TIME_PREVIOUS_DAYS', { count: 7 }),
           value: '7days',
+          dateQuery () {
+            return {
+              dateAfter: subDays(new Date(), 7),
+            }
+          },
         },
         {
           label: this.$t('STATISTICS.FILTER_TIME_PREVIOUS_DAYS', { count: 30 }),
           value: '30days',
+          dateQuery () {
+            return {
+              dateAfter: subDays(new Date(), 30),
+            }
+          },
         },
         {
           label: this.$t('STATISTICS.FILTER_TIME_PREVIOUS_MONTHS', { count: 3 }),
           value: '3months',
+          dateQuery () {
+            return {
+              dateAfter: subMonths(new Date(), 3),
+            }
+          },
         },
         {
           label: this.$t('STATISTICS.FILTER_TIME_PREVIOUS_MONTHS', { count: 6 }),
           value: '6months',
+          dateQuery () {
+            return {
+              dateAfter: subMonths(new Date(), 6),
+            }
+          },
         },
         {
           label: this.$t('STATISTICS.FILTER_TIME_PREVIOUS_MONTHS', { count: 12 }),
           value: '12months',
-          disable: this.hasUserFilter,
+          onlyAggregate: true,
+          dateQuery () {
+            return {
+              dateAfter: subMonths(new Date(), 12),
+            }
+          },
         },
         {
           label: this.$t('STATISTICS.FILTER_TIME_FOREVER'),
           value: null,
-          disable: this.hasUserFilter,
+          onlyAggregate: true,
+          dateQuery () {
+            return {}
+          },
         },
-      ]
+        {
+          label: getYear(now) - 2,
+          value: 'twoyearsago',
+          onlyAggregate: true,
+          sectionLabel: this.$t('STATISTICS.FILTER_TIME_YEARS_LABEL'),
+          dateQuery () {
+            const date = subYears(new Date(), 2)
+            return {
+              dateAfter: startOfYear(date),
+              dateBefore: endOfYear(date),
+            }
+          },
+        },
+        {
+          label: getYear(now) - 1,
+          value: 'lastyear',
+          onlyAggregate: true,
+          dateQuery () {
+            const date = subYears(new Date(), 1)
+            return {
+              dateAfter: startOfYear(date),
+              dateBefore: endOfYear(date),
+            }
+          },
+        },
+        {
+          label: getYear(now),
+          value: 'thisyear',
+          onlyAggregate: true,
+          dateQuery () {
+            const date = new Date()
+            return {
+              dateAfter: startOfYear(date),
+              dateBefore: endOfYear(date),
+            }
+          },
+        },
+      ].map(option => {
+        return {
+          ...option,
+          disable: option.onlyAggregate ? this.hasUserFilter : false,
+        }
+      })
     },
     totals () {
       return [
@@ -250,33 +343,7 @@ export default {
     },
     dateQuery () {
       if (!this.periodFilter) return {}
-      const now = new Date()
-      switch (this.periodFilter.value) {
-        case '7days':
-          return {
-            dateAfter: subDays(now, 7),
-          }
-        case '30days':
-          return {
-            dateAfter: subDays(now, 30),
-          }
-        case '3months':
-          return {
-            dateAfter: subMonths(now, 3),
-          }
-        case '6months':
-          return {
-            dateAfter: subMonths(now, 6),
-          }
-        case '12months':
-          return {
-            dateAfter: subMonths(now, 12),
-          }
-        case null:
-          return {}
-        default:
-          throw new Error(`unknown date filter option: ${this.periodFilter.value}`)
-      }
+      return this.periodFilter.dateQuery()
     },
     leftOptionsValues () {
       return this.leftOptionsSelected.map(option => option.value)
@@ -292,9 +359,9 @@ export default {
   },
   watch: {
     'userFilter.value' (value) {
-      if (value !== null && this.periodFilter.value === null) {
-        // It's required to have a period if have a user...
-        this.periodFilter = this.periodFilterOptions.find(option => option.value === '6months')
+      if (value !== null && this.periodFilter.onlyAggregate) {
+        // Cannot have a user selected with an "onlyAggregate" filter, select the highest possible one
+        this.periodFilter = this.periodFilterOptions.filter(option => !option.onlyAggregate).reverse()[0]
       }
     },
     query: {

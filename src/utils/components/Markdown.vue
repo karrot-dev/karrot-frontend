31b91a4340bb0d6f9ac6fbc11fb99a52f37898d0
Niharika <niharika@fsfe.org>
2022-07-11 11:17:59 +0200

    making a .vue file REUSE compliant

--- src/utils/components/Markdown.vue
+++ src/utils/components/Markdown.vue
@@ -1,3 +1,14 @@
+<!-- 
+SPDX-FileCopyrightText: 2016 Nick Sellen <hello@nicksellen.co.uk> 
+SPDX-FileCopyrightText: 2016 Karrot
+
+SPDX-License-Identifier: MIT
+
+Karrot
+-->
+
+
+
 <script>
 import markdown from './markdownRenderer'
 import { mapGetters } from 'vuex'

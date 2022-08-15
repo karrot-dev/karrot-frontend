71b7cacde8001e9b4830f19fe8443a264e643520
Nick Sellen <git@nicksellen.co.uk>
2022-08-15 09:49:21 +0100

    Remove unused code

--- src/activities/mutations.js
+++ src/activities/mutations.js
@@ -67,24 +67,6 @@ export function useSaveActivityTypeMutation () {
   ))
 }
 
-/*
-async save ({ commit, dispatch }, activityType) {
-  const data = await activityTypes.save(activityType)
-  commit('update', [data])
-  dispatch('toasts/show', {
-    message: 'NOTIFICATIONS.CHANGES_SAVED',
-    config: {
-      timeout: 2000,
-      icon: 'thumb_up',
-    },
-  }, { root: true })
-},
-async create ({ commit, dispatch, rootGetters }, data) {
-  await activityTypes.create({ group: rootGetters['currentGroup/id'], ...data })
-  dispatch('refresh')
-},
- */
-
 export function useICSRefreshTokenMutation (mutationOptions = {}) {
   const queryClient = useQueryClient()
   return useMutation(

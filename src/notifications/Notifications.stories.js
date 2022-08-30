91c339e2e32e6944727cf895239e88b1daf2add8
Tilmann Becker <tilmann.becker@gmail.com>
2022-08-30 09:15:46 +0200

    move notification stories up one level

--- src/notifications/Notifications.stories.js
+++ src/notifications/Notifications.stories.js
@@ -14,14 +14,9 @@ import {
 } from '>/mockBackend'
 import { addUserToGroup } from '>/mockBackend/groups'
 
-import Notifications from './Notifications'
+import Notifications from './components/Notifications'
 
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Notifications',
   component: Notifications,
 }
 

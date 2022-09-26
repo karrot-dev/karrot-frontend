919a3009c8ef4210ba0304374e50e8745bb095af
Tilmann Becker <tilmann.becker@gmail.com>
2022-09-26 17:10:14 +0200

    Refine places display (#2581)
    Add a new "place gallery" page to have a nice display of all places with additional information. The sidenav place list will only contain favorite places that users marked themselves (with the 'star' button on the place page).
    
    Code-wise, I refactored a bit, mostly by merging three components SidenavPlaces, SidenavPlacesUI and PlaceList. Hence quite some removed lines in the PR.

---
+++ src/places/pages/Places.spec.js
@@ -0,0 +1,52 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/vue'
+import { times } from 'lodash'
+
+import { resetServices } from '@/utils/datastore/helpers'
+
+import { withDefaults } from '>/helpers'
+import {
+  useMockBackend,
+  createUser,
+  createGroup,
+  loginAs,
+  createPlace,
+  createPlaceType,
+} from '>/mockBackend'
+import { addUserToGroup } from '>/mockBackend/groups'
+import '>/routerMocks'
+
+import Places from './Places'
+
+describe('Places', () => {
+  let places
+  let placeType
+  useMockBackend()
+
+  beforeEach(() => {
+    jest.resetModules()
+    resetServices()
+  })
+
+  beforeEach(() => {
+    const user = createUser()
+    const group = createGroup()
+    addUserToGroup(user, group)
+    user.currentGroup = group.id
+    placeType = createPlaceType({ group: group.id })
+    places = times(3, () => createPlace({ group: group.id }))
+    loginAs(user)
+  })
+
+  it('renders a list of places', async () => {
+    const { findByText, findAllByText } = render(Places, withDefaults())
+
+    // place name will be there
+    for (const place of places) {
+      await findByText(place.name)
+    }
+
+    // place type will be there as well
+    await findAllByText(placeType.name)
+  })
+})
--- src/sidenav/components/SidenavPlaces.vue
+++ src/sidenav/components/SidenavPlaces.vue
@@ -1,26 +1,130 @@
 <template>
-  <SidenavPlacesUI
-    :group-id="groupId"
-    :places="places"
-    :show-all-places="showAllPlaces"
-    :archived="archivedPlaces"
-    :is-editor="isEditor"
-    :pending="isLoadingPlaces"
-    @toggle-show-all-places="showAllPlaces = !showAllPlaces"
-  />
+  <SidenavBox>
+    <template #icon>
+      <QIcon
+        name="fas fa-fw fa-star"
+        color="secondary"
+      />
+    </template>
+    <template #name>
+      {{ $t('PLACE_LIST.FAVORITE_PLACES') }}
+    </template>
+
+    <KSpinner v-if="isLoadingPlaces" />
+
+    <template v-else>
+      <QList>
+        <QItem
+          v-for="place in subscribedPlaces"
+          :key="place.id"
+          :to="{ name: placeRoute(place), params: { groupId, placeId: place.id }}"
+          :class="{'router-link-active': getIsActivePlace(place)}"
+          dense
+        >
+          <QItemSection side>
+            <QIcon
+              v-bind="getPlaceIconProps(place)"
+              size="1.1em"
+            />
+          </QItemSection>
+          <QItemSection>
+            <QItemLabel
+              class="items-baseline"
+            >
+              {{ place.name }}
+            </QItemLabel>
+          </QItemSection>
+          <QItemSection
+            v-if="getUnreadWallMessageCount(place) > 0"
+            side
+          >
+            <QBadge
+              color="secondary"
+            >
+              {{ getUnreadWallMessageCount(place) > 99 ? '99+' : getUnreadWallMessageCount(place) }}
+            </QBadge>
+          </QItemSection>
+        </QItem>
+      </QList>
+
+      <div v-if="subscribedPlaces.length < 1">
+        <QBanner
+          class="bg-info q-mx-md q-mb-xs"
+          rounded
+        >
+          <i18n-t keypath="PLACE_LIST.SUBSCRIBE_HINT">
+            <template #icon>
+              <QIcon name="fas fa-star" />
+            </template>
+          </i18n-t>
+        </QBanner>
+      </div>
+
+      <QItem
+        :to="{ name: 'places', params: { groupId } }"
+        dense
+      >
+        <QItemSection side>
+          <QIcon
+            name="fas fa-arrow-right"
+            size="1.1em"
+          />
+        </QItemSection>
+        <QItemSection>
+          {{ $t('STOREEDIT.SHOW_ALL') }} ({{ activePlaceCount }})
+        </QItemSection>
+      </QItem>
+    </template>
+  </SidenavBox>
 </template>
 
 <script setup>
-import { useCurrentGroupService } from '@/group/services'
+import {
+  QIcon,
+  QItemSection,
+  QItem,
+  QList,
+  QItemLabel,
+  QBadge,
+  QBanner,
+} from 'quasar'
+import { computed } from 'vue'
 
-import SidenavPlacesUI from './SidenavPlacesUI'
+import { useCurrentGroupService } from '@/group/services'
+import { usePlaceHelpers } from '@/places/helpers'
+import { placeRoute } from '@/places/utils'
+import { useStatusService } from '@/status/services'
+
+import KSpinner from '@/utils/components/KSpinner'
+
+import SidenavBox from './SidenavBox'
+
+const {
+  getIsActivePlace,
+  getPlaceIconProps,
+} = usePlaceHelpers()
 
 const {
   groupId,
-  isEditor,
   places,
-  showAllPlaces,
-  archivedPlaces,
   isLoadingPlaces,
 } = useCurrentGroupService()
+
+const {
+  getPlaceStatus,
+} = useStatusService()
+
+function getUnreadWallMessageCount (place) {
+  return getPlaceStatus(place.id).unreadWallMessageCount
+}
+
+const subscribedPlaces = computed(() => places.value.filter(place => place.isSubscribed))
+const activePlaceCount = computed(() => places.value.filter(place => place.status === 'active').length)
 </script>
+
+<style scoped lang="sass">
+.tools
+  .bottom-right
+    top: 5px
+    left: 5px
+</style>

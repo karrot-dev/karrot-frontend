112b0fc74ef2eb616a82b98afda9d9aca0124362
Nick Sellen <git@nicksellen.co.uk>
2023-06-01 09:11:26 +0100

    Disable sentry session tracking

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -9,6 +9,11 @@ export function configureSentry (app, { dsn, environment }) {
     dsn,
     environment,
     release: process.env.KARROT.GIT_SHA1,
+
+    // Disable sentry session tracking
+    // See https://docs.sentry.io/platforms/javascript/configuration/releases/?original_referrer=https%3A%2F%2Fwww.startpage.com%2F#sessions
+    // and https://community.karrot.world/t/sending-too-much-data-to-sentry/1194
+    autoSessionTracking: false,
     ignoreErrors: [
       'ResizeObserver loop limit exceeded', // Chrome
       'ResizeObserver loop completed with undelivered notifications', // Firefox

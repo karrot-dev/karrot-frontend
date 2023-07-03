b6430a8762f501d08984e347f434de9273b61546
Nick Sellen <git@nicksellen.co.uk>
2023-07-03 22:24:06 +0100

    Update picker icon comment

--- src/utils/pickerIcons.js
+++ src/utils/pickerIcons.js
@@ -8,14 +8,12 @@ a big list with the names and tags.
 They are quite big! In total 1.1MB of data (143kb gzipped).
 
 We only actually use have 2 icon libraries installed so we can avoid loading them all.
-In theory the library could load them async, but it uses require() and webpack eagerly loads them all.
 
 Also, the component only lets you pick one icon-set at a time, but you can pass a list of icons...
 So by doing this we can allow the users to choose icons from either set.
 
-We use webpack IgnorePlugin so the component doesn't automatically load any of these icon-set files.
-We are then relying on loading them explicitly here.
-
+We used to have to use webpack IgnorePlugin to avoid all the icon sets bundled in,
+but it seems to be OK now.
 */
 import fontAwesomeV5 from '@quasar/quasar-ui-qiconpicker/dist/icon-set/fontawesome-v5.umd'
 import materialIcons from '@quasar/quasar-ui-qiconpicker/dist/icon-set/material-icons.umd'

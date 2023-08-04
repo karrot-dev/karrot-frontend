39350905f7e11e3fb579176d0212457e58a1be2a
Nick Sellen <git@nicksellen.co.uk>
2023-08-04 13:38:01 +0100

    Use dark as base theme if prefers dark

--- .storybook/karrot-theme.js
+++ .storybook/karrot-theme.js
@@ -1,7 +1,14 @@
 import { create } from '@storybook/theming/create'
 
 export default create({
-  base: 'light',
+  base: isDark() ? 'dark' : 'light',
   brandTitle: 'Karrot Storybook',
   brandUrl: 'https://github.com/karrot-dev/karrot-frontend',
 })
+
+function isDark () {
+  if (typeof window !== 'undefined' && window.matchMedia) {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+  }
+  return false
+}

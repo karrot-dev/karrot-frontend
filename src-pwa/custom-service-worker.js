ed48b80b121a0c333db0e5d293d5f40302df42de
Nick Sellen <git@nicksellen.co.uk>
2023-09-21 23:47:12 +0100

    Improving web push options

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -3,9 +3,6 @@
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "injectManifest"
  *
- * See https://docs.karrot.world/mobile.html for a hint ... you'll need to dig around a bit more on
- * https://console.firebase.google.com/ though to find the sender id.
- *
  */
 
 import { clientsClaim } from 'workbox-core'
@@ -18,32 +15,53 @@ self.skipWaiting()
 clientsClaim()
 
 self.addEventListener('push', event => {
-  console.log('got push event!', event)
   const data = event.data?.json()
-  const { head, body, url } = data
+  const {
+    title,
+    body,
+    url,
+    tag,
+    image_url: imageUrl,
+  } = data
+
+  // See stuff you can set here https://developer.mozilla.org/en-US/docs/Web/API/Notification
+  const options = {
+    body,
+    data: {},
+  }
+
+  if (tag) {
+    options.tag = tag
+    options.renotify = true
+  }
+
+  if (url) {
+    options.data.url = url
+  }
+
+  if (imageUrl) {
+    options.icon = imageUrl
+  }
+
   event.waitUntil(
-    self.registration.showNotification(head, {
-      body,
-      data: { url },
-      actions: [
-        {
-          action: 'blah',
-          title: 'Do It!',
-        },
-      ],
-    }),
+    self.registration.showNotification(title, options),
   )
 })
 
 self.addEventListener('notificationclick', event => {
   event.preventDefault()
-  event.waitUntil(
-    event.notification.close(),
-    self.clients.openWindow(event.notification.data.url),
-  )
+  if (event.notification.data.url) {
+    event.waitUntil(
+      event.notification.close(),
+      self.clients.openWindow(event.notification.data.url),
+    )
+  }
+  else {
+    event.waitUntil(
+      event.notification.close(),
+    )
+  }
 })
-
-// require('./firebase').init()
 
 // __WB_MANIFEST is used by injectManifest to set the files for caching
 precacheAndRoute(self.__WB_MANIFEST)

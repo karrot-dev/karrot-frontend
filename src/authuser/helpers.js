7940244cd0f5caedcfb31995e6d4991568fcc26f
John Shen <shennyj@umich.edu>
2023-12-04 22:21:01 -0500

    Implementing User Timezone
    Implementing User Timezone

--- src/authuser/helpers.js
+++ src/authuser/helpers.js
@@ -1,19 +1,11 @@
-import { unref } from 'vue'
+import { unref } from "vue";
 
-import { useAuthService } from '@/authuser/services'
+import { useAuthService } from "@/authuser/services";
 
-export function useAuthHelpers () {
-  const { userId: currentUserId } = useAuthService()
-
-  function getIsCurrentUser (userOrId) {
-    userOrId = unref(userOrId)
-    if (typeof userOrId === 'number') {
-      return currentUserId.value === userOrId
-    }
-    return currentUserId.value === userOrId.id
-  }
+export function useAuthHelpers() {
+  const { userId: currentUserId } = useAuthService();
 
   return {
     getIsCurrentUser,
-  }
+  };
 }

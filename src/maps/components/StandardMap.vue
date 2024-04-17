e366006b928ab2feda268723195c6ff6336010c5
scofielddd <qiyuan@bu.edu>
2024-04-17 18:21:32 -0400

    add functions: handleMarkerClicked, updateZoom, centerAndZoom. update functions: opacityFor. Those new and updating functions can handle the new localSelectMarker event

--- src/maps/components/StandardMap.vue
+++ src/maps/components/StandardMap.vue
@@ -1,5 +1,6 @@
 <template>
   <KMap
+    ref="kmap"
     v-bind="mapProps"
     :show-attribution="showAttribution"
     :padding-top-left="paddingTopLeft"
@@ -19,6 +20,7 @@
       :popup="marker.popup"
       :opacity="opacityFor(marker)"
       @dragend="event => dragend(event, marker)"
+      @marker-clicked="event => handleMarkerClicked(event, marker)"
     />
     <QMenu
       ref="popover"
@@ -42,7 +44,8 @@ import KMap from './KMap.vue'
 import KMarker from './KMarker.vue'
 
 const SELECTED_OPACITY = 1
-const UNSELECTED_OPACITY = 0.5
+const UNSELECTED_OPACITY = 0.7
+const i = 1
 
 export default {
   components: {
@@ -96,18 +99,23 @@ export default {
     'map-move-end',
     'marker-moved',
     'map-click',
+    'update:selectedMarkers',
   ],
   data () {
     return {
       lastZoom: 15,
       popoverOffset: [0, 0],
       popoverLatLng: null,
+      // Create the localSelect Marker, when we click the specific marker, it would push in localSelect marker
+      localSelectedMarkers: this.selectedMarkers || [],
     }
   },
   computed: {
     mapProps () {
+      console.log('the times called here: ', i)
       if (this.forceZoom && this.forceCenter) {
         // This is used on the full page map view, where the params come from the URL, so we need to ignore all the markers
+        console.log('here 1')
         return {
           zoom: this.forceZoom,
           center: this.forceCenter,
@@ -115,6 +123,7 @@ export default {
       }
       if (this.forceBounds) {
         // This is used when we emphasize nearby groups
+        console.log('here 2')
         return {
           bounds: this.forceBounds,
         }
@@ -147,7 +156,15 @@ export default {
       }
     },
     markersForBounds () {
-      return this.hasSelectedMarkers ? this.selectedMarkers : this.markers
+      if (this.hasSelectedMarkers) {
+        console.log('Selected markers:', this.selectedMarkers) // Log the selected markers
+        return this.selectedMarkers
+      }
+      else {
+        console.log('No selected markers, showing all markers.') // Inform when no markers are selected
+        return this.markers
+      }
+      // return this.hasSelectedMarkers ? this.selectedMarkers : this.markers
     },
     hasSelectedMarkers () {
       return this.selectedMarkers && this.selectedMarkers.length > 0
@@ -157,6 +174,16 @@ export default {
     zoom (val) {
       if (Number.isInteger(val)) this.lastZoom = val
     },
+    selectedMarkers: {
+      handler (newVal) {
+        this.localSelectedMarkers = newVal || []
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
+  mounted () {
+    this.localSelectedMarkers = [...this.selectedMarkers]
   },
   methods: {
     openContextMenu (event) {
@@ -172,16 +199,64 @@ export default {
     dragend ({ target }, marker) {
       this.$emit('marker-moved', target.getLatLng(), marker)
     },
-    updateZoom (val) {
-      if (Number.isInteger(val)) this.lastZoom = val
+    handleMarkerClicked ({ target }, marker) {
+      const isSelected = this.localSelectedMarkers.some(m => m.id === marker.id)
+      if (!isSelected) {
+        this.localSelectedMarkers = [marker]
+        marker.click_count = 1
+        this.centerAndZoom(marker.latLng, 15)
+      }
+      else {
+        // Marker is already selected, increment click count
+        marker.click_count = (marker.click_count || 0) + 1
+        if (marker.click_count % 2 === 0) {
+          // Even number of clicks: zoom out
+          this.fitBoundsToMarkers()
+        }
+        else {
+          // Odd number of clicks: zoom in
+          this.centerAndZoom(marker.latLng, 15)
+        }
+      }
+      this.$emit('update:selectedMarkers', this.localSelectedMarkers)
+      console.log('the local select marker is, ', this.localSelectedMarkers)
+      console.log('this.selectmarker is, ', this.localSelectedMarkers)
     },
+
+    updateZoom (val) {
+      console.log('in zoom level, the select marker is', this.localSelectedMarkers)
+      if (Number.isInteger(val)) this.lastZoom = val
+      if (this.localSelectedMarkers.length === 1) {
+        this.centerAndZoom(this.localSelectedMarkers[0].latLng, val)
+      }
+    },
+    centerAndZoom (latLng, zoomLevel) {
+      // If user choose specific marker, the map would automatically centralize to the location
+      const mapInstance = this.$refs.kmap.leafletMap
+      if (mapInstance) {
+        mapInstance.setView(latLng, zoomLevel)
+      }
+    },
+    fitBoundsToMarkers () {
+      // This function can help the map always focus on the selected marker that user choose
+      const mapInstance = this.$refs.kmap.leafletMap
+      if (mapInstance && this.markers.length) {
+        const bounds = latLngBounds(this.markersForBounds.map(m => m.latLng)).pad(0.2)
+        mapInstance.fitBounds(bounds)
+      }
+    },
+
     mapClick (event) {
       this.closeContextMenu()
       this.$emit('map-click', event)
     },
     opacityFor (marker) {
-      if (!this.hasSelectedMarkers) return SELECTED_OPACITY
-      return this.selectedMarkers.find(m => m.id === marker.id) ? SELECTED_OPACITY : UNSELECTED_OPACITY
+      // If user choose the specific marker, this marker would change to darker color
+      if (!marker || typeof marker.id === 'undefined') {
+        console.error('Invalid marker:', marker)
+        return UNSELECTED_OPACITY
+      }
+      return this.localSelectedMarkers.some(m => m.id === marker.id) ? SELECTED_OPACITY : UNSELECTED_OPACITY
     },
   },
 }
